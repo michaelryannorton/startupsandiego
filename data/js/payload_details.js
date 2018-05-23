f1be22750022c216e752ca09ed85448df5b46a48
Michael Ryan Norton <michael@michaelryannorton.com>
2018-05-22 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3016,
-	"created_at": "Sun May 20 00:17:02 +0000 2018",
+	"tweets": 3017,
+	"created_at": "Wed May 23 00:17:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_05.js
+++ data/js/tweets/2018_05.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2018_05 = 
 [
 	{
+		"created_at": "Tue May 22 21:43:48 +0000 2018",
+		"id": 999043172796911600,
+		"id_str": "999043172796911617",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/N9Wunp7ctS Thanks to @RonRobertsSD @SandraRodkey @MengalFX #sandiego #startups",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiego",
+					"indices": [
+						111,
+						120
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						121,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RonRobertsSD",
+					"name": "Ron Roberts",
+					"id": 72978389,
+					"id_str": "72978389",
+					"indices": [
+						73,
+						86
+					]
+				},
+				{
+					"screen_name": "SandraRodkey",
+					"name": "Sandra Rodkey",
+					"id": 3343856714,
+					"id_str": "3343856714",
+					"indices": [
+						87,
+						100
+					]
+				},
+				{
+					"screen_name": "MengalFX",
+					"name": "MAK Mengal",
+					"id": 493177407,
+					"id_str": "493177407",
+					"indices": [
+						101,
+						110
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/N9Wunp7ctS",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=34aba1a0-5e09-11e8-8cc9-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3009,
+			"friends_count": 2413,
+			"listed_count": 106,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1530,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3017,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/601662542964895745/_3BisLbt.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/601662542964895745/_3BisLbt.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat May 19 21:43:50 +0000 2018",
 		"id": 997956019308453900,
 		"id_str": "997956019308453889",
