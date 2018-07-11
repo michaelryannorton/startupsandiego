5a9dca0ecc2fadaead9b3eef9fab380da03035db
Michael Ryan Norton <michael@michaelryannorton.com>
2018-07-10 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3095,
-	"created_at": "Sun Jul 08 00:17:03 +0000 2018",
+	"tweets": 3096,
+	"created_at": "Wed Jul 11 00:17:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_07.js
+++ data/js/tweets/2018_07.js
@@ -1,6 +1,153 @@
 Grailbird.data.tweets_2018_07 = 
 [
 	{
+		"created_at": "Tue Jul 10 21:45:17 +0000 2018",
+		"id": 1016800551307239400,
+		"id_str": "1016800551307239424",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/UqYZvrOzkU Thanks to @sddowntownnews @PortVisionNews @mpwru #sandiego #empowerandhelp",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiego",
+					"indices": [
+						112,
+						121
+					]
+				},
+				{
+					"text": "empowerandhelp",
+					"indices": [
+						122,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sddowntownnews",
+					"name": "Downtown News",
+					"id": 262790137,
+					"id_str": "262790137",
+					"indices": [
+						73,
+						88
+					]
+				},
+				{
+					"screen_name": "PortVisionNews",
+					"name": "PortVision",
+					"id": 97536290,
+					"id_str": "97536290",
+					"indices": [
+						89,
+						104
+					]
+				},
+				{
+					"screen_name": "mpwru",
+					"name": "LeahO",
+					"id": 454092403,
+					"id_str": "454092403",
+					"indices": [
+						105,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/UqYZvrOzkU",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=886b3240-848a-11e8-8af9-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3196,
+			"friends_count": 2400,
+			"listed_count": 111,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1547,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3095,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Jul 07 21:45:18 +0000 2018",
 		"id": 1015713394408738800,
 		"id_str": "1015713394408738816",
@@ -37,7 +184,7 @@ Grailbird.data.tweets_2018_07 =
 				},
 				{
 					"screen_name": "inGodwetrust29",
-					"name": "DJ 🌹🌹#Trump 🚂🇺🇸🌹💁🏼‍♀️🌺",
+					"name": "DJ 🌹🌹#Trump 🚂🇺🇸🌹💁🏼♀️🌺",
 					"id": 897709569539006500,
 					"id_str": "897709569539006464",
 					"indices": [
