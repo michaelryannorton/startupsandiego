1961832d16a040c2a0fe1e7a8ac5c0a688f372ba
Michael Ryan Norton <michael@michaelryannorton.com>
2018-10-13 17:17:03 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3136,
-	"created_at": "Sat Oct 13 00:17:03 +0000 2018",
+	"tweets": 3138,
+	"created_at": "Sun Oct 14 00:17:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Sat Oct 13 21:48:02 +0000 2018",
+		"id": 1051228092788854800,
+		"id_str": "1051228092788854784",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/M8ZP33yWxm Thanks to @JimElliott123 @Rowe05B @MrHappy4870 #startups #sandiego",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startups",
+					"indices": [
+						110,
+						119
+					]
+				},
+				{
+					"text": "sandiego",
+					"indices": [
+						120,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JimElliott123",
+					"name": "Joe Elliott",
+					"id": 869330737861754900,
+					"id_str": "869330737861754880",
+					"indices": [
+						73,
+						87
+					]
+				},
+				{
+					"screen_name": "Rowe05B",
+					"name": "BDRSportsforMe",
+					"id": 2915688787,
+					"id_str": "2915688787",
+					"indices": [
+						88,
+						96
+					]
+				},
+				{
+					"screen_name": "MrHappy4870",
+					"name": "Brian",
+					"id": 124735878,
+					"id_str": "124735878",
+					"indices": [
+						97,
+						109
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/M8ZP33yWxm",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=a67f0710-cf31-11e8-a440-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3332,
+			"friends_count": 2389,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3136,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/M8ZP33yWxm Thanks to @JimElliott123 @Rowe05B @MrHappy4870 #startups #sandiego"
+	},
+	{
+		"created_at": "Fri Oct 12 17:49:28 +0000 2018",
+		"id": 1050805665185247200,
+		"id_str": "1050805665185247232",
+		"full_text": "We just posted our next @SDXD event:  Improv for UX Leaders. We've got 2 favorite community leaders guiding us in exercises guaranteed to change how we relate to each other: Michael Vargas and Gary@M_A_vargas forw@garywareis one! https://t.co/kJSWCVMOgn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDXD",
+					"name": "Samanta Dairef",
+					"id": 88992559,
+					"id_str": "88992559",
+					"indices": [
+						24,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kJSWCVMOgn",
+					"expanded_url": "https://lnkd.in/gXVFnbr",
+					"display_url": "lnkd.in/gXVFnbr",
+					"indices": [
+						230,
+						253
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.linkedin.com/\" rel=\"nofollow\">LinkedIn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2462041836,
+			"id_str": "2462041836",
+			"name": "Hopscotch Labs",
+			"screen_name": "hopscotchlabs",
+			"location": "San Diego",
+			"description": "Innovation that connects",
+			"url": "http://t.co/m5ApH2UpFw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/m5ApH2UpFw",
+							"expanded_url": "http://www.hopscotchlabs.com",
+							"display_url": "hopscotchlabs.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 183,
+			"friends_count": 107,
+			"listed_count": 8,
+			"created_at": "Thu Apr 24 18:37:49 +0000 2014",
+			"favourites_count": 162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 361,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/585655298305748993/CL3weqTo_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/585655298305748993/CL3weqTo_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2462041836/1428466140",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We just posted our next @SDXD event:  Improv for UX Leaders. We've got 2 favorite community leaders guiding us in exercises guaranteed to change how we relate to each other: Michael Vargas and Gary@M_A_vargas forw@garywareis one! https://t.co/kJSWCVMOgn"
+	},
+	{
 		"created_at": "Fri Oct 12 17:49:28 +0000 2018",
 		"id": 1050805665185247200,
 		"id_str": "1050805665185247232",
