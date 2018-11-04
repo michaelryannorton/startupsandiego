4b46bc70ec41327f0c6fd33c5434eb707a812557
Michael Ryan Norton <michael@michaelryannorton.com>
2018-11-03 17:36:52 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3147,
-	"created_at": "Wed Oct 31 00:17:03 +0000 2018",
+	"tweets": 3148,
+	"created_at": "Sun Nov 04 00:36:51 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_11.js
@@ -0,0 +1,155 @@
+Grailbird.data.tweets_2018_11 = 
+[
+	{
+		"created_at": "Sat Nov 03 21:47:56 +0000 2018",
+		"id": 1058838212154667000,
+		"id_str": "1058838212154667008",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/QN9r50Ct9N Thanks to @SDNewsfeed @Funding911 @ShiftRedSeven #sandiego #mixeduse",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			131
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiego",
+					"indices": [
+						112,
+						121
+					]
+				},
+				{
+					"text": "mixeduse",
+					"indices": [
+						122,
+						131
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDNewsfeed",
+					"name": "SD Newsfeed",
+					"id": 26678989,
+					"id_str": "26678989",
+					"indices": [
+						73,
+						84
+					]
+				},
+				{
+					"screen_name": "Funding911",
+					"name": "Asset Based Loans",
+					"id": 97706444,
+					"id_str": "97706444",
+					"indices": [
+						85,
+						96
+					]
+				},
+				{
+					"screen_name": "ShiftRedSeven",
+					"name": "☀Helio$ensual",
+					"id": 389786746,
+					"id_str": "389786746",
+					"indices": [
+						97,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QN9r50Ct9N",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=1e295c80-dfb2-11e8-a440-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3357,
+			"friends_count": 2382,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3146,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/QN9r50Ct9N Thanks to @SDNewsfeed @Funding911 @ShiftRedSeven #sandiego #mixeduse"
+	}
+]
