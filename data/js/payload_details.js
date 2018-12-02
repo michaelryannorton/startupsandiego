169ba640624308238ee7e0937036c8139c771b22
Michael Ryan Norton <michael@michaelryannorton.com>
2018-12-01 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3155,
-	"created_at": "Wed Nov 28 00:28:53 +0000 2018",
+	"tweets": 3156,
+	"created_at": "Sun Dec 02 00:17:02 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,155 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Sat Dec 01 22:48:02 +0000 2018",
+		"id": 1069000196032483300,
+		"id_str": "1069000196032483330",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/XOqdFHeCOZ Thanks to @VPWABUSE @Sandratiara7 @CoachRalph #sandiego #wifia",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiego",
+					"indices": [
+						109,
+						118
+					]
+				},
+				{
+					"text": "wifia",
+					"indices": [
+						119,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "VPWABUSE",
+					"name": "Abuse To Veterans",
+					"id": 3001447804,
+					"id_str": "3001447804",
+					"indices": [
+						73,
+						82
+					]
+				},
+				{
+					"screen_name": "Sandratiara7",
+					"name": "Sweetsandra7",
+					"id": 946878917289369600,
+					"id_str": "946878917289369600",
+					"indices": [
+						83,
+						96
+					]
+				},
+				{
+					"screen_name": "CoachRalph",
+					"name": "Jerry Ralph",
+					"id": 26026345,
+					"id_str": "26026345",
+					"indices": [
+						97,
+						108
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XOqdFHeCOZ",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=27af27c0-f5bb-11e8-9722-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3379,
+			"friends_count": 2375,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1567,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3154,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/XOqdFHeCOZ Thanks to @VPWABUSE @Sandratiara7 @CoachRalph #sandiego #wifia"
+	}
+]
