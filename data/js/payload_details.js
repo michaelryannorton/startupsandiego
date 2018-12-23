d294f02541e24d48b762c8fffe25f947ee998ee6
Michael Ryan Norton <michael@michaelryannorton.com>
2018-12-22 16:17:02 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3162,
-	"created_at": "Wed Dec 19 00:17:03 +0000 2018",
+	"tweets": 3163,
+	"created_at": "Sun Dec 23 00:17:02 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,158 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sat Dec 22 22:47:59 +0000 2018",
+		"id": 1076610330829221900,
+		"id_str": "1076610330829221888",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/KV6yCburb8 Thanks to @CasaFamiliar @Carlsbadbiz @SDoGoing #sandiego #pacificmarinecharters",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiego",
+					"indices": [
+						110,
+						119
+					]
+				},
+				{
+					"text": "pacificmarinecharters",
+					"indices": [
+						120,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CasaFamiliar",
+					"name": "Casa Familiar",
+					"id": 259007431,
+					"id_str": "259007431",
+					"indices": [
+						73,
+						86
+					]
+				},
+				{
+					"screen_name": "Carlsbadbiz",
+					"name": "Carlsbad Econ Dev",
+					"id": 2424103860,
+					"id_str": "2424103860",
+					"indices": [
+						87,
+						99
+					]
+				},
+				{
+					"screen_name": "SDoGoing",
+					"name": "San Diego oGoing",
+					"id": 1081122150,
+					"id_str": "1081122150",
+					"indices": [
+						100,
+						109
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/KV6yCburb8",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=9fe80190-063b-11e9-9722-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3398,
+			"friends_count": 2375,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1574,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3161,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/KV6yCburb8 Thanks to @CasaFamiliar @Carlsbadbiz @SDoGoing #sandiego #pacificmarinecharters"
+	},
+	{
 		"created_at": "Tue Dec 18 22:48:02 +0000 2018",
 		"id": 1075160790372511700,
 		"id_str": "1075160790372511744",
