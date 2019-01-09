ce29fd6a2ead50e968265ff7e7def3596d9bad64
Michael Ryan Norton <michael@michaelryannorton.com>
2019-01-08 16:17:04 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3181,
-	"created_at": "Tue Jan 08 00:17:03 +0000 2019",
+	"tweets": 3182,
+	"created_at": "Wed Jan 09 00:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,158 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 08 22:47:59 +0000 2019",
+		"id": 1082770923781013500,
+		"id_str": "1082770923781013504",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/cdRjFOjYyk Thanks to @KristeBeck @MetroMBA_LA @haydenhbird #ufcw770 #sdtechos",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ufcw770",
+					"indices": [
+						111,
+						119
+					]
+				},
+				{
+					"text": "sdtechos",
+					"indices": [
+						120,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KristeBeck",
+					"name": "Kriste Beck",
+					"id": 822971721929982000,
+					"id_str": "822971721929981953",
+					"indices": [
+						73,
+						84
+					]
+				},
+				{
+					"screen_name": "MetroMBA_LA",
+					"name": "MetroMBA LA",
+					"id": 2250847566,
+					"id_str": "2250847566",
+					"indices": [
+						85,
+						97
+					]
+				},
+				{
+					"screen_name": "haydenhbird",
+					"name": "Hayden Bird",
+					"id": 306284052,
+					"id_str": "306284052",
+					"indices": [
+						98,
+						110
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cdRjFOjYyk",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=70ff74d0-1397-11e9-9722-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3410,
+			"friends_count": 2377,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1580,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3169,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1518730213",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/cdRjFOjYyk Thanks to @KristeBeck @MetroMBA_LA @haydenhbird #ufcw770 #sdtechos"
+	},
+	{
 		"created_at": "Mon Jan 07 21:41:34 +0000 2019",
 		"id": 1082391819206512600,
 		"id_str": "1082391819206512640",
