8279d07f1ae3411f725edf4586a1c07c0ec737c8
Michael Ryan Norton <michael@michaelryannorton.com>
2019-02-22 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3224,
-	"created_at": "Thu Feb 21 00:17:03 +0000 2019",
+	"tweets": 3225,
+	"created_at": "Sat Feb 23 00:17:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 22 22:30:28 +0000 2019",
+		"id": 1099073968491909100,
+		"id_str": "1099073968491909120",
+		"full_text": "Learn from Reyanne's experience starting a company launched from her appreciation for sustainability. This panel of female founders will discuss developing innovative products, building brands, and attracting loyal customers. https://t.co/3FEQl3Stpe https://t.co/R3RjkrosyC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			249
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3FEQl3Stpe",
+					"expanded_url": "https://startupsd.org/convergence2019/",
+					"display_url": "startupsd.org/convergence201…",
+					"indices": [
+						226,
+						249
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1099073966713589800,
+					"id_str": "1099073966713589761",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/D0Cx8xfX0AEe5Up.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0Cx8xfX0AEe5Up.png",
+					"url": "https://t.co/R3RjkrosyC",
+					"display_url": "pic.twitter.com/R3RjkrosyC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1099073968491909120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1099073966713589800,
+					"id_str": "1099073966713589761",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/D0Cx8xfX0AEe5Up.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0Cx8xfX0AEe5Up.png",
+					"url": "https://t.co/R3RjkrosyC",
+					"display_url": "pic.twitter.com/R3RjkrosyC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1099073968491909120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Building the #SDTech scene | #SanDiego #GoSanDiego #SoCal #Startups #Tech | Join us in June for #SanDiego #StartupWeek #SDSW",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3500,
+			"friends_count": 2380,
+			"listed_count": 115,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1623,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3221,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1547628800",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Learn from Reyanne's experience starting a company launched from her appreciation for sustainability. This panel of female founders will discuss developing innovative products, building brands, and attracting loyal customers. https://t.co/3FEQl3Stpe https://t.co/R3RjkrosyC"
+	},
+	{
 		"created_at": "Wed Feb 20 22:30:27 +0000 2019",
 		"id": 1098349186301526000,
 		"id_str": "1098349186301526017",
