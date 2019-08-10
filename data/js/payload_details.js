7a5e21a0df3df96e295f3f167e0375dce5756b1d
Michael Ryan Norton <michael@michaelryannorton.com>
2019-08-09 17:17:03 -0700

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3482,
-	"created_at": "Wed Aug 07 00:17:02 +0000 2019",
+	"tweets": 3485,
+	"created_at": "Sat Aug 10 00:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,507 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 09 01:14:37 +0000 2019",
+		"id": 1158797981635637200,
+		"id_str": "1159634049771560961",
+		"full_text": "North County! @kdixon and I are hosting a meet &amp; greet to answer any questions you've got about @Techstars, accelerators, starting up, raising money...anything! Come join us at Incubate Ventures in Carlsbad on 8/27. RSVP here: https://t.co/Ew3FWU5nnX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kdixon",
+					"name": "Kendra ☕️🌮🍷",
+					"id": 14226640,
+					"id_str": "14226640",
+					"indices": [
+						14,
+						21
+					]
+				},
+				{
+					"screen_name": "techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						100,
+						110
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ew3FWU5nnX",
+					"expanded_url": "https://www.eventbrite.com/e/techstars-anywhere-meet-and-greet-registration-67828756637",
+					"display_url": "eventbrite.com/e/techstars-an…",
+					"indices": [
+						231,
+						254
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6837002,
+			"id_str": "6837002",
+			"name": "Ryan Kuder",
+			"screen_name": "ryankuder",
+			"location": "San Diego and #Anywhere",
+			"description": "Managing Director, @Techstars Anywhere. @AspenInstitute Fellow. Hoya. #GiveFirst #GoSanDiego",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6490,
+			"friends_count": 3496,
+			"listed_count": 422,
+			"created_at": "Fri Jun 15 16:05:55 +0000 2007",
+			"favourites_count": 7085,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 24059,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "180C12",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1039952319486550016/lD3gF0Hg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1039952319486550016/lD3gF0Hg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6837002/1516464745",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "FECC80",
+			"profile_sidebar_fill_color": "120108",
+			"profile_text_color": "7D333A",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 18,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "North County! @kdixon and I are hosting a meet &amp; greet to answer any questions you've got about @Techstars, accelerators, starting up, raising money...anything! Come join us at Incubate Ventures in Carlsbad on 8/27. RSVP here: https://t.co/Ew3FWU5nnX"
+	},
+	{
+		"created_at": "Fri Aug 09 01:14:06 +0000 2019",
+		"id": 1158932291340472300,
+		"id_str": "1159633921266466816",
+		"full_text": "“There are no clear A or B choices to success. There are opportunities. Do you choose to do nothing or do you choose to grow.” Excellent moderating by @Rachel_Gauvin of @freshbrewedtech and powerful advice from @karenclarkcole. ⚡️⚡️⚡️ #StartupGrind #SanDiego #WhySanDiego https://t.co/Jcp9gKgF5y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupGrind",
+					"indices": [
+						235,
+						248
+					]
+				},
+				{
+					"text": "SanDiego",
+					"indices": [
+						249,
+						258
+					]
+				},
+				{
+					"text": "WhySanDiego",
+					"indices": [
+						259,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Rachel_Gauvin",
+					"name": "Rachel Gauvin",
+					"id": 1563593432,
+					"id_str": "1563593432",
+					"indices": [
+						151,
+						165
+					]
+				},
+				{
+					"screen_name": "freshbrewedtech",
+					"name": "Fresh Brewed Tech",
+					"id": 970531248882896900,
+					"id_str": "970531248882896897",
+					"indices": [
+						169,
+						185
+					]
+				},
+				{
+					"screen_name": "karenclarkcole",
+					"name": "Karen Clark Cole",
+					"id": 188143523,
+					"id_str": "188143523",
+					"indices": [
+						211,
+						226
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1158932253763756000,
+					"id_str": "1158932253763756032",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EBVavphU4AAz8zg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBVavphU4AAz8zg.jpg",
+					"url": "https://t.co/Jcp9gKgF5y",
+					"display_url": "pic.twitter.com/Jcp9gKgF5y",
+					"expanded_url": "https://twitter.com/GirlsInTechSD/status/1158932291340472320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1158932253763756000,
+					"id_str": "1158932253763756032",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EBVavphU4AAz8zg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBVavphU4AAz8zg.jpg",
+					"url": "https://t.co/Jcp9gKgF5y",
+					"display_url": "pic.twitter.com/Jcp9gKgF5y",
+					"expanded_url": "https://twitter.com/GirlsInTechSD/status/1158932291340472320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887073681897279500,
+			"id_str": "887073681897279488",
+			"name": "Girls In Tech SD",
+			"screen_name": "GirlsInTechSD",
+			"location": "San Diego, CA",
+			"description": "Girls in Tech is a global non-profit focused on the engagement, education and empowerment of girls and women who are passionate about technology. #GITSD",
+			"url": "https://t.co/7kOmAxIxYa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7kOmAxIxYa",
+							"expanded_url": "https://bit.ly/2OHLOZW",
+							"display_url": "bit.ly/2OHLOZW",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1247,
+			"friends_count": 1143,
+			"listed_count": 24,
+			"created_at": "Mon Jul 17 22:17:01 +0000 2017",
+			"favourites_count": 11693,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5338,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/946808680678170624/XkPEvwzq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/946808680678170624/XkPEvwzq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887073681897279488/1565148581",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0d3163d3ce14d000",
+			"url": "https://api.twitter.com/1.1/geo/id/0d3163d3ce14d000.json",
+			"place_type": "poi",
+			"name": "Blink UX",
+			"full_name": "Blink UX",
+			"country_code": "",
+			"country": "",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-117.15516,
+							32.713545
+						],
+						[
+							-117.15516,
+							32.713545
+						],
+						[
+							-117.15516,
+							32.713545
+						],
+						[
+							-117.15516,
+							32.713545
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 10,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“There are no clear A or B choices to success. There are opportunities. Do you choose to do nothing or do you choose to grow.” Excellent moderating by @Rachel_Gauvin of @freshbrewedtech and powerful advice from @karenclarkcole. ⚡️⚡️⚡️ #StartupGrind #SanDiego #WhySanDiego https://t.co/Jcp9gKgF5y"
+	},
+	{
+		"created_at": "Fri Aug 09 01:13:54 +0000 2019",
+		"id": 1159120796641636400,
+		"id_str": "1159633872126001153",
+		"full_text": "The secret to being an entrepreneur: have a vision and work hard at it. @StartupSanDiego @Innovate_78 https://t.co/BQfUaKmcQW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						72,
+						88
+					]
+				},
+				{
+					"screen_name": "Innovate_78",
+					"name": "Innovate78",
+					"id": 3063174003,
+					"id_str": "3063174003",
+					"indices": [
+						89,
+						101
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BQfUaKmcQW",
+					"expanded_url": "http://ow.ly/ISiz30pjfzy",
+					"display_url": "ow.ly/ISiz30pjfzy",
+					"indices": [
+						102,
+						125
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 959787080,
+			"id_str": "959787080",
+			"name": "OS Maker Labs",
+			"screen_name": "OSMakerLabs",
+			"location": "Vista, CA 92081",
+			"description": "Open Source Maker Labs is a high-tech digital fabrication lab, where members can learn, collaborate, innovate, design, and build anything!",
+			"url": "http://t.co/gCvGdzy2dR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/gCvGdzy2dR",
+							"expanded_url": "http://www.opensourcemakerlabs.com",
+							"display_url": "opensourcemakerlabs.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 404,
+			"friends_count": 503,
+			"listed_count": 27,
+			"created_at": "Tue Nov 20 05:41:17 +0000 2012",
+			"favourites_count": 568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1281,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDEDED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2882501313/34c1b35c7751c7832f2e858c5409738e_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2882501313/34c1b35c7751c7832f2e858c5409738e_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/959787080/1353828843",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The secret to being an entrepreneur: have a vision and work hard at it. @StartupSanDiego @Innovate_78 https://t.co/BQfUaKmcQW"
+	},
+	{
 		"created_at": "Tue Aug 06 21:48:15 +0000 2019",
 		"id": 1158857343339249700,
 		"id_str": "1158857343339249672",
