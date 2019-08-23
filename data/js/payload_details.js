b9f1b1164faf435a62593b3735b144d040af5aa1
Michael Ryan Norton <michael@michaelryannorton.com>
2019-08-22 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3489,
-	"created_at": "Wed Aug 21 00:17:05 +0000 2019",
+	"tweets": 3490,
+	"created_at": "Fri Aug 23 00:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,249 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 22 13:39:49 +0000 2019",
+		"id": 1164343687570235400,
+		"id_str": "1164532629711888384",
+		"full_text": "Less than 48 hours left to take a chance and compete for $75K in Quick Pitch - the biggest pitch event in San Diego history! https://t.co/i5Dnf3IPZg via @tcasandiego @CONNECTinSD @sdvg @coxbusiness #SDtechos https://t.co/aBPDYJKKSG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDtechos",
+					"indices": [
+						198,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "tcasandiego",
+					"name": "Tech Coast Angels SD",
+					"id": 893178238679236600,
+					"id_str": "893178238679236612",
+					"indices": [
+						153,
+						165
+					]
+				},
+				{
+					"screen_name": "CONNECTinSD",
+					"name": "CONNECT",
+					"id": 319294242,
+					"id_str": "319294242",
+					"indices": [
+						166,
+						178
+					]
+				},
+				{
+					"screen_name": "sdvg",
+					"name": "SDVG",
+					"id": 19350368,
+					"id_str": "19350368",
+					"indices": [
+						179,
+						184
+					]
+				},
+				{
+					"screen_name": "coxbusiness",
+					"name": "Cox Business",
+					"id": 95263650,
+					"id_str": "95263650",
+					"indices": [
+						185,
+						197
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/i5Dnf3IPZg",
+					"expanded_url": "https://buff.ly/2Hk45vt",
+					"display_url": "buff.ly/2Hk45vt",
+					"indices": [
+						125,
+						148
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1164343686127337500,
+					"id_str": "1164343686127337472",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/ECiUaeAWwAAfPR5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECiUaeAWwAAfPR5.jpg",
+					"url": "https://t.co/aBPDYJKKSG",
+					"display_url": "pic.twitter.com/aBPDYJKKSG",
+					"expanded_url": "https://twitter.com/AK_LaunchLeader/status/1164343687570235394/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1164343686127337500,
+					"id_str": "1164343686127337472",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/ECiUaeAWwAAfPR5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECiUaeAWwAAfPR5.jpg",
+					"url": "https://t.co/aBPDYJKKSG",
+					"display_url": "pic.twitter.com/aBPDYJKKSG",
+					"expanded_url": "https://twitter.com/AK_LaunchLeader/status/1164343687570235394/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 601,
+							"h": 401,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3120144299,
+			"id_str": "3120144299",
+			"name": "Ashok Kamal 👊🏾",
+			"screen_name": "AK_LaunchLeader",
+			"location": "San Diego, CA",
+			"description": "CEO @tcasandiego | Angel @outsiteco @backstage_cap etc. | Founder https://t.co/8JzjqYLzwl | Aspiring surfer | Fantasy baseball aficionado | Presentist #SDtechos 💻🌮",
+			"url": "https://t.co/pPbienPRlJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pPbienPRlJ",
+							"expanded_url": "https://www.linkedin.com/in/ashokkamal",
+							"display_url": "linkedin.com/in/ashokkamal",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8JzjqYLzwl",
+							"expanded_url": "http://theschoolofrap.com",
+							"display_url": "theschoolofrap.com",
+							"indices": [
+								66,
+								89
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1280,
+			"friends_count": 195,
+			"listed_count": 104,
+			"created_at": "Thu Mar 26 23:56:28 +0000 2015",
+			"favourites_count": 2204,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3559,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/800157852132261888/Yexu1OAq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/800157852132261888/Yexu1OAq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3120144299/1454742295",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Less than 48 hours left to take a chance and compete for $75K in Quick Pitch - the biggest pitch event in San Diego history! https://t.co/i5Dnf3IPZg via @tcasandiego @CONNECTinSD @sdvg @coxbusiness #SDtechos https://t.co/aBPDYJKKSG"
+	},
+	{
 		"created_at": "Tue Aug 20 21:48:14 +0000 2019",
 		"id": 1163930766981718000,
 		"id_str": "1163930766981718016",
