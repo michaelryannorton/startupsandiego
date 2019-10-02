f29b0db1ed43927455960f8efa54890cdc528350
Michael Ryan Norton <michael@michaelryannorton.com>
2019-10-01 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3513,
-	"created_at": "Mon Sep 30 00:17:02 +0000 2019",
+	"tweets": 3514,
+	"created_at": "Wed Oct 02 00:17:03 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,165 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Tue Oct 01 21:48:14 +0000 2019",
+		"id": 1179151059106832400,
+		"id_str": "1179151059106832385",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/fu1qtrkRkQ Thanks to @UCSDMWP @SDNewsfeed @kingdominfo #technology #stateandlocal",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			133
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "technology",
+					"indices": [
+						107,
+						118
+					]
+				},
+				{
+					"text": "stateandlocal",
+					"indices": [
+						119,
+						133
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UCSDMWP",
+					"name": "UC San Diego Men's Water Polo",
+					"id": 1053857160,
+					"id_str": "1053857160",
+					"indices": [
+						73,
+						81
+					]
+				},
+				{
+					"screen_name": "SDNewsfeed",
+					"name": "SD Newsfeed",
+					"id": 26678989,
+					"id_str": "26678989",
+					"indices": [
+						82,
+						93
+					]
+				},
+				{
+					"screen_name": "kingdominfo",
+					"name": "Emerald Kingdom",
+					"id": 274120608,
+					"id_str": "274120608",
+					"indices": [
+						94,
+						106
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/fu1qtrkRkQ",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=2a2ddea0-e495-11e9-8c91-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3837,
+			"friends_count": 2383,
+			"listed_count": 115,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3548,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/fu1qtrkRkQ Thanks to @UCSDMWP @SDNewsfeed @kingdominfo #technology #stateandlocal"
+	}
+]
