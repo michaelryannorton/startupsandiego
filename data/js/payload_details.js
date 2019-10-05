493922b9d9ab5f6de0ce592e576e6bd4b5db548a
Michael Ryan Norton <michael@michaelryannorton.com>
2019-10-04 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3514,
-	"created_at": "Wed Oct 02 00:17:03 +0000 2019",
+	"tweets": 3515,
+	"created_at": "Sat Oct 05 00:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,307 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 02:59:35 +0000 2019",
+		"id": 1179948607010508800,
+		"id_str": "1179954186445672448",
+		"full_text": "Volunteer appreciation axe throwing night with @StartupSanDiego #startupsd https://t.co/2ExPf5Road",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startupsd",
+					"indices": [
+						64,
+						74
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						47,
+						63
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1179948458066595800,
+					"id_str": "1179948458066595841",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EGAE3vhUUAEIdcM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGAE3vhUUAEIdcM.jpg",
+					"url": "https://t.co/2ExPf5Road",
+					"display_url": "pic.twitter.com/2ExPf5Road",
+					"expanded_url": "https://twitter.com/NealBloom/status/1179948607010508800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1179948458066595800,
+					"id_str": "1179948458066595841",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EGAE3vhUUAEIdcM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGAE3vhUUAEIdcM.jpg",
+					"url": "https://t.co/2ExPf5Road",
+					"display_url": "pic.twitter.com/2ExPf5Road",
+					"expanded_url": "https://twitter.com/NealBloom/status/1179948607010508800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1179948458150523000,
+					"id_str": "1179948458150522880",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EGAE3v1U8AANuyq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGAE3v1U8AANuyq.jpg",
+					"url": "https://t.co/2ExPf5Road",
+					"display_url": "pic.twitter.com/2ExPf5Road",
+					"expanded_url": "https://twitter.com/NealBloom/status/1179948607010508800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1179948458825793500,
+					"id_str": "1179948458825793536",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EGAE3yWUwAAYo9z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGAE3yWUwAAYo9z.jpg",
+					"url": "https://t.co/2ExPf5Road",
+					"display_url": "pic.twitter.com/2ExPf5Road",
+					"expanded_url": "https://twitter.com/NealBloom/status/1179948607010508800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1179948459861762000,
+					"id_str": "1179948459861762048",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/EGAE32NUYAAqUcf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGAE32NUYAAqUcf.jpg",
+					"url": "https://t.co/2ExPf5Road",
+					"display_url": "pic.twitter.com/2ExPf5Road",
+					"expanded_url": "https://twitter.com/NealBloom/status/1179948607010508800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19131762,
+			"id_str": "19131762",
+			"name": "Neal Bloom💡",
+			"screen_name": "NealBloom",
+			"location": "San Diego, CA",
+			"description": "Tech ecosystem builder @StartupSanDiego @FreshBrewedTech | #sdlifechanging #sdtechos🌮💻",
+			"url": "https://t.co/sbmE5iLnTA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sbmE5iLnTA",
+							"expanded_url": "http://freshbrewedtech.com",
+							"display_url": "freshbrewedtech.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2127,
+			"friends_count": 259,
+			"listed_count": 185,
+			"created_at": "Sun Jan 18 01:31:26 +0000 2009",
+			"favourites_count": 12542,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11955,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1168054037041668096/_Fbtp05d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1168054037041668096/_Fbtp05d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19131762/1405621367",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Volunteer appreciation axe throwing night with @StartupSanDiego #startupsd https://t.co/2ExPf5Road"
+	},
+	{
 		"created_at": "Tue Oct 01 21:48:14 +0000 2019",
 		"id": 1179151059106832400,
 		"id_str": "1179151059106832385",
