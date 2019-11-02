522a0311374fd1c1efcc8b918309a103c1c53a6b
Michael Ryan Norton <michael@michaelryannorton.com>
2019-11-02 10:17:04 +1000

    Added 1 tweets

---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,258 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Fri Nov 01 17:20:28 +0000 2019",
+		"id": 1190317696212242400,
+		"id_str": "1190317696212242432",
+		"full_text": "Applications to speak at San Diego Startup Week 2020 are now open! https://t.co/S2YN6cIesb\n\n#sdsw20 #sdsw #startupweek https://t.co/L4T6mwIuk5 https://t.co/8QM5cATk54",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sdsw20",
+					"indices": [
+						92,
+						99
+					]
+				},
+				{
+					"text": "sdsw",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "startupweek",
+					"indices": [
+						106,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/S2YN6cIesb",
+					"expanded_url": "http://startupsd.org/speak",
+					"display_url": "startupsd.org/speak",
+					"indices": [
+						67,
+						90
+					]
+				},
+				{
+					"url": "https://t.co/L4T6mwIuk5",
+					"expanded_url": "http://bit.ly/36jAOvT",
+					"display_url": "bit.ly/36jAOvT",
+					"indices": [
+						119,
+						142
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1190317695109079000,
+					"id_str": "1190317695109079041",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/EITboxrWoAEZWqP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EITboxrWoAEZWqP.jpg",
+					"url": "https://t.co/8QM5cATk54",
+					"display_url": "pic.twitter.com/8QM5cATk54",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1190317696212242432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1190317695109079000,
+					"id_str": "1190317695109079041",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/EITboxrWoAEZWqP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EITboxrWoAEZWqP.jpg",
+					"url": "https://t.co/8QM5cATk54",
+					"display_url": "pic.twitter.com/8QM5cATk54",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1190317696212242432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3870,
+			"friends_count": 2388,
+			"listed_count": 113,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Applications to speak at San Diego Startup Week 2020 are now open! https://t.co/S2YN6cIesb\n\n#sdsw20 #sdsw #startupweek https://t.co/L4T6mwIuk5 https://t.co/8QM5cATk54"
+	}
+]
