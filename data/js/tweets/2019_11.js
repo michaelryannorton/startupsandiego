fde4d1b0f1bd7246554b082bf538baeabc5000cc
Michael Ryan Norton <michael@michaelryannorton.com>
2019-11-08 10:17:03 +1000

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,264 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Thu Nov 07 18:19:33 +0000 2019",
+		"id": 1192188794948214800,
+		"id_str": "1192506892225900546",
+		"full_text": "Sun, surf, &amp; solving problems for a living 💡 — just a typical day at SD-based @ViasatInc. Watch the FULL video w/ engineer Gary Lovely &amp; learn how San Diego has given him a new lease on life &gt;&gt; https://t.co/BLmptklosu #SDlifechanging @Innovate_78 @Carlsbadbiz https://t.co/2vaSenvcNf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDlifechanging",
+					"indices": [
+						232,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ViasatInc",
+					"name": "Viasat Inc.",
+					"id": 74550977,
+					"id_str": "74550977",
+					"indices": [
+						82,
+						92
+					]
+				},
+				{
+					"screen_name": "Innovate_78",
+					"name": "Innovate78",
+					"id": 3063174003,
+					"id_str": "3063174003",
+					"indices": [
+						248,
+						260
+					]
+				},
+				{
+					"screen_name": "Carlsbadbiz",
+					"name": "Carlsbad Econ Dev",
+					"id": 2424103860,
+					"id_str": "2424103860",
+					"indices": [
+						261,
+						273
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BLmptklosu",
+					"expanded_url": "https://buff.ly/2NnRkU3",
+					"display_url": "buff.ly/2NnRkU3",
+					"indices": [
+						208,
+						231
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1192188687028764700,
+					"id_str": "1192188687028764677",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1192188687028764677/pu/img/LvQd-w1Wkawh4NH6.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1192188687028764677/pu/img/LvQd-w1Wkawh4NH6.jpg",
+					"url": "https://t.co/2vaSenvcNf",
+					"display_url": "pic.twitter.com/2vaSenvcNf",
+					"expanded_url": "https://twitter.com/SDlifechanging/status/1192188794948214784/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1192188687028764700,
+					"id_str": "1192188687028764677",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1192188687028764677/pu/img/LvQd-w1Wkawh4NH6.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1192188687028764677/pu/img/LvQd-w1Wkawh4NH6.jpg",
+					"url": "https://t.co/2vaSenvcNf",
+					"display_url": "pic.twitter.com/2vaSenvcNf",
+					"expanded_url": "https://twitter.com/SDlifechanging/status/1192188794948214784/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 50900,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1192188687028764677/pu/vid/320x320/LrTHmyPY3ENeVQ5F.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1192188687028764677/pu/pl/VU_e4LwQlBGyLvrN.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1192188687028764677/pu/vid/480x480/4aDnymEelgobhrXs.mp4?tag=10"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1192188687028764677/pu/vid/720x720/NvPzg2NAWZDE2-Zq.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 821480201808814100,
+			"id_str": "821480201808814080",
+			"name": "SD: Life. Changing.",
+			"screen_name": "SDlifechanging",
+			"location": "San Diego, CA",
+			"description": "In San Diego, people are driven to do more. More days spent outdoors. More breakthrough tech. More ways to do things better. This is #SDlifechanging.",
+			"url": "https://t.co/VDoQB3Uip2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VDoQB3Uip2",
+							"expanded_url": "http://SDlifechanging.org",
+							"display_url": "SDlifechanging.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1347,
+			"friends_count": 288,
+			"listed_count": 13,
+			"created_at": "Tue Jan 17 22:11:57 +0000 2017",
+			"favourites_count": 1112,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1333,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1021811418457243649/RPBW0Hr6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021811418457243649/RPBW0Hr6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/821480201808814080/1566327274",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 24,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sun, surf, &amp; solving problems for a living 💡 — just a typical day at SD-based @ViasatInc. Watch the FULL video w/ engineer Gary Lovely &amp; learn how San Diego has given him a new lease on life &gt;&gt; https://t.co/BLmptklosu #SDlifechanging @Innovate_78 @Carlsbadbiz https://t.co/2vaSenvcNf"
+	},
+	{
 		"created_at": "Wed Nov 06 06:51:53 +0000 2019",
 		"id": 1191532722436468700,
 		"id_str": "1191971446840594432",
