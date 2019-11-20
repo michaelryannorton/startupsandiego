92094f215050ed49074145f55b76fa4c3821ef0f
Michael Ryan Norton <michael@michaelryannorton.com>
2019-11-20 10:17:04 +1000

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Tue Nov 19 22:48:11 +0000 2019",
+		"id": 1196923150463832000,
+		"id_str": "1196923150463832064",
+		"full_text": "Check out The San Diego Startup Pulse! https://t.co/feY76REnxG Thanks to @DonaldL17570423 @HotelierCabal @ucsdsoftball #phonyjoni #sdtechos",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "phonyjoni",
+					"indices": [
+						119,
+						129
+					]
+				},
+				{
+					"text": "sdtechos",
+					"indices": [
+						130,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DonaldL17570423",
+					"name": "Donald Long",
+					"id": 1189239773594038300,
+					"id_str": "1189239773594038272",
+					"indices": [
+						73,
+						89
+					]
+				},
+				{
+					"screen_name": "HotelierCabal",
+					"name": "Hotelier Cabal",
+					"id": 3450994166,
+					"id_str": "3450994166",
+					"indices": [
+						90,
+						104
+					]
+				},
+				{
+					"screen_name": "ucsdsoftball",
+					"name": "UCSD Softball",
+					"id": 1924203848,
+					"id_str": "1924203848",
+					"indices": [
+						105,
+						118
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/feY76REnxG",
+					"expanded_url": "https://paper.li/StartupSanDiego/1511729069?edition_id=a93775d0-0b1e-11ea-9c15-0cc47a0d164b",
+					"display_url": "paper.li/StartupSanDieg…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://paper.li\" rel=\"nofollow\">Paper.li</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3873,
+			"friends_count": 2387,
+			"listed_count": 114,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1923,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3581,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Check out The San Diego Startup Pulse! https://t.co/feY76REnxG Thanks to @DonaldL17570423 @HotelierCabal @ucsdsoftball #phonyjoni #sdtechos"
+	},
+	{
 		"created_at": "Sat Nov 16 22:48:12 +0000 2019",
 		"id": 1195835993095581700,
 		"id_str": "1195835993095581697",
