0b7911db1bde2863b8ebd1af958636649ec05ccb
Michael Ryan Norton <michael@michaelryannorton.com>
2019-12-03 10:17:03 +1000

    Added 1 tweets

---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,272 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 02 18:44:42 +0000 2019",
+		"id": 1201572919643721700,
+		"id_str": "1201572919643721732",
+		"full_text": "See you tonight at the 1st Mondays holiday networking event! Grab a ticket, your ugly sweater, and be at Societe Brewing @ 5:30pm. Tickets start at $5 here: https://t.co/QGiZoFFVRr\n\n#startupsd #entrepreneurship #networking #startups #sdsw https://t.co/1Uoc1jaCEG https://t.co/kMOFnD42Wv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startupsd",
+					"indices": [
+						182,
+						192
+					]
+				},
+				{
+					"text": "entrepreneurship",
+					"indices": [
+						193,
+						210
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						211,
+						222
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						223,
+						232
+					]
+				},
+				{
+					"text": "sdsw",
+					"indices": [
+						233,
+						238
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QGiZoFFVRr",
+					"expanded_url": "http://bit.ly/2DPEYyF",
+					"display_url": "bit.ly/2DPEYyF",
+					"indices": [
+						157,
+						180
+					]
+				},
+				{
+					"url": "https://t.co/1Uoc1jaCEG",
+					"expanded_url": "http://bit.ly/2P9hf1D",
+					"display_url": "bit.ly/2P9hf1D",
+					"indices": [
+						239,
+						262
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1201572918477697000,
+					"id_str": "1201572918477697025",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/EKzYNAGXsAE2ClS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKzYNAGXsAE2ClS.jpg",
+					"url": "https://t.co/kMOFnD42Wv",
+					"display_url": "pic.twitter.com/kMOFnD42Wv",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1201572919643721732/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1201572918477697000,
+					"id_str": "1201572918477697025",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/EKzYNAGXsAE2ClS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKzYNAGXsAE2ClS.jpg",
+					"url": "https://t.co/kMOFnD42Wv",
+					"display_url": "pic.twitter.com/kMOFnD42Wv",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1201572919643721732/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 335,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3889,
+			"friends_count": 2383,
+			"listed_count": 113,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1925,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3587,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "See you tonight at the 1st Mondays holiday networking event! Grab a ticket, your ugly sweater, and be at Societe Brewing @ 5:30pm. Tickets start at $5 here: https://t.co/QGiZoFFVRr\n\n#startupsd #entrepreneurship #networking #startups #sdsw https://t.co/1Uoc1jaCEG https://t.co/kMOFnD42Wv"
+	}
+]
