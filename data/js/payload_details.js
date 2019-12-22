0e6e8814cc9da195aa30ea2aa3b04664afeec4a7
Michael Ryan Norton <michael@michaelryannorton.com>
2019-12-22 10:17:03 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3559,
-	"created_at": "Tue Dec 17 00:17:03 +0000 2019",
+	"tweets": 3560,
+	"created_at": "Sun Dec 22 00:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sat Dec 21 20:07:23 +0000 2019",
+		"id": 1208479095027355600,
+		"id_str": "1208479095027355648",
+		"full_text": "10 days left to get the best price on tickets to San Diego Startup Week 2020! Tickets are $40 for General Admission until December 31st, then increase to $60 each. What a great gift for yourself! https://t.co/HwkqKZnLHe\n\n#SDSW #startupweek #startups #en… https://t.co/vYnomSUcJX https://t.co/HhwUjXqBqj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW",
+					"indices": [
+						221,
+						226
+					]
+				},
+				{
+					"text": "startupweek",
+					"indices": [
+						227,
+						239
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						240,
+						249
+					]
+				},
+				{
+					"text": "en",
+					"indices": [
+						250,
+						253
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HwkqKZnLHe",
+					"expanded_url": "http://bit.ly/2tzn1SO",
+					"display_url": "bit.ly/2tzn1SO",
+					"indices": [
+						196,
+						219
+					]
+				},
+				{
+					"url": "https://t.co/vYnomSUcJX",
+					"expanded_url": "http://bit.ly/38YrXRw",
+					"display_url": "bit.ly/38YrXRw",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1208479093865549800,
+					"id_str": "1208479093865549825",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EMVhVURXkAEUmE7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMVhVURXkAEUmE7.jpg",
+					"url": "https://t.co/HhwUjXqBqj",
+					"display_url": "pic.twitter.com/HhwUjXqBqj",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1208479095027355648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1208479093865549800,
+					"id_str": "1208479093865549825",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EMVhVURXkAEUmE7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMVhVURXkAEUmE7.jpg",
+					"url": "https://t.co/HhwUjXqBqj",
+					"display_url": "pic.twitter.com/HhwUjXqBqj",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1208479095027355648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 380,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3919,
+			"friends_count": 2382,
+			"listed_count": 113,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1943,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3604,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "10 days left to get the best price on tickets to San Diego Startup Week 2020! Tickets are $40 for General Admission until December 31st, then increase to $60 each. What a great gift for yourself! https://t.co/HwkqKZnLHe\n\n#SDSW #startupweek #startups #en… https://t.co/vYnomSUcJX https://t.co/HhwUjXqBqj"
+	},
+	{
 		"created_at": "Mon Dec 16 18:19:09 +0000 2019",
 		"id": 1206639917016715300,
 		"id_str": "1206639917016715264",
