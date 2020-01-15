0e4e2721477a0f8ac5f4f1470560054b13acc5c2
Michael Ryan Norton <michael@michaelryannorton.com>
2020-01-15 10:17:04 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3572,
-	"created_at": "Tue Jan 14 00:17:02 +0000 2020",
+	"tweets": 3573,
+	"created_at": "Wed Jan 15 00:17:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 14 16:40:11 +0000 2020",
+		"id": 1217124258964480000,
+		"id_str": "1217124258964480005",
+		"full_text": "Jan 20th: Hear what Jaime Onofre, Sr Talent Partner of @zapier looks for in talent when recruiting tech and engineering experts for Zapier and learn how you can ace your next interview.\n\nTickets just $5. https://t.co/tKzgVjLa2h\n\n#hiring #jobseeker #employment #tech #engineering https://t.co/FdK8NKMqC8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "hiring",
+					"indices": [
+						229,
+						236
+					]
+				},
+				{
+					"text": "jobseeker",
+					"indices": [
+						237,
+						247
+					]
+				},
+				{
+					"text": "employment",
+					"indices": [
+						248,
+						259
+					]
+				},
+				{
+					"text": "tech",
+					"indices": [
+						260,
+						265
+					]
+				},
+				{
+					"text": "engineering",
+					"indices": [
+						266,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "zapier",
+					"name": "Zapier",
+					"id": 383012058,
+					"id_str": "383012058",
+					"indices": [
+						55,
+						62
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tKzgVjLa2h",
+					"expanded_url": "http://ow.ly/UUjl50xTlGH",
+					"display_url": "ow.ly/UUjl50xTlGH",
+					"indices": [
+						204,
+						227
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1217124257316163600,
+					"id_str": "1217124257316163584",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EOQYEDzXUAAlm22.png",
+					"media_url_https": "https://pbs.twimg.com/media/EOQYEDzXUAAlm22.png",
+					"url": "https://t.co/FdK8NKMqC8",
+					"display_url": "pic.twitter.com/FdK8NKMqC8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1217124258964480005/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1217124257316163600,
+					"id_str": "1217124257316163584",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EOQYEDzXUAAlm22.png",
+					"media_url_https": "https://pbs.twimg.com/media/EOQYEDzXUAAlm22.png",
+					"url": "https://t.co/FdK8NKMqC8",
+					"display_url": "pic.twitter.com/FdK8NKMqC8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1217124258964480005/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 441,
+							"h": 221,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 291,
+									"y": 57,
+									"h": 70,
+									"w": 70
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3946,
+			"friends_count": 2383,
+			"listed_count": 114,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1950,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3621,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Jan 20th: Hear what Jaime Onofre, Sr Talent Partner of @zapier looks for in talent when recruiting tech and engineering experts for Zapier and learn how you can ace your next interview.\n\nTickets just $5. https://t.co/tKzgVjLa2h\n\n#hiring #jobseeker #employment #tech #engineering https://t.co/FdK8NKMqC8"
+	},
+	{
 		"created_at": "Mon Jan 13 17:32:04 +0000 2020",
 		"id": 1216774928315318300,
 		"id_str": "1216774928315318273",
