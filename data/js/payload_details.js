6532488dfa04f4031e6f1b3e01aebd4c12b61db2
Michael Ryan Norton <michael@michaelryannorton.com>
2020-01-18 10:17:04 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3574,
-	"created_at": "Thu Jan 16 00:17:03 +0000 2020",
+	"tweets": 3575,
+	"created_at": "Sat Jan 18 00:17:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,308 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 17 17:08:07 +0000 2020",
+		"id": 1218218453351501800,
+		"id_str": "1218218453351501826",
+		"full_text": "Wowww you guys. We've had overwhelming interest in our January 3rd Mondays event. Thank you to all that have bought tickets. For those that missed out, there'll be another event in February. Tickets available soon!\n\n#hiring #jobseeker #recruitment https://t.co/Nik4PX9aWP https://t.co/hq5IkFiqzL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "hiring",
+					"indices": [
+						216,
+						223
+					]
+				},
+				{
+					"text": "jobseeker",
+					"indices": [
+						224,
+						234
+					]
+				},
+				{
+					"text": "recruitment",
+					"indices": [
+						235,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Nik4PX9aWP",
+					"expanded_url": "http://bit.ly/2TzUyHu",
+					"display_url": "bit.ly/2TzUyHu",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1218218452114120700,
+					"id_str": "1218218452114120708",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EOf7OlEWoAQIc7l.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOf7OlEWoAQIc7l.jpg",
+					"url": "https://t.co/hq5IkFiqzL",
+					"display_url": "pic.twitter.com/hq5IkFiqzL",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1218218453351501826/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1218218452114120700,
+					"id_str": "1218218452114120708",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EOf7OlEWoAQIc7l.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOf7OlEWoAQIc7l.jpg",
+					"url": "https://t.co/hq5IkFiqzL",
+					"display_url": "pic.twitter.com/hq5IkFiqzL",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1218218453351501826/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 39,
+									"y": 143,
+									"h": 67,
+									"w": 67
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3956,
+			"friends_count": 2383,
+			"listed_count": 114,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1949,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3622,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wowww you guys. We've had overwhelming interest in our January 3rd Mondays event. Thank you to all that have bought tickets. For those that missed out, there'll be another event in February. Tickets available soon!\n\n#hiring #jobseeker #recruitment https://t.co/Nik4PX9aWP https://t.co/hq5IkFiqzL"
+	},
+	{
 		"created_at": "Wed Jan 15 17:08:17 +0000 2020",
 		"id": 1217493722251833300,
 		"id_str": "1217493722251833346",
