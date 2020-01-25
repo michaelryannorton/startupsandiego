ba9fde7327b48a755e11593c74a5e9a6c5d20c63
Michael Ryan Norton <michael@michaelryannorton.com>
2020-01-25 10:17:05 +1000

    Added 1 tweets

--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,596 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 24 16:55:13 +0000 2020",
+		"id": 1220751921964863500,
+		"id_str": "1220751921964863489",
+		"full_text": "Teamwork makes the dream work! \n\nIf you want to part of this rockstar team—and let's be honest, why wouldn't you?—you're in luck. We're currently looking for a new Director of Community to add a fresh face to the team. \n\nhttps://t.co/oAITu0KdyV\n\n#hiring #startups #volunteers https://t.co/nezYmCabul",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "hiring",
+					"indices": [
+						246,
+						253
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						254,
+						263
+					]
+				},
+				{
+					"text": "volunteers",
+					"indices": [
+						264,
+						275
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oAITu0KdyV",
+					"expanded_url": "http://ow.ly/Mh1p50y3Iua",
+					"display_url": "ow.ly/Mh1p50y3Iua",
+					"indices": [
+						221,
+						244
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1220751917191745500,
+					"id_str": "1220751917191745536",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EPD7Zp8XUAADWSJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPD7Zp8XUAADWSJ.jpg",
+					"url": "https://t.co/nezYmCabul",
+					"display_url": "pic.twitter.com/nezYmCabul",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1220751921964863489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 243,
+									"y": 75,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 181,
+									"y": 96,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 463,
+									"y": 69,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 307,
+									"y": 95,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 558,
+									"y": 55,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 371,
+									"y": 83,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 105,
+									"y": 44,
+									"h": 51,
+									"w": 51
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1445,
+									"y": 448,
+									"h": 244,
+									"w": 244
+								},
+								{
+									"x": 1078,
+									"y": 574,
+									"h": 236,
+									"w": 236
+								},
+								{
+									"x": 2748,
+									"y": 413,
+									"h": 279,
+									"w": 279
+								},
+								{
+									"x": 1823,
+									"y": 567,
+									"h": 244,
+									"w": 244
+								},
+								{
+									"x": 3311,
+									"y": 330,
+									"h": 307,
+									"w": 307
+								},
+								{
+									"x": 2201,
+									"y": 496,
+									"h": 255,
+									"w": 255
+								},
+								{
+									"x": 626,
+									"y": 263,
+									"h": 303,
+									"w": 303
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 733,
+									"y": 227,
+									"h": 123,
+									"w": 123
+								},
+								{
+									"x": 547,
+									"y": 291,
+									"h": 119,
+									"w": 119
+								},
+								{
+									"x": 1395,
+									"y": 209,
+									"h": 141,
+									"w": 141
+								},
+								{
+									"x": 925,
+									"y": 288,
+									"h": 123,
+									"w": 123
+								},
+								{
+									"x": 1681,
+									"y": 167,
+									"h": 155,
+									"w": 155
+								},
+								{
+									"x": 1117,
+									"y": 251,
+									"h": 129,
+									"w": 129
+								},
+								{
+									"x": 317,
+									"y": 133,
+									"h": 153,
+									"w": 153
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 430,
+									"y": 133,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 320,
+									"y": 170,
+									"h": 70,
+									"w": 70
+								},
+								{
+									"x": 817,
+									"y": 122,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 542,
+									"y": 168,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 985,
+									"y": 98,
+									"h": 91,
+									"w": 91
+								},
+								{
+									"x": 655,
+									"y": 147,
+									"h": 75,
+									"w": 75
+								},
+								{
+									"x": 186,
+									"y": 78,
+									"h": 90,
+									"w": 90
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1220751917191745500,
+					"id_str": "1220751917191745536",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EPD7Zp8XUAADWSJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPD7Zp8XUAADWSJ.jpg",
+					"url": "https://t.co/nezYmCabul",
+					"display_url": "pic.twitter.com/nezYmCabul",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1220751921964863489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 243,
+									"y": 75,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 181,
+									"y": 96,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 463,
+									"y": 69,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 307,
+									"y": 95,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 558,
+									"y": 55,
+									"h": 51,
+									"w": 51
+								},
+								{
+									"x": 371,
+									"y": 83,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 105,
+									"y": 44,
+									"h": 51,
+									"w": 51
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1445,
+									"y": 448,
+									"h": 244,
+									"w": 244
+								},
+								{
+									"x": 1078,
+									"y": 574,
+									"h": 236,
+									"w": 236
+								},
+								{
+									"x": 2748,
+									"y": 413,
+									"h": 279,
+									"w": 279
+								},
+								{
+									"x": 1823,
+									"y": 567,
+									"h": 244,
+									"w": 244
+								},
+								{
+									"x": 3311,
+									"y": 330,
+									"h": 307,
+									"w": 307
+								},
+								{
+									"x": 2201,
+									"y": 496,
+									"h": 255,
+									"w": 255
+								},
+								{
+									"x": 626,
+									"y": 263,
+									"h": 303,
+									"w": 303
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 733,
+									"y": 227,
+									"h": 123,
+									"w": 123
+								},
+								{
+									"x": 547,
+									"y": 291,
+									"h": 119,
+									"w": 119
+								},
+								{
+									"x": 1395,
+									"y": 209,
+									"h": 141,
+									"w": 141
+								},
+								{
+									"x": 925,
+									"y": 288,
+									"h": 123,
+									"w": 123
+								},
+								{
+									"x": 1681,
+									"y": 167,
+									"h": 155,
+									"w": 155
+								},
+								{
+									"x": 1117,
+									"y": 251,
+									"h": 129,
+									"w": 129
+								},
+								{
+									"x": 317,
+									"y": 133,
+									"h": 153,
+									"w": 153
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 430,
+									"y": 133,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 320,
+									"y": 170,
+									"h": 70,
+									"w": 70
+								},
+								{
+									"x": 817,
+									"y": 122,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 542,
+									"y": 168,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 985,
+									"y": 98,
+									"h": 91,
+									"w": 91
+								},
+								{
+									"x": 655,
+									"y": 147,
+									"h": 75,
+									"w": 75
+								},
+								{
+									"x": 186,
+									"y": 78,
+									"h": 90,
+									"w": 90
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3962,
+			"friends_count": 2381,
+			"listed_count": 115,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1949,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3624,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Teamwork makes the dream work! \n\nIf you want to part of this rockstar team—and let's be honest, why wouldn't you?—you're in luck. We're currently looking for a new Director of Community to add a fresh face to the team. \n\nhttps://t.co/oAITu0KdyV\n\n#hiring #startups #volunteers https://t.co/nezYmCabul"
+	},
+	{
 		"created_at": "Thu Jan 23 16:45:15 +0000 2020",
 		"id": 1220387024781435000,
 		"id_str": "1220387024781434881",
