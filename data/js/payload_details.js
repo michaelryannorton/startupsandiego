21bec519fa8bcb1d34445a046e209810652eb024
Michael Ryan Norton <michael@michaelryannorton.com>
2020-01-28 10:17:04 +1000

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3578,
-	"created_at": "Sun Jan 26 00:17:03 +0000 2020",
+	"tweets": 3579,
+	"created_at": "Tue Jan 28 00:17:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,171 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 27 16:00:52 +0000 2020",
+		"id": 1221825408116674600,
+		"id_str": "1221825408116674560",
+		"full_text": "Join us for beers and burritos at our February 1st Mondays event! Support local businesses, mingle with other entrepreneurs and professionals, and start the month off right. @SocieteBrewing \n\nhttps://t.co/Fr2k5pMT7M\n\n#startups #entrepreneurs #networking #sandiegoevents #sandiego",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startups",
+					"indices": [
+						217,
+						226
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						227,
+						241
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						242,
+						253
+					]
+				},
+				{
+					"text": "sandiegoevents",
+					"indices": [
+						254,
+						269
+					]
+				},
+				{
+					"text": "sandiego",
+					"indices": [
+						270,
+						279
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SocieteBrewing",
+					"name": "Societe Brewing",
+					"id": 277250135,
+					"id_str": "277250135",
+					"indices": [
+						174,
+						189
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Fr2k5pMT7M",
+					"expanded_url": "http://ow.ly/Amwg50y5cW4",
+					"display_url": "ow.ly/Amwg50y5cW4",
+					"indices": [
+						192,
+						215
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD | Dates for #SDSW20 to be announced soon. https://t.co/apBzLwX7RV",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								98,
+								121
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 3971,
+			"friends_count": 2380,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1949,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3626,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Join us for beers and burritos at our February 1st Mondays event! Support local businesses, mingle with other entrepreneurs and professionals, and start the month off right. @SocieteBrewing \n\nhttps://t.co/Fr2k5pMT7M\n\n#startups #entrepreneurs #networking #sandiegoevents #sandiego"
+	},
+	{
 		"created_at": "Sat Jan 25 18:00:41 +0000 2020",
 		"id": 1221130787132727300,
 		"id_str": "1221130787132727297",
