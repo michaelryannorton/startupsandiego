ae434b9b67b9528bc2cc83570c583376730cee30
Michael Ryan Norton <michael@michaelryannorton.com>
2020-03-02 16:17:04 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3601,
-	"created_at": "Fri Feb 28 00:17:04 +0000 2020",
+	"tweets": 3602,
+	"created_at": "Tue Mar 03 00:17:03 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_03.js
@@ -0,0 +1,282 @@
+Grailbird.data.tweets_2020_03 = 
+[
+	{
+		"created_at": "Mon Mar 02 16:01:07 +0000 2020",
+		"id": 1234509044888604700,
+		"id_str": "1234509044888604672",
+		"full_text": "See you all tonight @ballastpointbrewing for our 1st Mondays event! https://t.co/seOCpDyoo0\n\n#1stMondays #sandiego #sandiegoevents #networking #entrepreneur #entrepreneurship #smallbusiness  #startup #entrepreneurs #networkingevent  #startuplife #startups https://t.co/jS7ndQgP4W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						93,
+						104
+					]
+				},
+				{
+					"text": "sandiego",
+					"indices": [
+						105,
+						114
+					]
+				},
+				{
+					"text": "sandiegoevents",
+					"indices": [
+						115,
+						130
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						131,
+						142
+					]
+				},
+				{
+					"text": "entrepreneur",
+					"indices": [
+						143,
+						156
+					]
+				},
+				{
+					"text": "entrepreneurship",
+					"indices": [
+						157,
+						174
+					]
+				},
+				{
+					"text": "smallbusiness",
+					"indices": [
+						175,
+						189
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						191,
+						199
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						200,
+						214
+					]
+				},
+				{
+					"text": "networkingevent",
+					"indices": [
+						215,
+						231
+					]
+				},
+				{
+					"text": "startuplife",
+					"indices": [
+						233,
+						245
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						246,
+						255
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/seOCpDyoo0",
+					"expanded_url": "http://ow.ly/4ri550yzpsR",
+					"display_url": "ow.ly/4ri550yzpsR",
+					"indices": [
+						68,
+						91
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1234509043420582000,
+					"id_str": "1234509043420581893",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/ESHbby0XUAUyjf_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESHbby0XUAUyjf_.jpg",
+					"url": "https://t.co/jS7ndQgP4W",
+					"display_url": "pic.twitter.com/jS7ndQgP4W",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1234509044888604672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1234509043420582000,
+					"id_str": "1234509043420581893",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/ESHbby0XUAUyjf_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESHbby0XUAUyjf_.jpg",
+					"url": "https://t.co/jS7ndQgP4W",
+					"display_url": "pic.twitter.com/jS7ndQgP4W",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1234509044888604672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSW20 session voting coming soon. https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								81,
+								104
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4008,
+			"friends_count": 2376,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3653,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "See you all tonight @ballastpointbrewing for our 1st Mondays event! https://t.co/seOCpDyoo0\n\n#1stMondays #sandiego #sandiegoevents #networking #entrepreneur #entrepreneurship #smallbusiness  #startup #entrepreneurs #networkingevent  #startuplife #startups https://t.co/jS7ndQgP4W"
+	}
+]
