eeb83e85d5545d679b476d0c8748562c20b59993
Michael Ryan Norton <michael@michaelryannorton.com>
2020-04-02 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3607,
-	"created_at": "Thu Apr 02 00:17:03 +0000 2020",
+	"tweets": 3608,
+	"created_at": "Fri Apr 03 00:17:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 02 15:00:44 +0000 2020",
+		"id": 1245727876542017500,
+		"id_str": "1245727876542017536",
+		"full_text": "Get your ticket to tonight's virtual networking event for the startup community, put on by local startup @thinkacai. Use code SSD20 for 20% off your ticket! \n@metabyacai #MxA #sandiegostartups #networking \nhttps://t.co/OVdRPYCk88 https://t.co/F2URGKPKda",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MxA",
+					"indices": [
+						170,
+						174
+					]
+				},
+				{
+					"text": "sandiegostartups",
+					"indices": [
+						175,
+						192
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						193,
+						204
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thinkacai",
+					"name": "Acai",
+					"id": 1212878864096972800,
+					"id_str": "1212878864096972800",
+					"indices": [
+						105,
+						115
+					]
+				},
+				{
+					"screen_name": "metabyacai",
+					"name": "Meta by Acai",
+					"id": 1240925171071864800,
+					"id_str": "1240925171071864832",
+					"indices": [
+						158,
+						169
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OVdRPYCk88",
+					"expanded_url": "https://www.eventbrite.com/e/99288281924",
+					"display_url": "eventbrite.com/e/99288281924",
+					"indices": [
+						206,
+						229
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1245727873803210800,
+					"id_str": "1245727873803210752",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/EUm25qzX0AAYBDB.png",
+					"media_url_https": "https://pbs.twimg.com/media/EUm25qzX0AAYBDB.png",
+					"url": "https://t.co/F2URGKPKda",
+					"display_url": "pic.twitter.com/F2URGKPKda",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1245727876542017536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1835,
+							"h": 918,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1245727873803210800,
+					"id_str": "1245727873803210752",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/EUm25qzX0AAYBDB.png",
+					"media_url_https": "https://pbs.twimg.com/media/EUm25qzX0AAYBDB.png",
+					"url": "https://t.co/F2URGKPKda",
+					"display_url": "pic.twitter.com/F2URGKPKda",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1245727876542017536/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1835,
+							"h": 918,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSW20 session voting coming soon. https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								81,
+								104
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4026,
+			"friends_count": 2370,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 1956,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3659,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get your ticket to tonight's virtual networking event for the startup community, put on by local startup @thinkacai. Use code SSD20 for 20% off your ticket! \n@metabyacai #MxA #sandiegostartups #networking \nhttps://t.co/OVdRPYCk88 https://t.co/F2URGKPKda"
+	},
+	{
 		"created_at": "Wed Apr 01 06:44:57 +0000 2020",
 		"id": 1244699670473040000,
 		"id_str": "1245240719129755648",
