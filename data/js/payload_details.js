527afe9c883f99064c01fd6e876437cfaa7b2ca5
Michael Ryan Norton <michael@michaelryannorton.com>
2020-05-01 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3621,
-	"created_at": "Fri May 01 00:17:02 +0000 2020",
+	"tweets": 3622,
+	"created_at": "Sat May 02 00:17:02 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,208 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Fri May 01 16:50:40 +0000 2020",
+		"id": 1256260844884615200,
+		"id_str": "1256264789233262592",
+		"full_text": "Today’s the day! Join us from 10 – 11 a.m. in the digital Collaboratory for Downtown Innovation Startup Series with @StartupSanDiego and Mission Edge. Today’s discussion will focus on embracing resiliency and finding opportunities.\nRegister: https://t.co/7vqj603hI9 https://t.co/WBPNKZ9uFT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						116,
+						132
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7vqj603hI9",
+					"expanded_url": "http://www.eventbrite.com/e/embracing-resiliency-finding-opportunities-workshop-2-of-5-tickets-96579318341",
+					"display_url": "eventbrite.com/e/embracing-re…",
+					"indices": [
+						242,
+						265
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1256259917528236000,
+					"id_str": "1256259917528236032",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/EW8hvTHVAAAu10p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW8hvTHVAAAu10p.jpg",
+					"url": "https://t.co/WBPNKZ9uFT",
+					"display_url": "pic.twitter.com/WBPNKZ9uFT",
+					"expanded_url": "https://twitter.com/SDPartnership/status/1256260844884615168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 628,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 628,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256259917528236000,
+					"id_str": "1256259917528236032",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/EW8hvTHVAAAu10p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW8hvTHVAAAu10p.jpg",
+					"url": "https://t.co/WBPNKZ9uFT",
+					"display_url": "pic.twitter.com/WBPNKZ9uFT",
+					"expanded_url": "https://twitter.com/SDPartnership/status/1256260844884615168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 628,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 628,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 362197703,
+			"id_str": "362197703",
+			"name": "Downtown San Diego",
+			"screen_name": "SDPartnership",
+			"location": "San Diego, CA",
+			"description": "We are the leading advocate and voice for the Downtown San Diego neighborhood. Live, learn, work, play, stay, and eat! Visit us at http://t.co/pMoKbsag9u",
+			"url": "http://t.co/1TFLJb0KIg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1TFLJb0KIg",
+							"expanded_url": "http://downtownsandiego.org/",
+							"display_url": "downtownsandiego.org",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/pMoKbsag9u",
+							"expanded_url": "http://www.downtownsandiego.org",
+							"display_url": "downtownsandiego.org",
+							"indices": [
+								131,
+								153
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 12937,
+			"friends_count": 3320,
+			"listed_count": 245,
+			"created_at": "Fri Aug 26 00:46:31 +0000 2011",
+			"favourites_count": 2143,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5565,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "171717",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/590203620429148160/yxBSlyVy_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/590203620429148160/yxBSlyVy_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/362197703/1571768053",
+			"profile_link_color": "273234",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "62978D",
+			"profile_text_color": "46786D",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today’s the day! Join us from 10 – 11 a.m. in the digital Collaboratory for Downtown Innovation Startup Series with @StartupSanDiego and Mission Edge. Today’s discussion will focus on embracing resiliency and finding opportunities.\nRegister: https://t.co/7vqj603hI9 https://t.co/WBPNKZ9uFT"
+	}
+]
