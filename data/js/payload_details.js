01e198e2cec2116251cec0dbd824089b22edb682
Michael Ryan Norton <michael@michaelryannorton.com>
2020-05-22 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3637,
-	"created_at": "Fri May 22 00:17:02 +0000 2020",
+	"tweets": 3638,
+	"created_at": "Sat May 23 00:17:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 22 15:01:56 +0000 2020",
+		"id": 1263847571421888500,
+		"id_str": "1263847571421888513",
+		"full_text": "#TGIF San Diego! Grab your ☕ &amp; join us for #TheStartupSeries final #digital workshop + #virtual #networking session starting in t-minus 2 hours! ⏳ #StartupSD #community #entrepreneurs #MxA @SDPartnership @MissionEdgeSD @metabyacai https://t.co/qrsRifutcl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TGIF",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "TheStartupSeries",
+					"indices": [
+						47,
+						64
+					]
+				},
+				{
+					"text": "digital",
+					"indices": [
+						71,
+						79
+					]
+				},
+				{
+					"text": "virtual",
+					"indices": [
+						91,
+						99
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						100,
+						111
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						151,
+						161
+					]
+				},
+				{
+					"text": "community",
+					"indices": [
+						162,
+						172
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						173,
+						187
+					]
+				},
+				{
+					"text": "MxA",
+					"indices": [
+						188,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDPartnership",
+					"name": "Downtown San Diego",
+					"id": 362197703,
+					"id_str": "362197703",
+					"indices": [
+						193,
+						207
+					]
+				},
+				{
+					"screen_name": "MissionEdgeSD",
+					"name": "Mission Edge",
+					"id": 130852872,
+					"id_str": "130852872",
+					"indices": [
+						208,
+						222
+					]
+				},
+				{
+					"screen_name": "metabyacai",
+					"name": "Meta by Acai",
+					"id": 1240925171071864800,
+					"id_str": "1240925171071864832",
+					"indices": [
+						223,
+						234
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qrsRifutcl",
+					"expanded_url": "https://www.eventbrite.com/e/96588654265",
+					"display_url": "eventbrite.com/e/96588654265",
+					"indices": [
+						235,
+						258
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSW20 session voting coming soon. https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								81,
+								104
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4130,
+			"friends_count": 2370,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2004,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3701,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#TGIF San Diego! Grab your ☕ &amp; join us for #TheStartupSeries final #digital workshop + #virtual #networking session starting in t-minus 2 hours! ⏳ #StartupSD #community #entrepreneurs #MxA @SDPartnership @MissionEdgeSD @metabyacai https://t.co/qrsRifutcl"
+	},
+	{
 		"created_at": "Thu May 21 17:01:47 +0000 2020",
 		"id": 1263515342778912800,
 		"id_str": "1263515342778912770",
