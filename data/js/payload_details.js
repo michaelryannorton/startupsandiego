d46b989ca843d13afc022eef10dfa29d40c447ba
Michael Ryan Norton <michael@michaelryannorton.com>
2020-08-06 17:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3660,
-	"created_at": "Tue Aug 04 00:17:04 +0000 2020",
+	"tweets": 3661,
+	"created_at": "Fri Aug 07 00:17:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,359 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 06 03:50:28 +0000 2020",
+		"id": 1291220069188419600,
+		"id_str": "1291220069188419585",
+		"full_text": "Excited to support @TheBrinkSbdc on the road to #SDAngelCon 2021 🚀!  #StartupSD x #SDAC #community #partners https://t.co/0PFd83hZOR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDAngelCon",
+					"indices": [
+						48,
+						59
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						69,
+						79
+					]
+				},
+				{
+					"text": "SDAC",
+					"indices": [
+						82,
+						87
+					]
+				},
+				{
+					"text": "community",
+					"indices": [
+						88,
+						98
+					]
+				},
+				{
+					"text": "partners",
+					"indices": [
+						99,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TheBrinkSbdc",
+					"name": "Brink SBDC",
+					"id": 1020365445520502800,
+					"id_str": "1020365445520502784",
+					"indices": [
+						19,
+						32
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/0PFd83hZOR",
+					"expanded_url": "https://twitter.com/thebrinksbdc/status/1289248726779744256",
+					"display_url": "twitter.com/thebrinksbdc/s…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4143,
+			"friends_count": 2370,
+			"listed_count": 115,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2024,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3728,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1561565943",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1289248726779744300,
+		"quoted_status_id_str": "1289248726779744256",
+		"quoted_status_permalink": {
+			"url": "https://t.co/0PFd83hZOR",
+			"expanded": "https://twitter.com/thebrinksbdc/status/1289248726779744256",
+			"display": "twitter.com/thebrinksbdc/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jul 31 17:17:04 +0000 2020",
+			"id": 1289248726779744300,
+			"id_str": "1289248726779744256",
+			"full_text": "The journey begins. This year's E-Track series is better than ever. Deck deconstruction, marketing, financial statements, use of funds, investor networking, and the list goes on! Register for our 14 part workshop for FREE. We'll see you 9.9.20: https://t.co/c11v8OwMAV\n@SDAngelCon https://t.co/Jy8PgKzD1S",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/c11v8OwMAV",
+						"expanded_url": "https://zcu.io/sfnG",
+						"display_url": "zcu.io/sfnG",
+						"indices": [
+							245,
+							268
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1289248723801788400,
+						"id_str": "1289248723801788416",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/EeRU4jFU0AAmD9b.png",
+						"media_url_https": "https://pbs.twimg.com/media/EeRU4jFU0AAmD9b.png",
+						"url": "https://t.co/Jy8PgKzD1S",
+						"display_url": "pic.twitter.com/Jy8PgKzD1S",
+						"expanded_url": "https://twitter.com/TheBrinkSbdc/status/1289248726779744256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 960,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 502,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 356,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1289248723801788400,
+						"id_str": "1289248723801788416",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/EeRU4jFU0AAmD9b.png",
+						"media_url_https": "https://pbs.twimg.com/media/EeRU4jFU0AAmD9b.png",
+						"url": "https://t.co/Jy8PgKzD1S",
+						"display_url": "pic.twitter.com/Jy8PgKzD1S",
+						"expanded_url": "https://twitter.com/TheBrinkSbdc/status/1289248726779744256/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 960,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 960,
+								"h": 502,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 356,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://social.zoho.com\" rel=\"nofollow\">Zoho Social</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1020365445520502800,
+				"id_str": "1020365445520502784",
+				"name": "Brink SBDC",
+				"screen_name": "TheBrinkSbdc",
+				"location": "San Diego, CA",
+				"description": "The Brink SBDC offers world-class consulting and relevant, insightful guidance to growth-oriented, innovation-based entrepreneurs. It's free and confidential.",
+				"url": "https://t.co/GGi80SvtrZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/GGi80SvtrZ",
+								"expanded_url": "http://sandiego.edu/sbdc",
+								"display_url": "sandiego.edu/sbdc",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 222,
+				"friends_count": 406,
+				"listed_count": 2,
+				"created_at": "Fri Jul 20 17:50:54 +0000 2018",
+				"favourites_count": 961,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 346,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1247242712236998656/6h_GUS01_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247242712236998656/6h_GUS01_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1020365445520502784/1568066126",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Excited to support @TheBrinkSbdc on the road to #SDAngelCon 2021 🚀!  #StartupSD x #SDAC #community #partners https://t.co/0PFd83hZOR"
+	},
+	{
 		"created_at": "Mon Aug 03 17:57:37 +0000 2020",
 		"id": 1290346097517432800,
 		"id_str": "1290346097517432833",
