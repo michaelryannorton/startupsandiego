73e7d159f23f68297da83d1438f47314d8624a05
Michael Ryan Norton <michael@michaelryannorton.com>
2020-09-01 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3670,
-	"created_at": "Tue Sep 01 00:17:04 +0000 2020",
+	"tweets": 3671,
+	"created_at": "Wed Sep 02 00:17:02 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_09.js
@@ -0,0 +1,357 @@
+Grailbird.data.tweets_2020_09 = 
+[
+	{
+		"created_at": "Tue Sep 01 16:11:37 +0000 2020",
+		"id": 1300580172622475300,
+		"id_str": "1300828669003300865",
+		"full_text": "#1stMondays are BAAAAACK! We missed you @StartupSanDiego 😘  Register today! ⬇️ https://t.co/NObYwmmzMD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						40,
+						56
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NObYwmmzMD",
+					"expanded_url": "https://twitter.com/freshbrewedtech/status/1300482498401624067",
+					"display_url": "twitter.com/freshbrewedtec…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 49085200,
+			"id_str": "49085200",
+			"name": "UCSD OIC",
+			"screen_name": "UCSDInnovation",
+			"location": "La Jolla CA",
+			"description": "UC San Diego Office of Innovation and Commercialization: Leading regional social and economic development – building a dynamic innovation culture and ecosystem.",
+			"url": "https://t.co/Uij5ceg6o5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Uij5ceg6o5",
+							"expanded_url": "http://innovation.ucsd.edu",
+							"display_url": "innovation.ucsd.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1636,
+			"friends_count": 1239,
+			"listed_count": 101,
+			"created_at": "Sat Jun 20 19:02:47 +0000 2009",
+			"favourites_count": 722,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4417,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/843898368396939264/FtGToChU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/843898368396939264/FtGToChU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/49085200/1571341162",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1300482498401624000,
+		"quoted_status_id_str": "1300482498401624067",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NObYwmmzMD",
+			"expanded": "https://twitter.com/freshbrewedtech/status/1300482498401624067",
+			"display": "twitter.com/freshbrewedtec…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Aug 31 17:16:04 +0000 2020",
+			"id": 1300482498401624000,
+			"id_str": "1300482498401624067",
+			"full_text": "On Monday, Sept 7, join the @startupsandiego 1st Mondays Virtual Happy Hour and start connecting with the local startup community. Attendees chat during 5-minute speed networking sessions to meet San Diego tech-ecosystem talent. https://t.co/2pU0H52y5Q @sdstartupweek #1stMondays https://t.co/4FLZTuuWRb",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				279
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "1stMondays",
+						"indices": [
+							268,
+							279
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "StartupSanDiego",
+						"name": "Startup San Diego",
+						"id": 3222944604,
+						"id_str": "3222944604",
+						"indices": [
+							28,
+							44
+						]
+					},
+					{
+						"screen_name": "SDStartupWeek",
+						"name": "San Diego Startup Month",
+						"id": 2339762118,
+						"id_str": "2339762118",
+						"indices": [
+							253,
+							267
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/2pU0H52y5Q",
+						"expanded_url": "https://bit.ly/3gKzsyj",
+						"display_url": "bit.ly/3gKzsyj",
+						"indices": [
+							229,
+							252
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1300482495499116500,
+						"id_str": "1300482495499116557",
+						"indices": [
+							280,
+							303
+						],
+						"media_url": "http://pbs.twimg.com/media/Egw98HzWoA0qSL5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Egw98HzWoA0qSL5.jpg",
+						"url": "https://t.co/4FLZTuuWRb",
+						"display_url": "pic.twitter.com/4FLZTuuWRb",
+						"expanded_url": "https://twitter.com/freshbrewedtech/status/1300482498401624067/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1300482495499116500,
+						"id_str": "1300482495499116557",
+						"indices": [
+							280,
+							303
+						],
+						"media_url": "http://pbs.twimg.com/media/Egw98HzWoA0qSL5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Egw98HzWoA0qSL5.jpg",
+						"url": "https://t.co/4FLZTuuWRb",
+						"display_url": "pic.twitter.com/4FLZTuuWRb",
+						"expanded_url": "https://twitter.com/freshbrewedtech/status/1300482498401624067/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 600,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 970531248882896900,
+				"id_str": "970531248882896897",
+				"name": "Fresh Brewed Tech - SD Tech News",
+				"screen_name": "freshbrewedtech",
+				"location": "San Diego, CA",
+				"description": "Serving up the latest San Diego tech news and tacos 🌮 Subscribe to our newsletter here https://t.co/Msp95nNQqF & check out our podcast @TacosandTechPod",
+				"url": "https://t.co/gqgiCYPCGk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gqgiCYPCGk",
+								"expanded_url": "http://FreshBrewedTech.com",
+								"display_url": "FreshBrewedTech.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Msp95nNQqF",
+								"expanded_url": "http://conta.cc/2XsAbOi",
+								"display_url": "conta.cc/2XsAbOi",
+								"indices": [
+									87,
+									110
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 1224,
+				"friends_count": 859,
+				"listed_count": 16,
+				"created_at": "Mon Mar 05 05:27:36 +0000 2018",
+				"favourites_count": 1823,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4084,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/991396707085647872/EhZ0mt58_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/991396707085647872/EhZ0mt58_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/970531248882896897/1574713241",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#1stMondays are BAAAAACK! We missed you @StartupSanDiego 😘  Register today! ⬇️ https://t.co/NObYwmmzMD"
+	}
+]
