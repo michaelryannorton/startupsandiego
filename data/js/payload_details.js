37de969512dda433d5c0c423dad7c8b106690602
Michael Ryan Norton <michael@michaelryannorton.com>
2020-09-28 17:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3689,
-	"created_at": "Mon Sep 28 00:17:04 +0000 2020",
+	"tweets": 3690,
+	"created_at": "Tue Sep 29 00:17:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Mon Sep 28 17:45:28 +0000 2020",
+		"id": 1310636759051632600,
+		"id_str": "1310636759051632646",
+		"full_text": "Thanks to our incredible partner, @coxbusiness, we are officially entering a 12-hour Startup Month pass lightning flash-sale⚡! Don't miss out, snag your pass for 50% off with promo code: SDSMFLASH50 before they're gone! https://t.co/ARp1VZ12gm\n\n#SDSM2020 #StartUpSD #ThankYouCox https://t.co/kX6yMOjshN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSM2020",
+					"indices": [
+						245,
+						254
+					]
+				},
+				{
+					"text": "StartUpSD",
+					"indices": [
+						255,
+						265
+					]
+				},
+				{
+					"text": "ThankYouCox",
+					"indices": [
+						266,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "coxbusiness",
+					"name": "Cox Business",
+					"id": 95263650,
+					"id_str": "95263650",
+					"indices": [
+						34,
+						46
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/ARp1VZ12gm",
+					"expanded_url": "http://ow.ly/aCay50BDqjv",
+					"display_url": "ow.ly/aCay50BDqjv",
+					"indices": [
+						220,
+						243
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1310636754756567000,
+					"id_str": "1310636754756567040",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EjBRLzTWAAAtYsT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjBRLzTWAAAtYsT.jpg",
+					"url": "https://t.co/kX6yMOjshN",
+					"display_url": "pic.twitter.com/kX6yMOjshN",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1310636759051632646/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1310636754756567000,
+					"id_str": "1310636754756567040",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EjBRLzTWAAAtYsT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjBRLzTWAAAtYsT.jpg",
+					"url": "https://t.co/kX6yMOjshN",
+					"display_url": "pic.twitter.com/kX6yMOjshN",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1310636759051632646/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4168,
+			"friends_count": 2367,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2056,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3766,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thanks to our incredible partner, @coxbusiness, we are officially entering a 12-hour Startup Month pass lightning flash-sale⚡! Don't miss out, snag your pass for 50% off with promo code: SDSMFLASH50 before they're gone! https://t.co/ARp1VZ12gm\n\n#SDSM2020 #StartUpSD #ThankYouCox https://t.co/kX6yMOjshN"
+	},
+	{
 		"created_at": "Sun Sep 27 21:00:31 +0000 2020",
 		"id": 1310323459130642400,
 		"id_str": "1310323459130642434",
