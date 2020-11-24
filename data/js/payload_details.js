2fdb8ed534921d089bc199272d6a68e847373a4a
Michael Ryan Norton <michael@michaelryannorton.com>
2020-11-23 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3788,
-	"created_at": "Tue Nov 03 00:17:03 +0000 2020",
+	"tweets": 3789,
+	"created_at": "Tue Nov 24 00:17:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 23 19:00:25 +0000 2020",
+		"id": 1330949343378018300,
+		"id_str": "1330949343378018304",
+		"full_text": "Congratulations to the #SDSM2020 Incubator Demo Day winner, BizSecure! @biz_secure provides ID verification, data analytics, &amp; cyber security services. Congrats to all 10 incubator #startups! We're excited for your growth! https://t.co/c72ylyE3xe #StartupSanDiego https://t.co/m6tIGZBkTW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSM2020",
+					"indices": [
+						23,
+						32
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						185,
+						194
+					]
+				},
+				{
+					"text": "StartupSanDiego",
+					"indices": [
+						251,
+						267
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "biz_secure",
+					"name": "BizSecure",
+					"id": 1240499350079295500,
+					"id_str": "1240499350079295488",
+					"indices": [
+						71,
+						82
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/c72ylyE3xe",
+					"expanded_url": "http://ow.ly/9Pa450CsulM",
+					"display_url": "ow.ly/9Pa450CsulM",
+					"indices": [
+						227,
+						250
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1330949340928614400,
+					"id_str": "1330949340928614405",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/Enh7X7gXIAUqLNt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Enh7X7gXIAUqLNt.jpg",
+					"url": "https://t.co/m6tIGZBkTW",
+					"display_url": "pic.twitter.com/m6tIGZBkTW",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1330949343378018304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1330949340928614400,
+					"id_str": "1330949340928614405",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/Enh7X7gXIAUqLNt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Enh7X7gXIAUqLNt.jpg",
+					"url": "https://t.co/m6tIGZBkTW",
+					"display_url": "pic.twitter.com/m6tIGZBkTW",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1330949343378018304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4228,
+			"friends_count": 2365,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3884,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations to the #SDSM2020 Incubator Demo Day winner, BizSecure! @biz_secure provides ID verification, data analytics, &amp; cyber security services. Congrats to all 10 incubator #startups! We're excited for your growth! https://t.co/c72ylyE3xe #StartupSanDiego https://t.co/m6tIGZBkTW"
+	},
+	{
 		"created_at": "Mon Nov 02 19:30:07 +0000 2020",
 		"id": 1323346669421924400,
 		"id_str": "1323346669421924352",
