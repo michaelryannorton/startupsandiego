393455e32d1204896bca4eac7e9e12acb97eea38
Michael Ryan Norton <michael@michaelryannorton.com>
2020-12-28 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3804,
-	"created_at": "Wed Dec 23 00:17:02 +0000 2020",
+	"tweets": 3805,
+	"created_at": "Tue Dec 29 00:17:02 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Mon Dec 28 18:30:17 +0000 2020",
+		"id": 1343625334386782200,
+		"id_str": "1343625334386782213",
+		"full_text": "As we wrap 2020, we are taking time to reflect and extend gratitude to all those who've served #StartupSD! Special shoutout to @VentureConnect for helping us to #connect #educate #inspire and #grow our ecosystem since the beginning 🚀  https://t.co/HGQ3PPe2fi https://t.co/XQDAgaZxAX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSD",
+					"indices": [
+						95,
+						105
+					]
+				},
+				{
+					"text": "connect",
+					"indices": [
+						161,
+						169
+					]
+				},
+				{
+					"text": "educate",
+					"indices": [
+						170,
+						178
+					]
+				},
+				{
+					"text": "inspire",
+					"indices": [
+						179,
+						187
+					]
+				},
+				{
+					"text": "grow",
+					"indices": [
+						192,
+						197
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "VentureConnect",
+					"name": "Eric Otterson",
+					"id": 111437784,
+					"id_str": "111437784",
+					"indices": [
+						127,
+						142
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/HGQ3PPe2fi",
+					"expanded_url": "http://ow.ly/pwGs50CSnlB",
+					"display_url": "ow.ly/pwGs50CSnlB",
+					"indices": [
+						235,
+						258
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1343625332532924400,
+					"id_str": "1343625332532924418",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/EqWEHwDXUAIjQ28.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EqWEHwDXUAIjQ28.jpg",
+					"url": "https://t.co/XQDAgaZxAX",
+					"display_url": "pic.twitter.com/XQDAgaZxAX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1343625334386782213/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1343625332532924400,
+					"id_str": "1343625332532924418",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/EqWEHwDXUAIjQ28.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EqWEHwDXUAIjQ28.jpg",
+					"url": "https://t.co/XQDAgaZxAX",
+					"display_url": "pic.twitter.com/XQDAgaZxAX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1343625334386782213/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4249,
+			"friends_count": 2360,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3906,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "As we wrap 2020, we are taking time to reflect and extend gratitude to all those who've served #StartupSD! Special shoutout to @VentureConnect for helping us to #connect #educate #inspire and #grow our ecosystem since the beginning 🚀  https://t.co/HGQ3PPe2fi https://t.co/XQDAgaZxAX"
+	},
+	{
 		"created_at": "Tue Dec 22 18:30:07 +0000 2020",
 		"id": 1341450962674827300,
 		"id_str": "1341450962674827264",
