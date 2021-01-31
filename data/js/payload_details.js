35fd95ee79d528ed9e9299d3ddc5de3cca83ef74
Michael Ryan Norton <michael@michaelryannorton.com>
2021-01-30 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3812,
-	"created_at": "Sat Jan 30 00:17:02 +0000 2021",
+	"tweets": 3813,
+	"created_at": "Sun Jan 31 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,575 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sat Jan 30 00:57:06 +0000 2021",
+		"id": 1355290355332071400,
+		"id_str": "1355319092287131649",
+		"full_text": "Absolutely thrilled to have @ryankuder taking over the reigns as Chair of @StartupSanDiego with amazing support from new officers @Thinkleap @LSahba &amp; Javier Islas. Welcome new board members and thank you outgoing co-founders of the org @SDPhelan @VentureConnect #startupsd https://t.co/rCbfDlSnyr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startupsd",
+					"indices": [
+						267,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ryankuder",
+					"name": "Ryan Kuder",
+					"id": 6837002,
+					"id_str": "6837002",
+					"indices": [
+						28,
+						38
+					]
+				},
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						74,
+						90
+					]
+				},
+				{
+					"screen_name": "Thinkleap",
+					"name": "Caitlin Wege",
+					"id": 26823079,
+					"id_str": "26823079",
+					"indices": [
+						130,
+						140
+					]
+				},
+				{
+					"screen_name": "LSahba",
+					"name": "Lauree Miller Sahba",
+					"id": 576387800,
+					"id_str": "576387800",
+					"indices": [
+						141,
+						148
+					]
+				},
+				{
+					"screen_name": "SDPhelan",
+					"name": "Phelan Riessen ©",
+					"id": 6538602,
+					"id_str": "6538602",
+					"indices": [
+						241,
+						250
+					]
+				},
+				{
+					"screen_name": "VentureConnect",
+					"name": "Eric Otterson",
+					"id": 111437784,
+					"id_str": "111437784",
+					"indices": [
+						251,
+						266
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rCbfDlSnyr",
+					"expanded_url": "https://twitter.com/thinkleap/status/1355224407682076672",
+					"display_url": "twitter.com/thinkleap/stat…",
+					"indices": [
+						278,
+						301
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19131762,
+			"id_str": "19131762",
+			"name": "Neal Bloom🏃🏻",
+			"screen_name": "NealBloom",
+			"location": "SD, CA",
+			"description": "Entrepreneur, investor, community builder, friend. Podcasting @tacosandtechpod 🌮 🤖 🎙 Blogging @freshbrewedtech @OCFreshSqueezed",
+			"url": "https://t.co/sbmE5iLnTA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sbmE5iLnTA",
+							"expanded_url": "http://freshbrewedtech.com",
+							"display_url": "freshbrewedtech.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2599,
+			"friends_count": 293,
+			"listed_count": 193,
+			"created_at": "Sun Jan 18 01:31:26 +0000 2009",
+			"favourites_count": 18717,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15203,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1326571209660624899/8sor3Yhe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326571209660624899/8sor3Yhe_normal.jpg",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1355224407682076700,
+		"quoted_status_id_str": "1355224407682076672",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rCbfDlSnyr",
+			"expanded": "https://twitter.com/thinkleap/status/1355224407682076672",
+			"display": "twitter.com/thinkleap/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 29 18:40:52 +0000 2021",
+			"id": 1355224407682076700,
+			"id_str": "1355224407682076672",
+			"full_text": "@StartupSanDiego has announced a new board for 2021🚀 Honored to serve the startup community amongst these amazing leaders. @ryankuder @LSahba @jrtolver16\n @CarrieMSawyer @NealBloom @lexaRaeN @mkrenn @mrusk @FelenaHanson @SDPhelan #sdtechos #startups #SanDiego @fredmgrier https://t.co/oVlSq95gyM",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				271
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "sdtechos",
+						"indices": [
+							230,
+							239
+						]
+					},
+					{
+						"text": "startups",
+						"indices": [
+							240,
+							249
+						]
+					},
+					{
+						"text": "SanDiego",
+						"indices": [
+							250,
+							259
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "StartupSanDiego",
+						"name": "Startup San Diego",
+						"id": 3222944604,
+						"id_str": "3222944604",
+						"indices": [
+							0,
+							16
+						]
+					},
+					{
+						"screen_name": "ryankuder",
+						"name": "Ryan Kuder",
+						"id": 6837002,
+						"id_str": "6837002",
+						"indices": [
+							123,
+							133
+						]
+					},
+					{
+						"screen_name": "LSahba",
+						"name": "Lauree Miller Sahba",
+						"id": 576387800,
+						"id_str": "576387800",
+						"indices": [
+							134,
+							141
+						]
+					},
+					{
+						"screen_name": "jrtolver16",
+						"name": "JR",
+						"id": 725389453724934100,
+						"id_str": "725389453724934145",
+						"indices": [
+							142,
+							153
+						]
+					},
+					{
+						"screen_name": "CarrieMSawyer",
+						"name": "Carrie M. Sawyer",
+						"id": 12962832,
+						"id_str": "12962832",
+						"indices": [
+							155,
+							169
+						]
+					},
+					{
+						"screen_name": "NealBloom",
+						"name": "Neal Bloom🏃🏻",
+						"id": 19131762,
+						"id_str": "19131762",
+						"indices": [
+							170,
+							180
+						]
+					},
+					{
+						"screen_name": "lexaRaeN",
+						"name": "Alexa-Rae Navarro",
+						"id": 1666344193,
+						"id_str": "1666344193",
+						"indices": [
+							181,
+							190
+						]
+					},
+					{
+						"screen_name": "mkrenn",
+						"name": "Mike Krenn",
+						"id": 19940438,
+						"id_str": "19940438",
+						"indices": [
+							191,
+							198
+						]
+					},
+					{
+						"screen_name": "mrusk",
+						"name": "Mysty Rusk",
+						"id": 20454953,
+						"id_str": "20454953",
+						"indices": [
+							199,
+							205
+						]
+					},
+					{
+						"screen_name": "FelenaHanson",
+						"name": "Felena Hanson",
+						"id": 20824677,
+						"id_str": "20824677",
+						"indices": [
+							206,
+							219
+						]
+					},
+					{
+						"screen_name": "SDPhelan",
+						"name": "Phelan Riessen ©",
+						"id": 6538602,
+						"id_str": "6538602",
+						"indices": [
+							220,
+							229
+						]
+					},
+					{
+						"screen_name": "fredmgrier",
+						"name": "Fred Grier",
+						"id": 4775662891,
+						"id_str": "4775662891",
+						"indices": [
+							260,
+							271
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1355224288261886000,
+						"id_str": "1355224288261885952",
+						"indices": [
+							272,
+							295
+						],
+						"media_url": "http://pbs.twimg.com/media/Es65T1TU0AAWHwD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Es65T1TU0AAWHwD.jpg",
+						"url": "https://t.co/oVlSq95gyM",
+						"display_url": "pic.twitter.com/oVlSq95gyM",
+						"expanded_url": "https://twitter.com/Thinkleap/status/1355224407682076672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1355224288261886000,
+						"id_str": "1355224288261885952",
+						"indices": [
+							272,
+							295
+						],
+						"media_url": "http://pbs.twimg.com/media/Es65T1TU0AAWHwD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Es65T1TU0AAWHwD.jpg",
+						"url": "https://t.co/oVlSq95gyM",
+						"display_url": "pic.twitter.com/oVlSq95gyM",
+						"expanded_url": "https://twitter.com/Thinkleap/status/1355224407682076672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1355224288320622600,
+						"id_str": "1355224288320622594",
+						"indices": [
+							272,
+							295
+						],
+						"media_url": "http://pbs.twimg.com/media/Es65T1hVEAInPon.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Es65T1hVEAInPon.jpg",
+						"url": "https://t.co/oVlSq95gyM",
+						"display_url": "pic.twitter.com/oVlSq95gyM",
+						"expanded_url": "https://twitter.com/Thinkleap/status/1355224407682076672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 1080,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1355224351772037000,
+						"id_str": "1355224351772037121",
+						"indices": [
+							272,
+							295
+						],
+						"media_url": "http://pbs.twimg.com/media/Es65Xh5U0AEnMD8.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Es65Xh5U0AEnMD8.jpg",
+						"url": "https://t.co/oVlSq95gyM",
+						"display_url": "pic.twitter.com/oVlSq95gyM",
+						"expanded_url": "https://twitter.com/Thinkleap/status/1355224407682076672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 291,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 165,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 370,
+								"h": 1524,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": 3222944604,
+			"in_reply_to_user_id_str": "3222944604",
+			"in_reply_to_screen_name": "StartupSanDiego",
+			"user": {
+				"id": 26823079,
+				"id_str": "26823079",
+				"name": "Caitlin Wege",
+				"screen_name": "Thinkleap",
+				"location": "San Diego, CA",
+				"description": "Activist + Investor / President @tcasandiego / Partner, MooDoos / Board Member @startupsandiego @neuralace @pumpspotting / Chairperson@ CPL-Glasshouse Pharma",
+				"url": "https://t.co/flwrdspsL4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/flwrdspsL4",
+								"expanded_url": "http://www.moodooslp.com",
+								"display_url": "moodooslp.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 373,
+				"friends_count": 270,
+				"listed_count": 4,
+				"created_at": "Thu Mar 26 19:13:10 +0000 2009",
+				"favourites_count": 1869,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 329,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1225131446676803585/oISoAwya_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1225131446676803585/oISoAwya_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26823079/1582840020",
+				"profile_link_color": "2FC2EF",
+				"profile_sidebar_border_color": "181A1E",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 19,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Absolutely thrilled to have @ryankuder taking over the reigns as Chair of @StartupSanDiego with amazing support from new officers @Thinkleap @LSahba &amp; Javier Islas. Welcome new board members and thank you outgoing co-founders of the org @SDPhelan @VentureConnect #startupsd https://t.co/rCbfDlSnyr"
+	},
+	{
 		"created_at": "Fri Jan 29 19:00:56 +0000 2021",
 		"id": 1355229458295353300,
 		"id_str": "1355229458295353351",
