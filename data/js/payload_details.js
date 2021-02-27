66b39d630d53dffa9a000b1f7205a0363e3ca40b
Michael Ryan Norton <michael@michaelryannorton.com>
2021-02-26 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3820,
-	"created_at": "Wed Feb 24 00:17:02 +0000 2021",
+	"tweets": 3821,
+	"created_at": "Sat Feb 27 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 26 19:00:33 +0000 2021",
+		"id": 1365376224214868000,
+		"id_str": "1365376224214867978",
+		"full_text": "👋 Come to our 1st Mondays virtual event on March. 1st to meet Kim Walpole, Co-Founder and CEO of @trialsAI and network with the San Diego Startup ecosystem. 🚀 Grab your ticket! Link in bio or here: https://t.co/XN19yR8zpI https://t.co/4Y7NBR7WJS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			221
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "trialsAI",
+					"name": "trials.ai",
+					"id": 3406106890,
+					"id_str": "3406106890",
+					"indices": [
+						97,
+						106
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XN19yR8zpI",
+					"expanded_url": "https://www.runtheworld.today/app/invitation/15091",
+					"display_url": "runtheworld.today/app/invitation…",
+					"indices": [
+						198,
+						221
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1365376222004469800,
+					"id_str": "1365376222004469765",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/EvLKcJlXYAUKe_b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvLKcJlXYAUKe_b.jpg",
+					"url": "https://t.co/4Y7NBR7WJS",
+					"display_url": "pic.twitter.com/4Y7NBR7WJS",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1365376224214867978/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1365376222004469800,
+					"id_str": "1365376222004469765",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/EvLKcJlXYAUKe_b.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvLKcJlXYAUKe_b.jpg",
+					"url": "https://t.co/4Y7NBR7WJS",
+					"display_url": "pic.twitter.com/4Y7NBR7WJS",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1365376224214867978/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4320,
+			"friends_count": 2339,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2133,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3921,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "👋 Come to our 1st Mondays virtual event on March. 1st to meet Kim Walpole, Co-Founder and CEO of @trialsAI and network with the San Diego Startup ecosystem. 🚀 Grab your ticket! Link in bio or here: https://t.co/XN19yR8zpI https://t.co/4Y7NBR7WJS"
+	},
+	{
 		"created_at": "Tue Feb 23 19:05:07 +0000 2021",
 		"id": 1364290209240805400,
 		"id_str": "1364290209240805377",
