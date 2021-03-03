b67d2f96dedbb911b469e3771db0ef53be410ebf
Michael Ryan Norton <michael@michaelryannorton.com>
2021-03-02 16:17:02 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3821,
-	"created_at": "Sat Feb 27 00:17:02 +0000 2021",
+	"tweets": 3822,
+	"created_at": "Wed Mar 03 00:17:02 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,237 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Tue Mar 02 06:00:07 +0000 2021",
+		"id": 1366629370241122300,
+		"id_str": "1366629370241122307",
+		"full_text": "What does it take to grow a San Diego Venture Group Cool Company? Come to 1st Mondays Startup Stories with Kim Walpole and hear her story of https://t.co/V6kQjSi1QZ. \n\n🚀 Grab your ticket TODAY! Link in bio or here: https://t.co/RSniKqtSgL https://t.co/Eak5GIFOR2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/V6kQjSi1QZ",
+					"expanded_url": "http://ow.ly/cB6L50DM5Fm",
+					"display_url": "ow.ly/cB6L50DM5Fm",
+					"indices": [
+						141,
+						164
+					]
+				},
+				{
+					"url": "https://t.co/RSniKqtSgL",
+					"expanded_url": "http://ow.ly/P71R50DM5Fl",
+					"display_url": "ow.ly/P71R50DM5Fl",
+					"indices": [
+						215,
+						238
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1366628097974554600,
+					"id_str": "1366628097974554627",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1366628097974554627/pu/img/yW2X6IBamOrudyKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1366628097974554627/pu/img/yW2X6IBamOrudyKN.jpg",
+					"url": "https://t.co/Eak5GIFOR2",
+					"display_url": "pic.twitter.com/Eak5GIFOR2",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1366629370241122307/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1366628097974554600,
+					"id_str": "1366628097974554627",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1366628097974554627/pu/img/yW2X6IBamOrudyKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1366628097974554627/pu/img/yW2X6IBamOrudyKN.jpg",
+					"url": "https://t.co/Eak5GIFOR2",
+					"display_url": "pic.twitter.com/Eak5GIFOR2",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1366629370241122307/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 5000,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366628097974554627/pu/vid/320x320/HfTUsoq3TeChKssm.mp4?tag=10"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366628097974554627/pu/vid/720x720/JZ3twY6CtjDIZqZk.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1366628097974554627/pu/vid/480x480/sYXcVuODf9LtwoW4.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1366628097974554627/pu/pl/T2F3Tdfa69Q8tNcr.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4324,
+			"friends_count": 2340,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2133,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3922,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What does it take to grow a San Diego Venture Group Cool Company? Come to 1st Mondays Startup Stories with Kim Walpole and hear her story of https://t.co/V6kQjSi1QZ. \n\n🚀 Grab your ticket TODAY! Link in bio or here: https://t.co/RSniKqtSgL https://t.co/Eak5GIFOR2"
+	}
+]
