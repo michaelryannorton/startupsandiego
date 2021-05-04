be0eed32a57b8ea25fdbb1a5065a1b63e4a0c4c6
Michael Ryan Norton <michael@michaelryannorton.com>
2021-05-03 17:17:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3847,
-	"created_at": "Sat May 01 00:17:02 +0000 2021",
+	"tweets": 3848,
+	"created_at": "Tue May 04 00:17:02 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_05.js
@@ -0,0 +1,251 @@
+Grailbird.data.tweets_2021_05 = 
+[
+	{
+		"created_at": "Mon May 03 17:10:02 +0000 2021",
+		"id": 1389266010361016300,
+		"id_str": "1389266010361016320",
+		"full_text": "Join us for Tonights 1st Mondays, we'll be featuring Jamar Williams, CEO and Founder of PromoDrone! Learn about the future of drone #marketing, #startup stories, and network with some fellow San Diego #entrepreneurs. \n\nRegister: https://t.co/piA6uQZWBA https://t.co/NGxHp4NBhf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "marketing",
+					"indices": [
+						132,
+						142
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						144,
+						152
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						201,
+						215
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/piA6uQZWBA",
+					"expanded_url": "http://ow.ly/RWLp50EBlUG",
+					"display_url": "ow.ly/RWLp50EBlUG",
+					"indices": [
+						229,
+						252
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1389264763104989200,
+					"id_str": "1389264763104989185",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1389264763104989185/pu/img/-6GUycwYABIJjBnU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1389264763104989185/pu/img/-6GUycwYABIJjBnU.jpg",
+					"url": "https://t.co/NGxHp4NBhf",
+					"display_url": "pic.twitter.com/NGxHp4NBhf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1389266010361016320/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1389264763104989200,
+					"id_str": "1389264763104989185",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1389264763104989185/pu/img/-6GUycwYABIJjBnU.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1389264763104989185/pu/img/-6GUycwYABIJjBnU.jpg",
+					"url": "https://t.co/NGxHp4NBhf",
+					"display_url": "pic.twitter.com/NGxHp4NBhf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1389266010361016320/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 26767,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1389264763104989185/pu/vid/540x540/_8sK_qfYTgzO4Qxe.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1389264763104989185/pu/vid/720x720/_A-I2pNOLIYpk9pM.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1389264763104989185/pu/pl/5EFkYffUr87FyvwB.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1389264763104989185/pu/vid/320x320/J6Rqi7-qiJeef4MQ.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM20 The full month of October: https://t.co/apBzLwX7RV #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/apBzLwX7RV",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								80,
+								103
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4365,
+			"friends_count": 2332,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2134,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3950,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1599627051",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Join us for Tonights 1st Mondays, we'll be featuring Jamar Williams, CEO and Founder of PromoDrone! Learn about the future of drone #marketing, #startup stories, and network with some fellow San Diego #entrepreneurs. \n\nRegister: https://t.co/piA6uQZWBA https://t.co/NGxHp4NBhf"
+	}
+]
