578d154a646b983a334c97272fa4b727c614520a
Michael Ryan Norton <michael@michaelryannorton.com>
2021-06-18 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3944,
-	"created_at": "Wed Jun 09 00:17:02 +0000 2021",
+	"tweets": 3945,
+	"created_at": "Sat Jun 19 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 18 17:05:04 +0000 2021",
+		"id": 1405934599113478100,
+		"id_str": "1405934599113478146",
+		"full_text": "#Convergence2021 recordings are now available! Rewatch your favorite sessions or see the workshops you missed out on.\n\nMake sure to replay our Inter-University pitch competition! Featuring the winner, @jeweledapp!\n\nRecordings at: https://t.co/e4xMsoQYNN https://t.co/Q3M9g2nkLJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Convergence2021",
+					"indices": [
+						0,
+						16
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Jeweledapp",
+					"name": "Jeweled",
+					"id": 1375521117478211600,
+					"id_str": "1375521117478211588",
+					"indices": [
+						201,
+						212
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/e4xMsoQYNN",
+					"expanded_url": "http://startupsd.org/convergence/",
+					"display_url": "startupsd.org/convergence/",
+					"indices": [
+						230,
+						253
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1405934597238558700,
+					"id_str": "1405934597238558723",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/E4LiETXWEAM0D4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4LiETXWEAM0D4c.jpg",
+					"url": "https://t.co/Q3M9g2nkLJ",
+					"display_url": "pic.twitter.com/Q3M9g2nkLJ",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1405934599113478146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1405934597238558700,
+					"id_str": "1405934597238558723",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/E4LiETXWEAM0D4c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4LiETXWEAM0D4c.jpg",
+					"url": "https://t.co/Q3M9g2nkLJ",
+					"display_url": "pic.twitter.com/Q3M9g2nkLJ",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1405934599113478146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #Convergence2021 from May 24 - 28 https://t.co/jPPXmjGszk #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/jPPXmjGszk",
+							"expanded_url": "http://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								79,
+								102
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4366,
+			"friends_count": 2335,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2209,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4057,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1620629231",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Convergence2021 recordings are now available! Rewatch your favorite sessions or see the workshops you missed out on.\n\nMake sure to replay our Inter-University pitch competition! Featuring the winner, @jeweledapp!\n\nRecordings at: https://t.co/e4xMsoQYNN https://t.co/Q3M9g2nkLJ"
+	},
+	{
 		"created_at": "Tue Jun 08 16:10:03 +0000 2021",
 		"id": 1402296877509382100,
 		"id_str": "1402296877509382150",
