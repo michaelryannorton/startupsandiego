b39c96cc5a75a894e0d60d1965871932c93252a7
Michael Ryan Norton <michael@michaelryannorton.com>
2021-08-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3979,
-	"created_at": "Sat Jul 31 00:17:02 +0000 2021",
+	"tweets": 3980,
+	"created_at": "Tue Aug 03 00:17:02 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,206 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Mon Aug 02 17:51:01 +0000 2021",
+		"id": 1422253617453564000,
+		"id_str": "1422253617453563906",
+		"full_text": "Happening TONIGHT, #StartSD 1st Mondays will be featuring The Bright App teams CEO, CTO, and COO. Learn from these founders and how they grew their app and continue to empower fitness instructors. Learn more and register: https://t.co/9woEGV8N8E https://t.co/NKpk0TGnHb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartSD",
+					"indices": [
+						19,
+						27
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9woEGV8N8E",
+					"expanded_url": "http://ow.ly/BKcL50FHhIo",
+					"display_url": "ow.ly/BKcL50FHhIo",
+					"indices": [
+						222,
+						245
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1422253614886703000,
+					"id_str": "1422253614886703111",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/E7zcIJfWUAcOQRY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7zcIJfWUAcOQRY.jpg",
+					"url": "https://t.co/NKpk0TGnHb",
+					"display_url": "pic.twitter.com/NKpk0TGnHb",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1422253617453563906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1422253614886703000,
+					"id_str": "1422253614886703111",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/E7zcIJfWUAcOQRY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7zcIJfWUAcOQRY.jpg",
+					"url": "https://t.co/NKpk0TGnHb",
+					"display_url": "pic.twitter.com/NKpk0TGnHb",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1422253617453563906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #SDSM2021 from October 1 - 29  https://t.co/jPPXmjGszk #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/jPPXmjGszk",
+							"expanded_url": "http://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								76,
+								99
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4421,
+			"friends_count": 2333,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4102,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1625764342",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Happening TONIGHT, #StartSD 1st Mondays will be featuring The Bright App teams CEO, CTO, and COO. Learn from these founders and how they grew their app and continue to empower fitness instructors. Learn more and register: https://t.co/9woEGV8N8E https://t.co/NKpk0TGnHb"
+	}
+]
