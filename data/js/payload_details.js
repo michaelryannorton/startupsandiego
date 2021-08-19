a0e28063c25f066925f5012bbf8b87eb10590c20
Michael Ryan Norton <michael@michaelryannorton.com>
2021-08-18 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3982,
-	"created_at": "Wed Aug 18 00:17:02 +0000 2021",
+	"tweets": 3983,
+	"created_at": "Thu Aug 19 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 18 17:00:52 +0000 2021",
+		"id": 1428039203502399500,
+		"id_str": "1428039203502399489",
+		"full_text": "@founders_live is an unforgettable happy hour competition where up to five handpicked companies take the hot seat, with only 99 seconds to pitch their company. Join the crowd and Q&amp;A session. Find out more and register: https://t.co/tQFFgbGD10 https://t.co/e72MlP5DI2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "founders_live",
+					"name": "Founders Live",
+					"id": 751082887496208400,
+					"id_str": "751082887496208384",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/tQFFgbGD10",
+					"expanded_url": "http://ow.ly/Z5gR50FSHZT",
+					"display_url": "ow.ly/Z5gR50FSHZT",
+					"indices": [
+						224,
+						247
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1428039201384276000,
+					"id_str": "1428039201384275973",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/E9FqFmvXoAU1-Sl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9FqFmvXoAU1-Sl.jpg",
+					"url": "https://t.co/e72MlP5DI2",
+					"display_url": "pic.twitter.com/e72MlP5DI2",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1428039203502399489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 330,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 583,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1410,
+							"h": 685,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1428039201384276000,
+					"id_str": "1428039201384275973",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/E9FqFmvXoAU1-Sl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9FqFmvXoAU1-Sl.jpg",
+					"url": "https://t.co/e72MlP5DI2",
+					"display_url": "pic.twitter.com/e72MlP5DI2",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1428039203502399489/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 330,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 583,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1410,
+							"h": 685,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 751082887496208400,
+		"in_reply_to_user_id_str": "751082887496208384",
+		"in_reply_to_screen_name": "founders_live",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us for #SDSM2021 happening October 1 - 29, 2021 - Get tickets: https://t.co/pKuEWWhRPI #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pKuEWWhRPI",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4431,
+			"friends_count": 2334,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2244,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4106,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@founders_live is an unforgettable happy hour competition where up to five handpicked companies take the hot seat, with only 99 seconds to pitch their company. Join the crowd and Q&amp;A session. Find out more and register: https://t.co/tQFFgbGD10 https://t.co/e72MlP5DI2"
+	},
+	{
 		"created_at": "Tue Aug 17 17:00:25 +0000 2021",
 		"id": 1427676700557652000,
 		"id_str": "1427676700557651971",
