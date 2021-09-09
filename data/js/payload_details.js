0e6b8391935852f66877683f36f4be6baddf344a
Michael Ryan Norton <michael@michaelryannorton.com>
2021-09-08 17:17:03 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 3988,
-	"created_at": "Wed Sep 08 00:17:02 +0000 2021",
+	"tweets": 3990,
+	"created_at": "Thu Sep 09 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,454 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 08 17:00:48 +0000 2021",
+		"id": 1435649334134063000,
+		"id_str": "1435649334134063105",
+		"full_text": "For this years #SDSM2021 we'll be having our first ever Startup Month Awards! We're taking nominations for #Investor of the year, Startup Champion, leaders and more! Make sure to nominate who you think is the best. Nominate here: https://t.co/RrgzqAqXdE\n\n#SDSM2021 #StartSD https://t.co/SDkjYOJBbT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSM2021",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"text": "Investor",
+					"indices": [
+						107,
+						116
+					]
+				},
+				{
+					"text": "SDSM2021",
+					"indices": [
+						255,
+						264
+					]
+				},
+				{
+					"text": "StartSD",
+					"indices": [
+						265,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RrgzqAqXdE",
+					"expanded_url": "http://ow.ly/aO2g50G5WFv",
+					"display_url": "ow.ly/aO2g50G5WFv",
+					"indices": [
+						230,
+						253
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1435649331860807700,
+					"id_str": "1435649331860807680",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E-xzdf-XIAAXEqT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-xzdf-XIAAXEqT.jpg",
+					"url": "https://t.co/SDkjYOJBbT",
+					"display_url": "pic.twitter.com/SDkjYOJBbT",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1435649334134063105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1435649331860807700,
+					"id_str": "1435649331860807680",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/E-xzdf-XIAAXEqT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-xzdf-XIAAXEqT.jpg",
+					"url": "https://t.co/SDkjYOJBbT",
+					"display_url": "pic.twitter.com/SDkjYOJBbT",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1435649334134063105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us for #SDSM2021 happening October 1 - 29, 2021 - Get tickets: https://t.co/pKuEWWhRPI #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pKuEWWhRPI",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4443,
+			"friends_count": 2333,
+			"listed_count": 0,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2248,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For this years #SDSM2021 we'll be having our first ever Startup Month Awards! We're taking nominations for #Investor of the year, Startup Champion, leaders and more! Make sure to nominate who you think is the best. Nominate here: https://t.co/RrgzqAqXdE\n\n#SDSM2021 #StartSD https://t.co/SDkjYOJBbT"
+	},
+	{
+		"created_at": "Wed Sep 08 16:15:04 +0000 2021",
+		"id": 1435637823831527400,
+		"id_str": "1435637823831527425",
+		"full_text": "All through October 4-8th we'll be having #SDSM Content Week! Whether it's marketing and sales, data and customer or people and design, choose the track that's right for you. Get your tickets today! https://t.co/a53cGPHufT\n\n#SDSM2021 #StartupSD #SDSMcontentweek https://t.co/At6TRCjQbY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSM",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"text": "SDSM2021",
+					"indices": [
+						224,
+						233
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						234,
+						244
+					]
+				},
+				{
+					"text": "SDSMcontentweek",
+					"indices": [
+						245,
+						261
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/a53cGPHufT",
+					"expanded_url": "https://www.sandiegostartupweek.com/",
+					"display_url": "sandiegostartupweek.com",
+					"indices": [
+						199,
+						222
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1435637821885329400,
+					"id_str": "1435637821885329410",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/E-xo_h-XMAI-2UL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-xo_h-XMAI-2UL.jpg",
+					"url": "https://t.co/At6TRCjQbY",
+					"display_url": "pic.twitter.com/At6TRCjQbY",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1435637823831527425/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1435637821885329400,
+					"id_str": "1435637821885329410",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/E-xo_h-XMAI-2UL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-xo_h-XMAI-2UL.jpg",
+					"url": "https://t.co/At6TRCjQbY",
+					"display_url": "pic.twitter.com/At6TRCjQbY",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1435637823831527425/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us for #SDSM2021 happening October 1 - 29, 2021 - Get tickets: https://t.co/pKuEWWhRPI #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pKuEWWhRPI",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4443,
+			"friends_count": 2333,
+			"listed_count": 0,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2248,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4115,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "All through October 4-8th we'll be having #SDSM Content Week! Whether it's marketing and sales, data and customer or people and design, choose the track that's right for you. Get your tickets today! https://t.co/a53cGPHufT\n\n#SDSM2021 #StartupSD #SDSMcontentweek https://t.co/At6TRCjQbY"
+	},
+	{
 		"created_at": "Tue Sep 07 17:15:03 +0000 2021",
 		"id": 1435290528820629500,
 		"id_str": "1435290528820629511",
