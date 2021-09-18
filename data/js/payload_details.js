24594c1a4d0b2f7d5d4407fa6b727aea3835acd4
Michael Ryan Norton <michael@michaelryannorton.com>
2021-09-17 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4000,
-	"created_at": "Fri Sep 17 00:17:02 +0000 2021",
+	"tweets": 4001,
+	"created_at": "Sat Sep 18 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 17 17:10:03 +0000 2021",
+		"id": 1438913152461254700,
+		"id_str": "1438913152461254657",
+		"full_text": "The Startup Champion of The Year honors an individual who values #community collaboration &amp; the coalescing of #entrepreneurs, academic, research, capital, government, corporate, &amp; NGOs, to create a frictionless #startup environment. To nominate: https://t.co/6DxvdpKAzX\n\n#SDSM2021 https://t.co/TzFimtfjBf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			288
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "community",
+					"indices": [
+						65,
+						75
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						114,
+						128
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						219,
+						227
+					]
+				},
+				{
+					"text": "SDSM2021",
+					"indices": [
+						279,
+						288
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6DxvdpKAzX",
+					"expanded_url": "https://airtable.com/shryjqVdmblYHryHA",
+					"display_url": "airtable.com/shryjqVdmblYHr…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1438913149902758000,
+					"id_str": "1438913149902757895",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/E_gL4tqXoAccx3h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_gL4tqXoAccx3h.jpg",
+					"url": "https://t.co/TzFimtfjBf",
+					"display_url": "pic.twitter.com/TzFimtfjBf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1438913152461254657/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438913149902758000,
+					"id_str": "1438913149902757895",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/E_gL4tqXoAccx3h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_gL4tqXoAccx3h.jpg",
+					"url": "https://t.co/TzFimtfjBf",
+					"display_url": "pic.twitter.com/TzFimtfjBf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1438913152461254657/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us for #SDSM2021 happening October 1 - 29, 2021 - Get tickets: https://t.co/pKuEWWhRPI #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pKuEWWhRPI",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4441,
+			"friends_count": 2331,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2255,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4127,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Startup Champion of The Year honors an individual who values #community collaboration &amp; the coalescing of #entrepreneurs, academic, research, capital, government, corporate, &amp; NGOs, to create a frictionless #startup environment. To nominate: https://t.co/6DxvdpKAzX\n\n#SDSM2021 https://t.co/TzFimtfjBf"
+	},
+	{
 		"created_at": "Thu Sep 16 18:00:23 +0000 2021",
 		"id": 1438563429162303500,
 		"id_str": "1438563429162303489",
