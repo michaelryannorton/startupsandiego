e0961e57d305e091156dd0b8b886f1eb0a128125
Michael Ryan Norton <michael@michaelryannorton.com>
2021-10-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4020,
-	"created_at": "Sat Oct 02 00:17:02 +0000 2021",
+	"tweets": 4021,
+	"created_at": "Sun Oct 03 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,298 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 02 03:22:48 +0000 2021",
+		"id": 1444052896367071200,
+		"id_str": "1444140782508658690",
+		"full_text": "Can't wait to check this out next week and hear my boss @KatieFellenz speak about all things #marketing! 🚀 @StartupSanDiego https://t.co/UZAndbSR39",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "marketing",
+					"indices": [
+						93,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KatieFellenz",
+					"name": "Katie Fellenz",
+					"id": 2867750514,
+					"id_str": "2867750514",
+					"indices": [
+						56,
+						69
+					]
+				},
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						107,
+						123
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/UZAndbSR39",
+					"expanded_url": "https://twitter.com/trustandwill/status/1441106898086875137",
+					"display_url": "twitter.com/trustandwill/s…",
+					"indices": [
+						124,
+						147
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15923740,
+			"id_str": "15923740",
+			"name": "Danielle Nuzzo",
+			"screen_name": "DailyNuzzo",
+			"location": "San Diego",
+			"description": "Mom 👶  🐶, Wife ❤️️ and Comms for @trustandwill. Views are my own.",
+			"url": "https://t.co/csrvaTOTeu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/csrvaTOTeu",
+							"expanded_url": "https://www.linkedin.com/in/daniellenuzzo/",
+							"display_url": "linkedin.com/in/daniellenuz…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 979,
+			"friends_count": 1627,
+			"listed_count": 33,
+			"created_at": "Wed Aug 20 21:42:53 +0000 2008",
+			"favourites_count": 1100,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3905,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/800882385461989376/eiwNsAyW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/800882385461989376/eiwNsAyW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15923740/1609798373",
+			"profile_link_color": "0E72F5",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "0FFFAB",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1441106898086875100,
+		"quoted_status_id_str": "1441106898086875137",
+		"quoted_status_permalink": {
+			"url": "https://t.co/UZAndbSR39",
+			"expanded": "https://twitter.com/trustandwill/status/1441106898086875137",
+			"display": "twitter.com/trustandwill/s…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Sep 23 18:27:13 +0000 2021",
+			"id": 1441106898086875100,
+			"id_str": "1441106898086875137",
+			"full_text": "We're excited for @StartupSanDiego Month!🚀\nJoin our very own Head of Marketing, @KatieFellenz, on Oct 5th for discussions on the latest happenings in San Diego tech + the #startup community! #SDSM2021 https://t.co/FcVjDQ4doq",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				200
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "startup",
+						"indices": [
+							171,
+							179
+						]
+					},
+					{
+						"text": "SDSM2021",
+						"indices": [
+							191,
+							200
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "StartupSanDiego",
+						"name": "Startup San Diego",
+						"id": 3222944604,
+						"id_str": "3222944604",
+						"indices": [
+							18,
+							34
+						]
+					},
+					{
+						"screen_name": "KatieFellenz",
+						"name": "Katie Fellenz",
+						"id": 2867750514,
+						"id_str": "2867750514",
+						"indices": [
+							80,
+							93
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/FcVjDQ4doq",
+						"expanded_url": "https://twitter.com/StartupSanDiego/status/1440736114474582030",
+						"display_url": "twitter.com/StartupSanDieg…",
+						"indices": [
+							201,
+							224
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 895876199372562400,
+				"id_str": "895876199372562433",
+				"name": "Trust & Will",
+				"screen_name": "trustandwill",
+				"location": "San Diego, CA",
+				"description": "Modern estate planning for every American. Our mission is to provide families a better way to plan for the future 💙",
+				"url": "https://t.co/aHz05jzhZ2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/aHz05jzhZ2",
+								"expanded_url": "http://trustandwill.com",
+								"display_url": "trustandwill.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1552,
+				"friends_count": 12,
+				"listed_count": 25,
+				"created_at": "Fri Aug 11 05:15:05 +0000 2017",
+				"favourites_count": 5100,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 917,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1371828965275435008/uDpf7p-o_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371828965275435008/uDpf7p-o_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/895876199372562433/1605559754",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1440736114474582000,
+			"quoted_status_id_str": "1440736114474582030",
+			"quoted_status_permalink": {
+				"url": "https://t.co/FcVjDQ4doq",
+				"expanded": "https://twitter.com/StartupSanDiego/status/1440736114474582030",
+				"display": "twitter.com/StartupSanDieg…"
+			},
+			"retweet_count": 4,
+			"favorite_count": 8,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Can't wait to check this out next week and hear my boss @KatieFellenz speak about all things #marketing! 🚀 @StartupSanDiego https://t.co/UZAndbSR39"
+	},
+	{
 		"created_at": "Fri Oct 01 19:52:39 +0000 2021",
 		"id": 1444027502297591800,
 		"id_str": "1444027502297591809",
