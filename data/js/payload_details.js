ab4b6c73406c53416b22032e3f6865c1c4a4ff55
Michael Ryan Norton <michael@michaelryannorton.com>
2021-11-17 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4116,
-	"created_at": "Wed Nov 17 00:17:02 +0000 2021",
+	"tweets": 4117,
+	"created_at": "Thu Nov 18 00:17:02 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 17 21:27:17 +0000 2021",
+		"id": 1461083547373117400,
+		"id_str": "1461083547373117441",
+		"full_text": "Huge shoutout to one of our Startup San Diego Investors, @trustandwill for being selected as part of the @AARP's AgeTech Collaborative! This local SD #startup is definitely leading the way in the Longevity Economy.\n\nRead more:https://t.co/5GhKzQU1BO https://t.co/2jTFKkmOcS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			249
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						150,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "trustandwill",
+					"name": "Trust & Will",
+					"id": 895876199372562400,
+					"id_str": "895876199372562433",
+					"indices": [
+						57,
+						70
+					]
+				},
+				{
+					"screen_name": "AARP",
+					"name": "AARP",
+					"id": 80628196,
+					"id_str": "80628196",
+					"indices": [
+						105,
+						110
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5GhKzQU1BO",
+					"expanded_url": "https://www.aarp.org/home-family/personal-technology/info-2021/age-tech-collaborative.html",
+					"display_url": "aarp.org/home-family/pe…",
+					"indices": [
+						226,
+						249
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1461083545678622700,
+					"id_str": "1461083545678622722",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/FEbPvl9XsAIIDjH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEbPvl9XsAIIDjH.jpg",
+					"url": "https://t.co/2jTFKkmOcS",
+					"display_url": "pic.twitter.com/2jTFKkmOcS",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1461083547373117441/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 391,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1140,
+							"h": 655,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1140,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1461083545678622700,
+					"id_str": "1461083545678622722",
+					"indices": [
+						250,
+						273
+					],
+					"media_url": "http://pbs.twimg.com/media/FEbPvl9XsAIIDjH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FEbPvl9XsAIIDjH.jpg",
+					"url": "https://t.co/2jTFKkmOcS",
+					"display_url": "pic.twitter.com/2jTFKkmOcS",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1461083547373117441/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 391,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1140,
+							"h": 655,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1140,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us for #SDSM2021 happening October 1 - 29, 2021 - Get tickets: https://t.co/pKuEWWhRPI #StartupSD",
+			"url": "https://t.co/7LZNZT3tWU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7LZNZT3tWU",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pKuEWWhRPI",
+							"expanded_url": "http://sandiegostartupweek.com",
+							"display_url": "sandiegostartupweek.com",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4498,
+			"friends_count": 2324,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2330,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4267,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Huge shoutout to one of our Startup San Diego Investors, @trustandwill for being selected as part of the @AARP's AgeTech Collaborative! This local SD #startup is definitely leading the way in the Longevity Economy.\n\nRead more:https://t.co/5GhKzQU1BO https://t.co/2jTFKkmOcS"
+	},
+	{
 		"created_at": "Tue Nov 16 19:52:16 +0000 2021",
 		"id": 1460697243892265000,
 		"id_str": "1460697243892264962",
