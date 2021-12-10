330e5fe183ee070fb911208f0fe380cfa4216b57
Michael Ryan Norton <michael@michaelryannorton.com>
2021-12-09 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4126,
-	"created_at": "Wed Dec 01 00:17:02 +0000 2021",
+	"tweets": 4127,
+	"created_at": "Fri Dec 10 00:17:02 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Thu Dec 09 19:49:26 +0000 2021",
+		"id": 1469031452968956000,
+		"id_str": "1469031452968955908",
+		"full_text": "Last week we organized our #GivingTuesday campaign to raise funds for our 2022 1st Mondays. We're excited to announce that we raised $3,530 in individual investments! Thank you #startup community for showing up and supporting us. \n\nBecome an investor: https://t.co/HMh10CkSgM https://t.co/eoNWVrfcXx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GivingTuesday",
+					"indices": [
+						27,
+						41
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						177,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HMh10CkSgM",
+					"expanded_url": "https://startupsd.org/donate/",
+					"display_url": "startupsd.org/donate/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1469031451421286400,
+					"id_str": "1469031451421286402",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMMUmHXoAI9Ecq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGMMUmHXoAI9Ecq.jpg",
+					"url": "https://t.co/eoNWVrfcXx",
+					"display_url": "pic.twitter.com/eoNWVrfcXx",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1469031452968955908/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1469031451421286400,
+					"id_str": "1469031451421286402",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMMUmHXoAI9Ecq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGMMUmHXoAI9Ecq.jpg",
+					"url": "https://t.co/eoNWVrfcXx",
+					"display_url": "pic.twitter.com/eoNWVrfcXx",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1469031452968955908/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4503,
+			"friends_count": 2323,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2341,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4280,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Last week we organized our #GivingTuesday campaign to raise funds for our 2022 1st Mondays. We're excited to announce that we raised $3,530 in individual investments! Thank you #startup community for showing up and supporting us. \n\nBecome an investor: https://t.co/HMh10CkSgM https://t.co/eoNWVrfcXx"
+	}
+]
