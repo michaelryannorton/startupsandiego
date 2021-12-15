8d6625f9e9e61c13d55e925640faad5144781b8e
Michael Ryan Norton <michael@michaelryannorton.com>
2021-12-14 16:17:03 -0800

    Added 2 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Tue Dec 14 18:25:08 +0000 2021",
+		"id": 1470822179554181000,
+		"id_str": "1470822179554181125",
+		"full_text": "Be part of the @founding San Diego Spring Virtual 2022 Program! Applications are now OPEN. \n\nGet a chance to work with top #investors and entrepreneurs, receive mentorship from local and international #founders, and more!\n\nTo learn more and to apply: https://t.co/WOvtuTi3vX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "investors",
+					"indices": [
+						123,
+						133
+					]
+				},
+				{
+					"text": "founders",
+					"indices": [
+						201,
+						210
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "founding",
+					"name": "Founder Institute",
+					"id": 29054405,
+					"id_str": "29054405",
+					"indices": [
+						15,
+						24
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/WOvtuTi3vX",
+					"expanded_url": "http://ow.ly/ZOvb50HaIlY",
+					"display_url": "ow.ly/ZOvb50HaIlY",
+					"indices": [
+						251,
+						274
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4508,
+			"friends_count": 2323,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4283,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1629310407",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Be part of the @founding San Diego Spring Virtual 2022 Program! Applications are now OPEN. \n\nGet a chance to work with top #investors and entrepreneurs, receive mentorship from local and international #founders, and more!\n\nTo learn more and to apply: https://t.co/WOvtuTi3vX"
+	},
+	{
+		"created_at": "Tue Dec 14 18:06:24 +0000 2021",
+		"id": 1469061329059295200,
+		"id_str": "1470817463864598539",
+		"full_text": "founding: ☀️ Applications are now OPEN for the #SanDiego Founder Institute virtual 2022 accelerator → https://t.co/QU7ps4psWK \nBuild an enduring company in the New Year w/ help from #SoCal's top #startup experts!\n\n@StartupSanDiego @StartupSoCalorg #SDst… https://t.co/kugGI3rLuc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SanDiego",
+					"indices": [
+						47,
+						56
+					]
+				},
+				{
+					"text": "SoCal",
+					"indices": [
+						182,
+						188
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						195,
+						203
+					]
+				},
+				{
+					"text": "SDst",
+					"indices": [
+						248,
+						253
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						214,
+						230
+					]
+				},
+				{
+					"screen_name": "StartupSoCalorg",
+					"name": "StartupSoCal",
+					"id": 2404962456,
+					"id_str": "2404962456",
+					"indices": [
+						231,
+						247
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QU7ps4psWK",
+					"expanded_url": "https://bit.ly/3pBesQU",
+					"display_url": "bit.ly/3pBesQU",
+					"indices": [
+						102,
+						125
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1469051554997190700,
+					"id_str": "1469051554997190660",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMemxxXwAQN7vl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGMemxxXwAQN7vl.jpg",
+					"url": "https://t.co/kugGI3rLuc",
+					"display_url": "pic.twitter.com/kugGI3rLuc",
+					"expanded_url": "https://twitter.com/founding/status/1469051652938293248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 500,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1469051652938293200,
+					"source_status_id_str": "1469051652938293248",
+					"source_user_id": 29054405,
+					"source_user_id_str": "29054405"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1469051554997190700,
+					"id_str": "1469051554997190660",
+					"indices": [
+						255,
+						278
+					],
+					"media_url": "http://pbs.twimg.com/media/FGMemxxXwAQN7vl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGMemxxXwAQN7vl.jpg",
+					"url": "https://t.co/kugGI3rLuc",
+					"display_url": "pic.twitter.com/kugGI3rLuc",
+					"expanded_url": "https://twitter.com/founding/status/1469051652938293248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 500,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 1469051652938293200,
+					"source_status_id_str": "1469051652938293248",
+					"source_user_id": 29054405,
+					"source_user_id_str": "29054405"
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1380187863476707300,
+			"id_str": "1380187863476707328",
+			"name": "Founder Institute Santa Cruz, CA",
+			"screen_name": "FI_SantaCruz",
+			"location": "Santa Cruz, CA",
+			"description": "We are building a program that will support Climate, Oceans, Sustainability and Energy startups and create a thriving startup ecosystem in Santa Cruz.",
+			"url": "https://t.co/9nAjYpnbZT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9nAjYpnbZT",
+							"expanded_url": "https://fi.co/santa_cruz",
+							"display_url": "fi.co/santa_cruz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61,
+			"friends_count": 80,
+			"listed_count": 0,
+			"created_at": "Thu Apr 08 15:56:58 +0000 2021",
+			"favourites_count": 25,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1380192910210002944/3v1SdL7j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1380192910210002944/3v1SdL7j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1380187863476707328/1621456350",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "founding: ☀️ Applications are now OPEN for the #SanDiego Founder Institute virtual 2022 accelerator → https://t.co/QU7ps4psWK \nBuild an enduring company in the New Year w/ help from #SoCal's top #startup experts!\n\n@StartupSanDiego @StartupSoCalorg #SDst… https://t.co/kugGI3rLuc"
+	},
+	{
 		"created_at": "Thu Dec 09 19:49:26 +0000 2021",
 		"id": 1469031452968956000,
 		"id_str": "1469031452968955908",
