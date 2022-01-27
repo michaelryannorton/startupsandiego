e03f3aaa6dab416297973d1fb982dab95d8069ed
Michael Ryan Norton <michael@michaelryannorton.com>
2022-01-26 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4135,
-	"created_at": "Sat Jan 22 00:17:02 +0000 2022",
+	"tweets": 4136,
+	"created_at": "Thu Jan 27 00:17:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 26 18:30:03 +0000 2022",
+		"id": 1486406092804472800,
+		"id_str": "1486406092804472845",
+		"full_text": "@MissionEdgeSD is now accepting applications through Feb 4th for their SAIL program! \n\nSAIL is a ten-week hybrid accelerator program that teaches social #entrepreneurs how to test and validate their business ideas and maximize their social impact.\n\nApply: https://t.co/gnakhDF0vC https://t.co/WioJabgBVX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						153,
+						167
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MissionEdgeSD",
+					"name": "Mission Edge",
+					"id": 130852872,
+					"id_str": "130852872",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gnakhDF0vC",
+					"expanded_url": "http://ow.ly/PvUJ50HENxs",
+					"display_url": "ow.ly/PvUJ50HENxs",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1486406091558768600,
+					"id_str": "1486406091558768644",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FKDGdwdWUAQY0l6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKDGdwdWUAQY0l6.jpg",
+					"url": "https://t.co/WioJabgBVX",
+					"display_url": "pic.twitter.com/WioJabgBVX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1486406092804472845/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 525,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 850,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1486406091558768600,
+					"id_str": "1486406091558768644",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FKDGdwdWUAQY0l6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKDGdwdWUAQY0l6.jpg",
+					"url": "https://t.co/WioJabgBVX",
+					"display_url": "pic.twitter.com/WioJabgBVX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1486406092804472845/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 525,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 850,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 130852872,
+		"in_reply_to_user_id_str": "130852872",
+		"in_reply_to_screen_name": "MissionEdgeSD",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us Feb 7th for our in-person 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4525,
+			"friends_count": 2322,
+			"listed_count": 120,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4290,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1642666589",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@MissionEdgeSD is now accepting applications through Feb 4th for their SAIL program! \n\nSAIL is a ten-week hybrid accelerator program that teaches social #entrepreneurs how to test and validate their business ideas and maximize their social impact.\n\nApply: https://t.co/gnakhDF0vC https://t.co/WioJabgBVX"
+	},
+	{
 		"created_at": "Fri Jan 21 16:00:27 +0000 2022",
 		"id": 1484556506246926300,
 		"id_str": "1484556506246926344",
