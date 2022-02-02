aab91b575cfe839eb52939cbf820cd3f3914a318
Michael Ryan Norton <michael@michaelryannorton.com>
2022-02-01 16:17:04 -0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4137,
-	"created_at": "Tue Feb 01 00:17:03 +0000 2022",
+	"tweets": 4139,
+	"created_at": "Wed Feb 02 00:17:03 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,357 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 01 18:35:02 +0000 2022",
+		"id": 1488581676041134000,
+		"id_str": "1488581676041134087",
+		"full_text": "Need an extra hand in your lab? @SDRegionalEDC’s #advancingSD is selecting 15 small Life Sciences companies to host paid interns and footing the bill.\n\nCheck your eligibility and apply by Monday, February 14: https://t.co/GSyQUad8x4 https://t.co/wDlnbAovuF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "advancingSD",
+					"indices": [
+						49,
+						61
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDRegionalEDC",
+					"name": "San Diego Regional EDC",
+					"id": 113474827,
+					"id_str": "113474827",
+					"indices": [
+						32,
+						46
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GSyQUad8x4",
+					"expanded_url": "http://ow.ly/xw3850HJEGl",
+					"display_url": "ow.ly/xw3850HJEGl",
+					"indices": [
+						209,
+						232
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1488581673486803000,
+					"id_str": "1488581673486802944",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FKiBJStWYAAwFF6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKiBJStWYAAwFF6.jpg",
+					"url": "https://t.co/wDlnbAovuF",
+					"display_url": "pic.twitter.com/wDlnbAovuF",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1488581676041134087/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1012,
+							"h": 506,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1012,
+							"h": 506,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488581673486803000,
+					"id_str": "1488581673486802944",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FKiBJStWYAAwFF6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKiBJStWYAAwFF6.jpg",
+					"url": "https://t.co/wDlnbAovuF",
+					"display_url": "pic.twitter.com/wDlnbAovuF",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1488581676041134087/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1012,
+							"h": 506,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1012,
+							"h": 506,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us Feb 7th for our in-person 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4533,
+			"friends_count": 2322,
+			"listed_count": 120,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4294,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1642666589",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Need an extra hand in your lab? @SDRegionalEDC’s #advancingSD is selecting 15 small Life Sciences companies to host paid interns and footing the bill.\n\nCheck your eligibility and apply by Monday, February 14: https://t.co/GSyQUad8x4 https://t.co/wDlnbAovuF"
+	},
+	{
+		"created_at": "Tue Feb 01 18:13:19 +0000 2022",
+		"id": 1488241489800941600,
+		"id_str": "1488576208308776962",
+		"full_text": "#1stMondays are back with @StartupSanDiego! Join our friends Feb 7th for a night of startup stories &amp; networking 🚀#StartupSD EIR, @TaraFalcone will be moderating an investor panel to match #entrepreneurs with expert investors. \nGet tickets:\nhttps://t.co/1jhPyH7OdB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						118,
+						128
+					]
+				},
+				{
+					"text": "entrepreneurs",
+					"indices": [
+						193,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						26,
+						42
+					]
+				},
+				{
+					"screen_name": "TaraFalcone",
+					"name": "Tara Falcone",
+					"id": 4047907342,
+					"id_str": "4047907342",
+					"indices": [
+						134,
+						146
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/1jhPyH7OdB",
+					"expanded_url": "https://www.eventbrite.com/e/startupsd-1st-mondays-february-2022-tickets-251764744127",
+					"display_url": "eventbrite.com/e/startupsd-1s…",
+					"indices": [
+						245,
+						268
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 49085200,
+			"id_str": "49085200",
+			"name": "UCSD OIC",
+			"screen_name": "UCSDInnovation",
+			"location": "La Jolla CA",
+			"description": "UC San Diego Office of Innovation and Commercialization: Leading regional social and economic development – building a dynamic innovation culture and ecosystem.",
+			"url": "https://t.co/Uij5ceg6o5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Uij5ceg6o5",
+							"expanded_url": "http://innovation.ucsd.edu",
+							"display_url": "innovation.ucsd.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1794,
+			"friends_count": 1370,
+			"listed_count": 103,
+			"created_at": "Sat Jun 20 19:02:47 +0000 2009",
+			"favourites_count": 826,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/843898368396939264/FtGToChU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/843898368396939264/FtGToChU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/49085200/1622227014",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#1stMondays are back with @StartupSanDiego! Join our friends Feb 7th for a night of startup stories &amp; networking 🚀#StartupSD EIR, @TaraFalcone will be moderating an investor panel to match #entrepreneurs with expert investors. \nGet tickets:\nhttps://t.co/1jhPyH7OdB"
+	}
+]
