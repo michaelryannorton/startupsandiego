cd9f33a82b6f523f99077d5e0687d50452214615
Michael Ryan Norton <michael@michaelryannorton.com>
2022-02-04 16:17:03 -0800

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 04 17:42:13 +0000 2022",
+		"id": 1489655548341633000,
+		"id_str": "1489655548341633031",
+		"full_text": "Joining us for our 1st Mondays mini-investor panel is #entrepreneur Heather Steer, Chief Financial Officer, and Co-Founder at Splitfyi!\n\nGet your last-minute tickets &amp; join Heather for a night of #startup discussions &amp; networking.\n\nTickets: https://t.co/m0Q1W5gASg https://t.co/zzyk8VTrzC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "entrepreneur",
+					"indices": [
+						54,
+						67
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						200,
+						208
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/m0Q1W5gASg",
+					"expanded_url": "http://ow.ly/syrW50HMRjj",
+					"display_url": "ow.ly/syrW50HMRjj",
+					"indices": [
+						249,
+						272
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1489655546571595800,
+					"id_str": "1489655546571595780",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FKxR07sXIAQFc8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKxR07sXIAQFc8w.jpg",
+					"url": "https://t.co/zzyk8VTrzC",
+					"display_url": "pic.twitter.com/zzyk8VTrzC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1489655548341633031/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489655546571595800,
+					"id_str": "1489655546571595780",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FKxR07sXIAQFc8w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKxR07sXIAQFc8w.jpg",
+					"url": "https://t.co/zzyk8VTrzC",
+					"display_url": "pic.twitter.com/zzyk8VTrzC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1489655548341633031/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us Feb 7th for our in-person 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4534,
+			"friends_count": 2322,
+			"listed_count": 121,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2353,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4301,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1642666589",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Joining us for our 1st Mondays mini-investor panel is #entrepreneur Heather Steer, Chief Financial Officer, and Co-Founder at Splitfyi!\n\nGet your last-minute tickets &amp; join Heather for a night of #startup discussions &amp; networking.\n\nTickets: https://t.co/m0Q1W5gASg https://t.co/zzyk8VTrzC"
+	},
+	{
 		"created_at": "Thu Feb 03 19:37:18 +0000 2022",
 		"id": 1489322119993438200,
 		"id_str": "1489322119993438218",
