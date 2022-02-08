d00cb1ca8ef5abfcd5f538de371f1926a9138c80
Michael Ryan Norton <michael@michaelryannorton.com>
2022-02-07 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4145,
-	"created_at": "Sat Feb 05 00:17:02 +0000 2022",
+	"tweets": 4146,
+	"created_at": "Tue Feb 08 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 07 19:42:41 +0000 2022",
+		"id": 1490773028686467000,
+		"id_str": "1490773028686467074",
+		"full_text": "Get ready for TONIGHT! We're excited to see the #startup community come together for our 1st Mondays event🚀\n\nWe'll be featuring @silviamah, @ALBsharah  and Heather Steer for our mini-investor panel, moderated by @TaraFalcone. \n\nGrab tickets: https://t.co/flKABZSOaK\n\n#1stMondays https://t.co/ns3ZaKCqdx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						48,
+						56
+					]
+				},
+				{
+					"text": "1stMondays",
+					"indices": [
+						267,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "silviamah",
+					"name": "Silvia Mah",
+					"id": 200349076,
+					"id_str": "200349076",
+					"indices": [
+						128,
+						138
+					]
+				},
+				{
+					"screen_name": "ALBsharah",
+					"name": "Al Bsharah",
+					"id": 16711411,
+					"id_str": "16711411",
+					"indices": [
+						140,
+						150
+					]
+				},
+				{
+					"screen_name": "TaraFalcone",
+					"name": "Tara Falcone",
+					"id": 4047907342,
+					"id_str": "4047907342",
+					"indices": [
+						212,
+						224
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/flKABZSOaK",
+					"expanded_url": "http://ow.ly/K2sv50HOIbs",
+					"display_url": "ow.ly/K2sv50HOIbs",
+					"indices": [
+						242,
+						265
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1490773026669047800,
+					"id_str": "1490773026669047810",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FLBKK1dXsAI7Fm4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLBKK1dXsAI7Fm4.jpg",
+					"url": "https://t.co/ns3ZaKCqdx",
+					"display_url": "pic.twitter.com/ns3ZaKCqdx",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1490773028686467074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1490773026669047800,
+					"id_str": "1490773026669047810",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FLBKK1dXsAI7Fm4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLBKK1dXsAI7Fm4.jpg",
+					"url": "https://t.co/ns3ZaKCqdx",
+					"display_url": "pic.twitter.com/ns3ZaKCqdx",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1490773028686467074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us Feb 7th for our in-person 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4536,
+			"friends_count": 2322,
+			"listed_count": 121,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2353,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4302,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1642666589",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get ready for TONIGHT! We're excited to see the #startup community come together for our 1st Mondays event🚀\n\nWe'll be featuring @silviamah, @ALBsharah  and Heather Steer for our mini-investor panel, moderated by @TaraFalcone. \n\nGrab tickets: https://t.co/flKABZSOaK\n\n#1stMondays https://t.co/ns3ZaKCqdx"
+	},
+	{
 		"created_at": "Fri Feb 04 17:42:13 +0000 2022",
 		"id": 1489655548341633000,
 		"id_str": "1489655548341633031",
