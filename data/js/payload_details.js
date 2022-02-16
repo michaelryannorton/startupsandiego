5065e3c091ddc8b20585e14de5bbc6fd12d60685
Michael Ryan Norton <michael@michaelryannorton.com>
2022-02-15 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4153,
-	"created_at": "Tue Feb 15 00:17:02 +0000 2022",
+	"tweets": 4154,
+	"created_at": "Wed Feb 16 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Tue Feb 15 20:05:03 +0000 2022",
+		"id": 1493677757569683500,
+		"id_str": "1493677757569683459",
+		"full_text": "@WTCSanDiego is proud to present the newest set of MetroConnect VI companies! The cohort will gain access to a suite of resources to support expansion into #international markets. \n\nLearn more: https://t.co/oi50vdVPT9\n\n@SDRegionalEDC https://t.co/vTzYZw1i12",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			233
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "international",
+					"indices": [
+						156,
+						170
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "WTCSanDiego",
+					"name": "World Trade Center San Diego",
+					"id": 4795580413,
+					"id_str": "4795580413",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "SDRegionalEDC",
+					"name": "San Diego Regional EDC",
+					"id": 113474827,
+					"id_str": "113474827",
+					"indices": [
+						219,
+						233
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oi50vdVPT9",
+					"expanded_url": "http://ow.ly/Nlec50HVUaj",
+					"display_url": "ow.ly/Nlec50HVUaj",
+					"indices": [
+						194,
+						217
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1493677753983553500,
+					"id_str": "1493677753983553537",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FLqcAN8X0AElJvk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLqcAN8X0AElJvk.jpg",
+					"url": "https://t.co/vTzYZw1i12",
+					"display_url": "pic.twitter.com/vTzYZw1i12",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1493677757569683459/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 582,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 386,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 582,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1493677753983553500,
+					"id_str": "1493677753983553537",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FLqcAN8X0AElJvk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLqcAN8X0AElJvk.jpg",
+					"url": "https://t.co/vTzYZw1i12",
+					"display_url": "pic.twitter.com/vTzYZw1i12",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1493677757569683459/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 582,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 386,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 582,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 4795580413,
+		"in_reply_to_user_id_str": "4795580413",
+		"in_reply_to_screen_name": "WTCSanDiego",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on May 2nd for our in-person 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKNlw69",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKNlw69",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4547,
+			"friends_count": 2320,
+			"listed_count": 121,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2367,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4314,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1644780956",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@WTCSanDiego is proud to present the newest set of MetroConnect VI companies! The cohort will gain access to a suite of resources to support expansion into #international markets. \n\nLearn more: https://t.co/oi50vdVPT9\n\n@SDRegionalEDC https://t.co/vTzYZw1i12"
+	},
+	{
 		"created_at": "Mon Feb 14 19:45:34 +0000 2022",
 		"id": 1493310467846070300,
 		"id_str": "1493310467846070273",
