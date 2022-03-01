65098a5682218ea8415b502033aece789072ca95
Michael Ryan Norton <michael@michaelryannorton.com>
2022-02-28 16:17:04 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4157,
-	"created_at": "Thu Feb 24 00:17:03 +0000 2022",
+	"tweets": 4158,
+	"created_at": "Tue Mar 01 00:17:03 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 28 17:01:57 +0000 2022",
+		"id": 1498342723497209900,
+		"id_str": "1498342723497209856",
+		"full_text": "Convergence is BACK! We’re bringing together our region’s next-generation #founders and #tech workforce with the local innovation ecosystem!\n\nGrab EARLY-BIRD tickets until Friday, March 11, 2022: https://t.co/XwrW1Zun6f\n\n#Convergence2022 #StartupSD https://t.co/EpQ0DRiuYv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "founders",
+					"indices": [
+						74,
+						83
+					]
+				},
+				{
+					"text": "tech",
+					"indices": [
+						88,
+						93
+					]
+				},
+				{
+					"text": "Convergence2022",
+					"indices": [
+						221,
+						237
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						238,
+						248
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XwrW1Zun6f",
+					"expanded_url": "http://ow.ly/hwhU50I62OH",
+					"display_url": "ow.ly/hwhU50I62OH",
+					"indices": [
+						196,
+						219
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1498342721626550300,
+					"id_str": "1498342721626550286",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/FMsuxEpXsA4w95z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMsuxEpXsA4w95z.jpg",
+					"url": "https://t.co/EpQ0DRiuYv",
+					"display_url": "pic.twitter.com/EpQ0DRiuYv",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1498342723497209856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498342721626550300,
+					"id_str": "1498342721626550286",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/FMsuxEpXsA4w95z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMsuxEpXsA4w95z.jpg",
+					"url": "https://t.co/EpQ0DRiuYv",
+					"display_url": "pic.twitter.com/EpQ0DRiuYv",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1498342723497209856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 12-13 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4556,
+			"friends_count": 2318,
+			"listed_count": 120,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2370,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1645724595",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Convergence is BACK! We’re bringing together our region’s next-generation #founders and #tech workforce with the local innovation ecosystem!\n\nGrab EARLY-BIRD tickets until Friday, March 11, 2022: https://t.co/XwrW1Zun6f\n\n#Convergence2022 #StartupSD https://t.co/EpQ0DRiuYv"
+	},
+	{
 		"created_at": "Wed Feb 23 19:29:31 +0000 2022",
 		"id": 1496567921287614500,
 		"id_str": "1496567921287614469",
