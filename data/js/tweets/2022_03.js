2239646740f3b33f78db44020d6d8d5e1da5717e
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-02 16:17:03 -0800

    Added 1 tweets

---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,146 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Wed Mar 02 19:17:13 +0000 2022",
+		"id": 1499101539448901600,
+		"id_str": "1499101539448901634",
+		"full_text": "This year, @LaunchFactorySD has partnered with @WowYow to leverage their breakthrough computer vision for video #AI technology to develop multiple #startups in new verticals.\n\nApply and learn more about the Founders in Residence program: https://t.co/rGKkGvrfuE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AI",
+					"indices": [
+						112,
+						115
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						147,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LaunchFactorySD",
+					"name": "Launch Factory",
+					"id": 1220430197435981800,
+					"id_str": "1220430197435981824",
+					"indices": [
+						11,
+						27
+					]
+				},
+				{
+					"screen_name": "WowYow",
+					"name": "WowYow Inc.",
+					"id": 224019494,
+					"id_str": "224019494",
+					"indices": [
+						47,
+						54
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rGKkGvrfuE",
+					"expanded_url": "http://ow.ly/9KrP50I8rBV",
+					"display_url": "ow.ly/9KrP50I8rBV",
+					"indices": [
+						238,
+						261
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 12-13 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4559,
+			"friends_count": 2318,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2372,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1645724595",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This year, @LaunchFactorySD has partnered with @WowYow to leverage their breakthrough computer vision for video #AI technology to develop multiple #startups in new verticals.\n\nApply and learn more about the Founders in Residence program: https://t.co/rGKkGvrfuE"
+	}
+]
