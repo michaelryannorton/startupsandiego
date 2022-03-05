ed51bdabcb0405f80fe4349624b9417073b5001c
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-04 16:17:02 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4159,
-	"created_at": "Thu Mar 03 00:17:02 +0000 2022",
+	"tweets": 4160,
+	"created_at": "Sat Mar 05 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 04 17:41:27 +0000 2022",
+		"id": 1499802214558220300,
+		"id_str": "1499802214558220290",
+		"full_text": "Amazing to see Tosh Dutt, CEO at @ChargeNetStnUS  and #SDSM Incubator Alumni, win a new investment from @MDFinanceSD.\n\nChargeNet Stations is a #cleantech company constructing solar-powered batteries for EV's at quick service restaurants.\n\nLearn more: https://t.co/5PIynX5pAJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSM",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "cleantech",
+					"indices": [
+						143,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ChargeNetStnUS",
+					"name": "ChargeNet Stations",
+					"id": 1316790923536158700,
+					"id_str": "1316790923536158721",
+					"indices": [
+						33,
+						48
+					]
+				},
+				{
+					"screen_name": "MDFinanceSD",
+					"name": "Mission Driven Finance",
+					"id": 739934416890265600,
+					"id_str": "739934416890265604",
+					"indices": [
+						104,
+						116
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5PIynX5pAJ",
+					"expanded_url": "http://ow.ly/Ap2750IarEe",
+					"display_url": "ow.ly/Ap2750IarEe",
+					"indices": [
+						251,
+						274
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4559,
+			"friends_count": 2316,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2375,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4322,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Amazing to see Tosh Dutt, CEO at @ChargeNetStnUS  and #SDSM Incubator Alumni, win a new investment from @MDFinanceSD.\n\nChargeNet Stations is a #cleantech company constructing solar-powered batteries for EV's at quick service restaurants.\n\nLearn more: https://t.co/5PIynX5pAJ"
+	},
+	{
 		"created_at": "Wed Mar 02 19:17:13 +0000 2022",
 		"id": 1499101539448901600,
 		"id_str": "1499101539448901634",
