3f0bc34433f0e23251934ebe4323c726a6410807
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-07 16:17:03 -0800

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 07 19:40:05 +0000 2022",
+		"id": 1500919232208547800,
+		"id_str": "1500919232208547844",
+		"full_text": "@UCSDExtension is offering a Machine Learning Engineering Bootcamp! Learn online from an industry-driven curriculum. Deploy machine learning algorithms and build a complete application.\n\nLearn more and apply: https://t.co/hL4W8E5DIY https://t.co/PWeEnVlHi6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "UCSDExtension",
+					"name": "UC San Diego Extension",
+					"id": 22994081,
+					"id_str": "22994081",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hL4W8E5DIY",
+					"expanded_url": "http://ow.ly/mQGq50Ic8iT",
+					"display_url": "ow.ly/mQGq50Ic8iT",
+					"indices": [
+						209,
+						232
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1500919225606619100,
+					"id_str": "1500919225606619140",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FNRWFV7WQAQNxXF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNRWFV7WQAQNxXF.jpg",
+					"url": "https://t.co/PWeEnVlHi6",
+					"display_url": "pic.twitter.com/PWeEnVlHi6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1500919232208547844/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1500919225606619100,
+					"id_str": "1500919225606619140",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/FNRWFV7WQAQNxXF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNRWFV7WQAQNxXF.jpg",
+					"url": "https://t.co/PWeEnVlHi6",
+					"display_url": "pic.twitter.com/PWeEnVlHi6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1500919232208547844/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 22994081,
+		"in_reply_to_user_id_str": "22994081",
+		"in_reply_to_screen_name": "UCSDExtension",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4559,
+			"friends_count": 2317,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2375,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4324,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@UCSDExtension is offering a Machine Learning Engineering Bootcamp! Learn online from an industry-driven curriculum. Deploy machine learning algorithms and build a complete application.\n\nLearn more and apply: https://t.co/hL4W8E5DIY https://t.co/PWeEnVlHi6"
+	},
+	{
 		"created_at": "Fri Mar 04 17:41:27 +0000 2022",
 		"id": 1499802214558220300,
 		"id_str": "1499802214558220290",
