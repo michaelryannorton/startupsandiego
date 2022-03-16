06b3e11771d0e341e153929301a4e1cf7b5629a7
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-15 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 15 19:12:50 +0000 2022",
+		"id": 1503811477030981600,
+		"id_str": "1503811477030981638",
+		"full_text": "We are excited to announce that our community partner @BBBPacificSW will be hosting their 2022 Women’s Entrepreneur Summit! \n\nJoin virtually or in-person on Thursday, March 24th from  for this exclusive one-day event. Reserve your spot now: https://t.co/MJlwLZVH5M https://t.co/CsJ0jWLKyU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBBPacificSW",
+					"name": "BBB Serving the Pacific Southwest",
+					"id": 21798919,
+					"id_str": "21798919",
+					"indices": [
+						54,
+						67
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MJlwLZVH5M",
+					"expanded_url": "https://whova.com/web/women9_202203/?view=preview",
+					"display_url": "whova.com/web/women9_202…",
+					"indices": [
+						241,
+						264
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1503811468093018000,
+					"id_str": "1503811468093018114",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FN6ckAzX0AIHpZa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN6ckAzX0AIHpZa.jpg",
+					"url": "https://t.co/CsJ0jWLKyU",
+					"display_url": "pic.twitter.com/CsJ0jWLKyU",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1503811477030981638/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503811468093018000,
+					"id_str": "1503811468093018114",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FN6ckAzX0AIHpZa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN6ckAzX0AIHpZa.jpg",
+					"url": "https://t.co/CsJ0jWLKyU",
+					"display_url": "pic.twitter.com/CsJ0jWLKyU",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1503811477030981638/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4564,
+			"friends_count": 2317,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2377,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4328,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are excited to announce that our community partner @BBBPacificSW will be hosting their 2022 Women’s Entrepreneur Summit! \n\nJoin virtually or in-person on Thursday, March 24th from  for this exclusive one-day event. Reserve your spot now: https://t.co/MJlwLZVH5M https://t.co/CsJ0jWLKyU"
+	},
+	{
 		"created_at": "Fri Mar 11 18:00:40 +0000 2022",
 		"id": 1502343766765670400,
 		"id_str": "1502343766765670406",
