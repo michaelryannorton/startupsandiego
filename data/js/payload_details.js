b1f4fb07351b2760a104abf8fbf40a4a7ea6972a
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-16 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4164,
-	"created_at": "Wed Mar 16 00:17:02 +0000 2022",
+	"tweets": 4165,
+	"created_at": "Thu Mar 17 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 16 17:52:55 +0000 2022",
+		"id": 1504153754240798700,
+		"id_str": "1504153754240798722",
+		"full_text": "We’re excited to announce that Startup San Diego was selected for a $1,500 subgrant through the Empower by GoDaddy in partnership with @igniteBBB! This subgrant will support our efforts to advance the #entrepreneurship &amp; #startup community in San Diego.\n\n@BBBPacificSW https://t.co/8P3HGEKvbg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "entrepreneurship",
+					"indices": [
+						201,
+						218
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						225,
+						233
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "igniteBBB",
+					"name": "ignite Sparked by BBB",
+					"id": 1088922760670195700,
+					"id_str": "1088922760670195712",
+					"indices": [
+						135,
+						145
+					]
+				},
+				{
+					"screen_name": "BBBPacificSW",
+					"name": "BBB Serving the Pacific Southwest",
+					"id": 21798919,
+					"id_str": "21798919",
+					"indices": [
+						259,
+						272
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1504153751950708700,
+					"id_str": "1504153751950708738",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FN_T3jiX0AIjYlX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN_T3jiX0AIjYlX.jpg",
+					"url": "https://t.co/8P3HGEKvbg",
+					"display_url": "pic.twitter.com/8P3HGEKvbg",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1504153754240798722/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504153751950708700,
+					"id_str": "1504153751950708738",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/FN_T3jiX0AIjYlX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN_T3jiX0AIjYlX.jpg",
+					"url": "https://t.co/8P3HGEKvbg",
+					"display_url": "pic.twitter.com/8P3HGEKvbg",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1504153754240798722/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4564,
+			"friends_count": 2316,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2377,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4329,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We’re excited to announce that Startup San Diego was selected for a $1,500 subgrant through the Empower by GoDaddy in partnership with @igniteBBB! This subgrant will support our efforts to advance the #entrepreneurship &amp; #startup community in San Diego.\n\n@BBBPacificSW https://t.co/8P3HGEKvbg"
+	},
+	{
 		"created_at": "Tue Mar 15 19:12:50 +0000 2022",
 		"id": 1503811477030981600,
 		"id_str": "1503811477030981638",
