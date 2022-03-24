bf6f3caaa83e58ff8c5e72877786e2cbd8cf9170
Michael Ryan Norton <michael@michaelryannorton.com>
2022-03-23 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 23 20:55:21 +0000 2022",
+		"id": 1506736381170700300,
+		"id_str": "1506736381170700289",
+		"full_text": "Get LAST MINUTE tickets for tonight's March Mingle! San Diego Tech Scene and Startup SD invite you to an evening of networking with local techies while enjoying some local libations and fresh-off-the-grill street tacos.\n\nGet tickets online or at the door: https://t.co/DAXC2HfDcq https://t.co/UPIvdgLaSm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DAXC2HfDcq",
+					"expanded_url": "https://marchmingle.com/",
+					"display_url": "marchmingle.com",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1506736379677618200,
+					"id_str": "1506736379677618184",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FOkAwRjXsAgJ8zr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOkAwRjXsAgJ8zr.jpg",
+					"url": "https://t.co/UPIvdgLaSm",
+					"display_url": "pic.twitter.com/UPIvdgLaSm",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1506736381170700289/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 755,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 428,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1288,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506736379677618200,
+					"id_str": "1506736379677618184",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FOkAwRjXsAgJ8zr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOkAwRjXsAgJ8zr.jpg",
+					"url": "https://t.co/UPIvdgLaSm",
+					"display_url": "pic.twitter.com/UPIvdgLaSm",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1506736381170700289/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 755,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 428,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1288,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4571,
+			"friends_count": 2315,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2380,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4330,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get LAST MINUTE tickets for tonight's March Mingle! San Diego Tech Scene and Startup SD invite you to an evening of networking with local techies while enjoying some local libations and fresh-off-the-grill street tacos.\n\nGet tickets online or at the door: https://t.co/DAXC2HfDcq https://t.co/UPIvdgLaSm"
+	},
+	{
 		"created_at": "Wed Mar 16 17:52:55 +0000 2022",
 		"id": 1504153754240798700,
 		"id_str": "1504153754240798722",
