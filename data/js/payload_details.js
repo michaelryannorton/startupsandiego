64a1a46d1c5c8b70c8d41bd07c8dca132f367b8d
Michael Ryan Norton <michael@michaelryannorton.com>
2022-04-06 17:17:03 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4177,
-	"created_at": "Tue Apr 05 00:17:02 +0000 2022",
+	"tweets": 4179,
+	"created_at": "Thu Apr 07 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,309 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 06 17:00:27 +0000 2022",
+		"id": 1511750694998577200,
+		"id_str": "1511750694998577160",
+		"full_text": "Our new round of featured speakers and workshop leaders boast years of startup experience! These experts will help you find your next tech role, teach you what to put on your next resume, and give you tips on how to exceed in your industry.\n\n Get tickets: https://t.co/HobC2s48Aq https://t.co/9IEx3S3Gu0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HobC2s48Aq",
+					"expanded_url": "https://startupsd.org/convergence/",
+					"display_url": "startupsd.org/convergence/",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1511750692989509600,
+					"id_str": "1511750692989509633",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FPrRPuSXIAEqd0t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPrRPuSXIAEqd0t.jpg",
+					"url": "https://t.co/9IEx3S3Gu0",
+					"display_url": "pic.twitter.com/9IEx3S3Gu0",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1511750694998577160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1511750692989509600,
+					"id_str": "1511750692989509633",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FPrRPuSXIAEqd0t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPrRPuSXIAEqd0t.jpg",
+					"url": "https://t.co/9IEx3S3Gu0",
+					"display_url": "pic.twitter.com/9IEx3S3Gu0",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1511750694998577160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us April 13-14 for Convergence 2022. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKND7uJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKND7uJ",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4582,
+			"friends_count": 2315,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2389,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4348,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1646433266",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our new round of featured speakers and workshop leaders boast years of startup experience! These experts will help you find your next tech role, teach you what to put on your next resume, and give you tips on how to exceed in your industry.\n\n Get tickets: https://t.co/HobC2s48Aq https://t.co/9IEx3S3Gu0"
+	},
+	{
+		"created_at": "Wed Apr 06 16:41:52 +0000 2022",
+		"id": 1511405385286099000,
+		"id_str": "1511746017099722755",
+		"full_text": "Convergence, @StartupSanDiego's annual event focused on converging and bridging student talent and startup ecosystem is happening on April 12-13th. Use our community partner code: StellaLabs30 to claim your 30% discount on Convergence tickets here. https://t.co/Jde07aRuQq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						13,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Jde07aRuQq",
+					"expanded_url": "https://bit.ly/Convergence2022",
+					"display_url": "bit.ly/Convergence2022",
+					"indices": [
+						249,
+						272
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">Agorapulse app</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1480627296,
+			"id_str": "1480627296",
+			"name": "Stella",
+			"screen_name": "wearestellaco",
+			"location": "San Diego",
+			"description": "Created for women, by women 💪🏻💪🏽💪🏿. Stella connects women founders and investors to the right resource–at the right time–in their journeys.",
+			"url": "https://t.co/fLeXRyzgok",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fLeXRyzgok",
+							"expanded_url": "https://linktr.ee/stellalabs",
+							"display_url": "linktr.ee/stellalabs",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2339,
+			"friends_count": 1224,
+			"listed_count": 222,
+			"created_at": "Mon Jun 03 21:16:42 +0000 2013",
+			"favourites_count": 2405,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8075,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1509573094830231564/EZMnzHdA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1509573094830231564/EZMnzHdA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1480627296/1646797576",
+			"profile_link_color": "8FA5B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Convergence, @StartupSanDiego's annual event focused on converging and bridging student talent and startup ecosystem is happening on April 12-13th. Use our community partner code: StellaLabs30 to claim your 30% discount on Convergence tickets here. https://t.co/Jde07aRuQq"
+	},
+	{
 		"created_at": "Mon Apr 04 18:32:56 +0000 2022",
 		"id": 1511049195892977700,
 		"id_str": "1511049195892977672",
