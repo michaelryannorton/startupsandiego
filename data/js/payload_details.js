d8bb66ff744786b9d245612b8ce64f6f3b60a08b
Michael Ryan Norton <michael@michaelryannorton.com>
2022-04-11 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4181,
-	"created_at": "Sat Apr 09 00:17:02 +0000 2022",
+	"tweets": 4182,
+	"created_at": "Tue Apr 12 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,263 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Mon Apr 11 17:54:40 +0000 2022",
+		"id": 1513569552751161300,
+		"id_str": "1513576277894172672",
+		"full_text": "This week I’ll be speaking w/ other amazing folks at @StartupSanDiego Convergence. Night 1: help with #jobinterview &amp; Night 2: navigating the #startup ecosystem w/ @NealBloom. \n\nLink: https://t.co/YIxpmatRYd https://t.co/N6xm0ZNzYW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "jobinterview",
+					"indices": [
+						102,
+						115
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						146,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						53,
+						69
+					]
+				},
+				{
+					"screen_name": "NealBloom",
+					"name": "Neal Bloom",
+					"id": 19131762,
+					"id_str": "19131762",
+					"indices": [
+						168,
+						178
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/YIxpmatRYd",
+					"expanded_url": "https://startupsd.org/convergence",
+					"display_url": "startupsd.org/convergence",
+					"indices": [
+						188,
+						211
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1513569546959200300,
+					"id_str": "1513569546959200258",
+					"indices": [
+						212,
+						235
+					],
+					"media_url": "http://pbs.twimg.com/media/FQFHe9VaIAIyEUj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQFHe9VaIAIyEUj.jpg",
+					"url": "https://t.co/N6xm0ZNzYW",
+					"display_url": "pic.twitter.com/N6xm0ZNzYW",
+					"expanded_url": "https://twitter.com/juls_alejandro/status/1513569552751161346/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1513569546959200300,
+					"id_str": "1513569546959200258",
+					"indices": [
+						212,
+						235
+					],
+					"media_url": "http://pbs.twimg.com/media/FQFHe9VaIAIyEUj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQFHe9VaIAIyEUj.jpg",
+					"url": "https://t.co/N6xm0ZNzYW",
+					"display_url": "pic.twitter.com/N6xm0ZNzYW",
+					"expanded_url": "https://twitter.com/juls_alejandro/status/1513569552751161346/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1513569547009282000,
+					"id_str": "1513569547009282055",
+					"indices": [
+						212,
+						235
+					],
+					"media_url": "http://pbs.twimg.com/media/FQFHe9hWUAcOYfe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQFHe9hWUAcOYfe.jpg",
+					"url": "https://t.co/N6xm0ZNzYW",
+					"display_url": "pic.twitter.com/N6xm0ZNzYW",
+					"expanded_url": "https://twitter.com/juls_alejandro/status/1513569552751161346/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3433772714,
+			"id_str": "3433772714",
+			"name": "Julius Alejandro",
+			"screen_name": "juls_alejandro",
+			"location": "San Diego, CA",
+			"description": "I tweet about hip-hop, anime, DEI, social good, and all things related to startup to be inclusive for all.",
+			"url": "https://t.co/0LieXReANe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0LieXReANe",
+							"expanded_url": "http://www.juliusalejandro.com",
+							"display_url": "juliusalejandro.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 489,
+			"friends_count": 749,
+			"listed_count": 64,
+			"created_at": "Thu Sep 03 05:53:06 +0000 2015",
+			"favourites_count": 2682,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2816,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1319694208270172160/fvHmMcpU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1319694208270172160/fvHmMcpU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3433772714/1611903256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This week I’ll be speaking w/ other amazing folks at @StartupSanDiego Convergence. Night 1: help with #jobinterview &amp; Night 2: navigating the #startup ecosystem w/ @NealBloom. \n\nLink: https://t.co/YIxpmatRYd https://t.co/N6xm0ZNzYW"
+	},
+	{
 		"created_at": "Fri Apr 08 17:51:05 +0000 2022",
 		"id": 1512475880601358300,
 		"id_str": "1512488214841409538",
