810867a4f867592b9b9ee7a1968b6ececd3ffb3f
Michael Ryan Norton <michael@michaelryannorton.com>
2022-04-26 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4210,
-	"created_at": "Tue Apr 26 00:17:03 +0000 2022",
+	"tweets": 4211,
+	"created_at": "Wed Apr 27 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 26 15:59:55 +0000 2022",
+		"id": 1518983221010223000,
+		"id_str": "1518983221010223105",
+		"full_text": "The month of May is Military Appreciation Month! Join us on May 2nd for our 1st Mondays event as we focus on recruiting, specifically from two unique talent pools available in our #startup ecosystem: the Military and Mexico.\n\nGet your tickets: https://t.co/oscL1X5goB https://t.co/eXzSuRQWFH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						180,
+						188
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oscL1X5goB",
+					"expanded_url": "http://ow.ly/3c0750ISzIa",
+					"display_url": "ow.ly/3c0750ISzIa",
+					"indices": [
+						244,
+						267
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1518983219005345800,
+					"id_str": "1518983219005345792",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FRSDMJSX0AA5BnA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRSDMJSX0AA5BnA.jpg",
+					"url": "https://t.co/eXzSuRQWFH",
+					"display_url": "pic.twitter.com/eXzSuRQWFH",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1518983221010223105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1518983219005345800,
+					"id_str": "1518983219005345792",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FRSDMJSX0AA5BnA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRSDMJSX0AA5BnA.jpg",
+					"url": "https://t.co/eXzSuRQWFH",
+					"display_url": "pic.twitter.com/eXzSuRQWFH",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1518983221010223105/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on May 2nd for 1st Mondays. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKNlw69",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKNlw69",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4610,
+			"friends_count": 2311,
+			"listed_count": 0,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4389,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1650477957",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The month of May is Military Appreciation Month! Join us on May 2nd for our 1st Mondays event as we focus on recruiting, specifically from two unique talent pools available in our #startup ecosystem: the Military and Mexico.\n\nGet your tickets: https://t.co/oscL1X5goB https://t.co/eXzSuRQWFH"
+	},
+	{
 		"created_at": "Mon Apr 25 19:01:16 +0000 2022",
 		"id": 1518666467918626800,
 		"id_str": "1518666467918626817",
