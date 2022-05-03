52675b3d71cb40937888ab9c7fb0f9978f1f6c0c
Michael Ryan Norton <michael@michaelryannorton.com>
2022-05-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4213,
-	"created_at": "Sat Apr 30 00:17:02 +0000 2022",
+	"tweets": 4214,
+	"created_at": "Tue May 03 00:17:02 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,199 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Mon May 02 18:39:34 +0000 2022",
+		"id": 1521197724179251200,
+		"id_str": "1521197724179251200",
+		"full_text": "HAPPENING TONIGHT! Join us for 1st Mondays, hosted by @Insperity and held at Ballast Point Brewing Miramar. In honor of May being Military Appreciation Month, we’ll share how startups in SD can tap into veteran and military spouse talent.\n\nGet tickets: https://t.co/PsTqVzPJHI https://t.co/YjEWRQ3BS8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Insperity",
+					"name": "Insperity",
+					"id": 149176270,
+					"id_str": "149176270",
+					"indices": [
+						54,
+						64
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PsTqVzPJHI",
+					"expanded_url": "https://lnkd.in/gnvff6tQ",
+					"display_url": "lnkd.in/gnvff6tQ",
+					"indices": [
+						253,
+						276
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1521197722224709600,
+					"id_str": "1521197722224709632",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FRxhRMpWUAAkF99.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRxhRMpWUAAkF99.jpg",
+					"url": "https://t.co/YjEWRQ3BS8",
+					"display_url": "pic.twitter.com/YjEWRQ3BS8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1521197724179251200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521197722224709600,
+					"id_str": "1521197722224709632",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FRxhRMpWUAAkF99.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRxhRMpWUAAkF99.jpg",
+					"url": "https://t.co/YjEWRQ3BS8",
+					"display_url": "pic.twitter.com/YjEWRQ3BS8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1521197724179251200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on May 2nd for 1st Mondays. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKNlw69",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKNlw69",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4626,
+			"friends_count": 2310,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4393,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1650477957",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "HAPPENING TONIGHT! Join us for 1st Mondays, hosted by @Insperity and held at Ballast Point Brewing Miramar. In honor of May being Military Appreciation Month, we’ll share how startups in SD can tap into veteran and military spouse talent.\n\nGet tickets: https://t.co/PsTqVzPJHI https://t.co/YjEWRQ3BS8"
+	}
+]
