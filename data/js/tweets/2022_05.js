37dfd14d16d85184acbf396173f451a6fc4a8bdc
Michael Ryan Norton <michael@michaelryannorton.com>
2022-05-04 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 04 19:47:03 +0000 2022",
+		"id": 1521939480621559800,
+		"id_str": "1521939480621559808",
+		"full_text": "Join Oralia Alvarez, Startup SD board member &amp; Director of Business Development at @MDFinanceSD for the Regenerative Harvest Portfolio info session! This new nationwide financing strategy supports inclusive, regenerative food systems.\nHappening tomorrow: https://t.co/GpUgmBuPdN. https://t.co/M7B9pIe1er",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MDFinanceSD",
+					"name": "Mission Driven Finance",
+					"id": 739934416890265600,
+					"id_str": "739934416890265604",
+					"indices": [
+						87,
+						99
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GpUgmBuPdN",
+					"expanded_url": "http://bit.ly/MDFharvest",
+					"display_url": "bit.ly/MDFharvest",
+					"indices": [
+						259,
+						282
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1521939478591508500,
+					"id_str": "1521939478591508481",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FR8D5GcXEAEsI-D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR8D5GcXEAEsI-D.jpg",
+					"url": "https://t.co/M7B9pIe1er",
+					"display_url": "pic.twitter.com/M7B9pIe1er",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1521939480621559808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521939478591508500,
+					"id_str": "1521939478591508481",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FR8D5GcXEAEsI-D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR8D5GcXEAEsI-D.jpg",
+					"url": "https://t.co/M7B9pIe1er",
+					"display_url": "pic.twitter.com/M7B9pIe1er",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1521939480621559808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on May 2nd for 1st Mondays. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/dCUHKNlw69",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dCUHKNlw69",
+							"expanded_url": "https://startupsd.org/",
+							"display_url": "startupsd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4629,
+			"friends_count": 2309,
+			"listed_count": 119,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4394,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1650477957",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Join Oralia Alvarez, Startup SD board member &amp; Director of Business Development at @MDFinanceSD for the Regenerative Harvest Portfolio info session! This new nationwide financing strategy supports inclusive, regenerative food systems.\nHappening tomorrow: https://t.co/GpUgmBuPdN. https://t.co/M7B9pIe1er"
+	},
+	{
 		"created_at": "Mon May 02 18:39:34 +0000 2022",
 		"id": 1521197724179251200,
 		"id_str": "1521197724179251200",
