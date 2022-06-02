30c83455af3a0127277f912a977e0da0b00a5908
Michael Ryan Norton <michael@michaelryannorton.com>
2022-06-01 17:17:03 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4219,
-	"created_at": "Wed Jun 01 00:17:02 +0000 2022",
+	"tweets": 4221,
+	"created_at": "Thu Jun 02 00:17:02 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_06.js
@@ -0,0 +1,339 @@
+Grailbird.data.tweets_2022_06 = 
+[
+	{
+		"created_at": "Wed Jun 01 17:53:31 +0000 2022",
+		"id": 1532057771788492800,
+		"id_str": "1532057771788492800",
+		"full_text": "The Inaugural @BBBPacificSW — Diversity, Equity and Inclusion Summit, in partnership with @GoDaddy will be happening TOMORROW! As a proud community sponsor, we are excited for this event and encourage you to attend.\n\nLearn more &amp; register: https://t.co/yVcmEfLCPy\n\n#DEI #Diversity https://t.co/KcCDhZlgox",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			284
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DEI",
+					"indices": [
+						269,
+						273
+					]
+				},
+				{
+					"text": "Diversity",
+					"indices": [
+						274,
+						284
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBBPacificSW",
+					"name": "BBB Serving the Pacific Southwest",
+					"id": 21798919,
+					"id_str": "21798919",
+					"indices": [
+						14,
+						27
+					]
+				},
+				{
+					"screen_name": "GoDaddy",
+					"name": "GoDaddy",
+					"id": 14949454,
+					"id_str": "14949454",
+					"indices": [
+						90,
+						98
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yVcmEfLCPy",
+					"expanded_url": "http://ow.ly/S0Qv50JnsxN",
+					"display_url": "ow.ly/S0Qv50JnsxN",
+					"indices": [
+						244,
+						267
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532057770333294600,
+					"id_str": "1532057770333294593",
+					"indices": [
+						285,
+						308
+					],
+					"media_url": "http://pbs.twimg.com/media/FUL2bMgX0AEpB0o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUL2bMgX0AEpB0o.jpg",
+					"url": "https://t.co/KcCDhZlgox",
+					"display_url": "pic.twitter.com/KcCDhZlgox",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1532057771788492800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532057770333294600,
+					"id_str": "1532057770333294593",
+					"indices": [
+						285,
+						308
+					],
+					"media_url": "http://pbs.twimg.com/media/FUL2bMgX0AEpB0o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUL2bMgX0AEpB0o.jpg",
+					"url": "https://t.co/KcCDhZlgox",
+					"display_url": "pic.twitter.com/KcCDhZlgox",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1532057771788492800/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4659,
+			"friends_count": 2310,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4400,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Inaugural @BBBPacificSW — Diversity, Equity and Inclusion Summit, in partnership with @GoDaddy will be happening TOMORROW! As a proud community sponsor, we are excited for this event and encourage you to attend.\n\nLearn more &amp; register: https://t.co/yVcmEfLCPy\n\n#DEI #Diversity https://t.co/KcCDhZlgox"
+	},
+	{
+		"created_at": "Wed Jun 01 16:10:55 +0000 2022",
+		"id": 1532031951699320800,
+		"id_str": "1532031951699320836",
+		"full_text": "@TEDxSanDiego We can't wait! See you there! #SDSW2022",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			53
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						44,
+						53
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TEDxSanDiego",
+					"name": "TEDxSanDiego",
+					"id": 115168119,
+					"id_str": "115168119",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1531681536810135600,
+		"in_reply_to_status_id_str": "1531681536810135552",
+		"in_reply_to_user_id": 115168119,
+		"in_reply_to_user_id_str": "115168119",
+		"in_reply_to_screen_name": "TEDxSanDiego",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4659,
+			"friends_count": 2310,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4400,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@TEDxSanDiego We can't wait! See you there! #SDSW2022"
+	}
+]
