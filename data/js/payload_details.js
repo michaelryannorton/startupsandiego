bf0cb53ae66926f6e4c15fe7b5f375fdfd0e9f02
Michael Ryan Norton <michael@michaelryannorton.com>
2022-06-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4221,
-	"created_at": "Thu Jun 02 00:17:02 +0000 2022",
+	"tweets": 4222,
+	"created_at": "Fri Jun 03 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 02 15:08:10 +0000 2022",
+		"id": 1532378548136251400,
+		"id_str": "1532378548136251392",
+		"full_text": "Be part of the Startup San Diego Entrepreneur-in-Residence (EIR) program and get involved with the startup community, applications are now officially OPEN! Work with the best, learn from the best, and build with the best!\n\nApply to be an EIR by June 10th: https://t.co/oAqQkMVzbC https://t.co/llALfgcnJf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oAqQkMVzbC",
+					"expanded_url": "https://lnkd.in/gJbA6BQv",
+					"display_url": "lnkd.in/gJbA6BQv",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1532378544256450600,
+					"id_str": "1532378544256450560",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FUQaKsfWAAAv1L1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUQaKsfWAAAv1L1.jpg",
+					"url": "https://t.co/llALfgcnJf",
+					"display_url": "pic.twitter.com/llALfgcnJf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1532378548136251392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532378544256450600,
+					"id_str": "1532378544256450560",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FUQaKsfWAAAv1L1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUQaKsfWAAAv1L1.jpg",
+					"url": "https://t.co/llALfgcnJf",
+					"display_url": "pic.twitter.com/llALfgcnJf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1532378548136251392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4661,
+			"friends_count": 2310,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4401,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Be part of the Startup San Diego Entrepreneur-in-Residence (EIR) program and get involved with the startup community, applications are now officially OPEN! Work with the best, learn from the best, and build with the best!\n\nApply to be an EIR by June 10th: https://t.co/oAqQkMVzbC https://t.co/llALfgcnJf"
+	},
+	{
 		"created_at": "Wed Jun 01 17:53:31 +0000 2022",
 		"id": 1532057771788492800,
 		"id_str": "1532057771788492800",
