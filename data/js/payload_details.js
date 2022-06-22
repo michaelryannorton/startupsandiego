c27db8e0f9612368d23b3eee1aa51cc4c6c571f2
Michael Ryan Norton <michael@michaelryannorton.com>
2022-06-21 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4224,
-	"created_at": "Wed Jun 15 00:17:02 +0000 2022",
+	"tweets": 4225,
+	"created_at": "Wed Jun 22 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 21 16:53:11 +0000 2022",
+		"id": 1539290342905741300,
+		"id_str": "1539290342905741317",
+		"full_text": "Get in front of potential investors, practice your #startup pitch! We're excited to announce that our annual San Diego Startup Week Pitch Competition applications are now OPEN! In partnership with @SDSU, @CONNECTinSD &amp; @nufundventures. Apply by July 31st: https://t.co/QTBoc5EY5X https://t.co/0JultI0VYR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						51,
+						59
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDSU",
+					"name": "San Diego State University",
+					"id": 16739786,
+					"id_str": "16739786",
+					"indices": [
+						197,
+						202
+					]
+				},
+				{
+					"screen_name": "CONNECTinSD",
+					"name": "Connect",
+					"id": 319294242,
+					"id_str": "319294242",
+					"indices": [
+						204,
+						216
+					]
+				},
+				{
+					"screen_name": "nufundventures",
+					"name": "NuFund",
+					"id": 893178238679236600,
+					"id_str": "893178238679236612",
+					"indices": [
+						223,
+						238
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QTBoc5EY5X",
+					"expanded_url": "http://ow.ly/7baz50JCU1N",
+					"display_url": "ow.ly/7baz50JCU1N",
+					"indices": [
+						260,
+						283
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1539290339906932700,
+					"id_str": "1539290339906932739",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FVyoaJxX0AMWPO4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVyoaJxX0AMWPO4.jpg",
+					"url": "https://t.co/0JultI0VYR",
+					"display_url": "pic.twitter.com/0JultI0VYR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1539290342905741317/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1539290339906932700,
+					"id_str": "1539290339906932739",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FVyoaJxX0AMWPO4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVyoaJxX0AMWPO4.jpg",
+					"url": "https://t.co/0JultI0VYR",
+					"display_url": "pic.twitter.com/0JultI0VYR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1539290342905741317/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4663,
+			"friends_count": 2308,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4404,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get in front of potential investors, practice your #startup pitch! We're excited to announce that our annual San Diego Startup Week Pitch Competition applications are now OPEN! In partnership with @SDSU, @CONNECTinSD &amp; @nufundventures. Apply by July 31st: https://t.co/QTBoc5EY5X https://t.co/0JultI0VYR"
+	},
+	{
 		"created_at": "Tue Jun 14 16:39:45 +0000 2022",
 		"id": 1536750247669547000,
 		"id_str": "1536750247669547016",
