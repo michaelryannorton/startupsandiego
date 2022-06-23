8828b4bc4168339b174c7f0042599130dee5b527
Michael Ryan Norton <michael@michaelryannorton.com>
2022-06-22 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4225,
-	"created_at": "Wed Jun 22 00:17:02 +0000 2022",
+	"tweets": 4226,
+	"created_at": "Thu Jun 23 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 22 15:42:08 +0000 2022",
+		"id": 1539634854048010200,
+		"id_str": "1539634854048010242",
+		"full_text": "Are you a #femalefounder that's ready to pitch your business to #womeninvestors? Would you like the chance to win a $10k investment? Then apply for Stella's Women's Fast Pitch Competition! Hurry, applications close TONIGHT. \n\n⭐ Apply here: https://t.co/qrnqt6RmiO \n\n#WVS2022 https://t.co/u1lrF2h0a6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "femalefounder",
+					"indices": [
+						10,
+						24
+					]
+				},
+				{
+					"text": "womeninvestors",
+					"indices": [
+						64,
+						79
+					]
+				},
+				{
+					"text": "WVS2022",
+					"indices": [
+						266,
+						274
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qrnqt6RmiO",
+					"expanded_url": "https://bit.ly/394IAQA",
+					"display_url": "bit.ly/394IAQA",
+					"indices": [
+						240,
+						263
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1539634851841818600,
+					"id_str": "1539634851841818625",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FV3hvYvXwAEO6K_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV3hvYvXwAEO6K_.jpg",
+					"url": "https://t.co/u1lrF2h0a6",
+					"display_url": "pic.twitter.com/u1lrF2h0a6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1539634854048010242/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1539634851841818600,
+					"id_str": "1539634851841818625",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FV3hvYvXwAEO6K_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FV3hvYvXwAEO6K_.jpg",
+					"url": "https://t.co/u1lrF2h0a6",
+					"display_url": "pic.twitter.com/u1lrF2h0a6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1539634854048010242/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4662,
+			"friends_count": 2308,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4405,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Are you a #femalefounder that's ready to pitch your business to #womeninvestors? Would you like the chance to win a $10k investment? Then apply for Stella's Women's Fast Pitch Competition! Hurry, applications close TONIGHT. \n\n⭐ Apply here: https://t.co/qrnqt6RmiO \n\n#WVS2022 https://t.co/u1lrF2h0a6"
+	},
+	{
 		"created_at": "Tue Jun 21 16:53:11 +0000 2022",
 		"id": 1539290342905741300,
 		"id_str": "1539290342905741317",
