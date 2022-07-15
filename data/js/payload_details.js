9a886601c007a88bac7be570d877949e4cad0f9c
Michael Ryan Norton <michael@michaelryannorton.com>
2022-07-14 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4233,
-	"created_at": "Thu Jul 14 00:17:02 +0000 2022",
+	"tweets": 4234,
+	"created_at": "Fri Jul 15 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Thu Jul 14 17:46:58 +0000 2022",
+		"id": 1547638801517842400,
+		"id_str": "1547638801517842434",
+		"full_text": "Did you know that #SDSW2022 features 8 event tracks throughout the four days? Each day will focus on Founder &amp; People, Design &amp; Developer, Data &amp; Customer and Marketing &amp; Sales. Meet experts and gain relevant connections!\n\n🚀Get your Innovation Pass: https://t.co/pjEYxLuttX https://t.co/71zL9aP1at",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			289
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						18,
+						27
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pjEYxLuttX",
+					"expanded_url": "http://startupsd.org/sandiegostartupweek/",
+					"display_url": "startupsd.org/sandiegostartu…",
+					"indices": [
+						266,
+						289
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1547638799043158000,
+					"id_str": "1547638799043158017",
+					"indices": [
+						290,
+						313
+					],
+					"media_url": "http://pbs.twimg.com/media/FXpRSbmUYAEuftz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXpRSbmUYAEuftz.jpg",
+					"url": "https://t.co/71zL9aP1at",
+					"display_url": "pic.twitter.com/71zL9aP1at",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1547638801517842434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1547638799043158000,
+					"id_str": "1547638799043158017",
+					"indices": [
+						290,
+						313
+					],
+					"media_url": "http://pbs.twimg.com/media/FXpRSbmUYAEuftz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXpRSbmUYAEuftz.jpg",
+					"url": "https://t.co/71zL9aP1at",
+					"display_url": "pic.twitter.com/71zL9aP1at",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1547638801517842434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4662,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Did you know that #SDSW2022 features 8 event tracks throughout the four days? Each day will focus on Founder &amp; People, Design &amp; Developer, Data &amp; Customer and Marketing &amp; Sales. Meet experts and gain relevant connections!\n\n🚀Get your Innovation Pass: https://t.co/pjEYxLuttX https://t.co/71zL9aP1at"
+	},
+	{
 		"created_at": "Wed Jul 13 15:37:38 +0000 2022",
 		"id": 1547243863751917600,
 		"id_str": "1547243863751917568",
