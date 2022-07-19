74783747b6fb4cbc42d1f60da63c33da88aa3ecb
Michael Ryan Norton <michael@michaelryannorton.com>
2022-07-18 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Mon Jul 18 15:42:01 +0000 2022",
+		"id": 1549056907381317600,
+		"id_str": "1549056907381317632",
+		"full_text": "We have exciting news, this year's @SDStartupWeek Pitch Competition will be giving a CASH PRIZE to the winner! Interested in going head-to-head in one of SD's toughest #pitchcompetition? Compete for the spot as heavyweight champ. Sign up by July 31st: https://t.co/mpoh3hFJhc https://t.co/bhNc9TTYw8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pitchcompetition",
+					"indices": [
+						168,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDStartupWeek",
+					"name": "San Diego Startup Week",
+					"id": 2339762118,
+					"id_str": "2339762118",
+					"indices": [
+						35,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mpoh3hFJhc",
+					"expanded_url": "http://ow.ly/8Jg550JYhKZ",
+					"display_url": "ow.ly/8Jg550JYhKZ",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1549056905238159400,
+					"id_str": "1549056905238159360",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/FX9bDE2WIAA4lcw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FX9bDE2WIAA4lcw.jpg",
+					"url": "https://t.co/bhNc9TTYw8",
+					"display_url": "pic.twitter.com/bhNc9TTYw8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1549056907381317632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1549056905238159400,
+					"id_str": "1549056905238159360",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/FX9bDE2WIAA4lcw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FX9bDE2WIAA4lcw.jpg",
+					"url": "https://t.co/bhNc9TTYw8",
+					"display_url": "pic.twitter.com/bhNc9TTYw8",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1549056907381317632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4664,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4415,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We have exciting news, this year's @SDStartupWeek Pitch Competition will be giving a CASH PRIZE to the winner! Interested in going head-to-head in one of SD's toughest #pitchcompetition? Compete for the spot as heavyweight champ. Sign up by July 31st: https://t.co/mpoh3hFJhc https://t.co/bhNc9TTYw8"
+	},
+	{
 		"created_at": "Fri Jul 15 21:13:44 +0000 2022",
 		"id": 1548053222106902500,
 		"id_str": "1548053222106902530",
