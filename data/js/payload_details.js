824f95c39175341fdcd3f1c262c8a49997248582
Michael Ryan Norton <michael@michaelryannorton.com>
2022-07-19 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4236,
-	"created_at": "Tue Jul 19 00:17:02 +0000 2022",
+	"tweets": 4237,
+	"created_at": "Wed Jul 20 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Tue Jul 19 16:26:18 +0000 2022",
+		"id": 1549430440804622300,
+		"id_str": "1549430440804622336",
+		"full_text": "🎉 Congratulations to @StartupSanDiego Entrepreneur-in-Residence, @TaraFalcone (CEO &amp; Founder of @ReasonInvest), for being chosen as part of @Techstars Anywhere Accelerator 2022 Summer Class!\n\nRead the official press release: https://t.co/wza5vCVFih\n\n#startup #femalefounders https://t.co/Jq9aCryVWq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						254,
+						262
+					]
+				},
+				{
+					"text": "femalefounders",
+					"indices": [
+						263,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "StartupSanDiego",
+					"name": "Startup San Diego",
+					"id": 3222944604,
+					"id_str": "3222944604",
+					"indices": [
+						21,
+						37
+					]
+				},
+				{
+					"screen_name": "TaraFalcone",
+					"name": "Tara Falcone",
+					"id": 4047907342,
+					"id_str": "4047907342",
+					"indices": [
+						65,
+						77
+					]
+				},
+				{
+					"screen_name": "ReasonInvest",
+					"name": "Reason",
+					"id": 1412533501639872500,
+					"id_str": "1412533501639872512",
+					"indices": [
+						100,
+						113
+					]
+				},
+				{
+					"screen_name": "Techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						144,
+						154
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/wza5vCVFih",
+					"expanded_url": "http://ow.ly/PflA50JZmXL",
+					"display_url": "ow.ly/PflA50JZmXL",
+					"indices": [
+						229,
+						252
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1549430439202496500,
+					"id_str": "1549430439202496513",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FYCuxnTXoAEVRr4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYCuxnTXoAEVRr4.jpg",
+					"url": "https://t.co/Jq9aCryVWq",
+					"display_url": "pic.twitter.com/Jq9aCryVWq",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1549430440804622336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1549430439202496500,
+					"id_str": "1549430439202496513",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FYCuxnTXoAEVRr4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYCuxnTXoAEVRr4.jpg",
+					"url": "https://t.co/Jq9aCryVWq",
+					"display_url": "pic.twitter.com/Jq9aCryVWq",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1549430440804622336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 418,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4666,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4416,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎉 Congratulations to @StartupSanDiego Entrepreneur-in-Residence, @TaraFalcone (CEO &amp; Founder of @ReasonInvest), for being chosen as part of @Techstars Anywhere Accelerator 2022 Summer Class!\n\nRead the official press release: https://t.co/wza5vCVFih\n\n#startup #femalefounders https://t.co/Jq9aCryVWq"
+	},
+	{
 		"created_at": "Mon Jul 18 15:42:01 +0000 2022",
 		"id": 1549056907381317600,
 		"id_str": "1549056907381317632",
