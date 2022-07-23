0236567cd374f46b25ddf065776e4af5428906ca
Michael Ryan Norton <michael@michaelryannorton.com>
2022-07-22 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4238,
-	"created_at": "Fri Jul 22 00:17:02 +0000 2022",
+	"tweets": 4239,
+	"created_at": "Sat Jul 23 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 22 16:42:27 +0000 2022",
+		"id": 1550521666224021500,
+		"id_str": "1550521666224021504",
+		"full_text": "We're inviting some of the top #startup executives, founders, CEOs, and experts to #SDSW2022! Take a look at some of our featured speakers and learn from the best. Connect with these #leaders at this year's SD Startup Week.\n\n🎟️ Get your Innovation Passes: https://t.co/JaoSqFcLLS https://t.co/Tm7lfy8zwQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						31,
+						39
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						83,
+						92
+					]
+				},
+				{
+					"text": "leaders",
+					"indices": [
+						183,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JaoSqFcLLS",
+					"expanded_url": "http://ow.ly/uz9X50K2ltP",
+					"display_url": "ow.ly/uz9X50K2ltP",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1550521663019749400,
+					"id_str": "1550521663019749376",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FYSPPMzXEAAqE09.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYSPPMzXEAAqE09.jpg",
+					"url": "https://t.co/Tm7lfy8zwQ",
+					"display_url": "pic.twitter.com/Tm7lfy8zwQ",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1550521666224021504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1550521663019749400,
+					"id_str": "1550521663019749376",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/FYSPPMzXEAAqE09.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYSPPMzXEAAqE09.jpg",
+					"url": "https://t.co/Tm7lfy8zwQ",
+					"display_url": "pic.twitter.com/Tm7lfy8zwQ",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1550521666224021504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4665,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4418,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're inviting some of the top #startup executives, founders, CEOs, and experts to #SDSW2022! Take a look at some of our featured speakers and learn from the best. Connect with these #leaders at this year's SD Startup Week.\n\n🎟️ Get your Innovation Passes: https://t.co/JaoSqFcLLS https://t.co/Tm7lfy8zwQ"
+	},
+	{
 		"created_at": "Thu Jul 21 19:06:43 +0000 2022",
 		"id": 1550195586384728000,
 		"id_str": "1550195586384728065",
