b7b4646d641eec24e6f6b56c1f8d2a0f57094a93
Michael Ryan Norton <michael@michaelryannorton.com>
2022-08-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4243,
-	"created_at": "Tue Aug 02 00:17:02 +0000 2022",
+	"tweets": 4244,
+	"created_at": "Wed Aug 03 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Tue Aug 02 15:28:04 +0000 2022",
+		"id": 1554489214124937200,
+		"id_str": "1554489214124937216",
+		"full_text": "Due to popular demand, we've decided to extend the deadline for our @SDStartupWeek Heavyweight Pitch Competition &amp; Tournament! You have until Sunday, August 14th to submit your application and have a chance to win a CASH PRIZE.\n\n🏆 Sign-up: https://t.co/UFDukjg8CU\n\n#SDSW2022 https://t.co/9KW4jhmImC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						269,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SDStartupWeek",
+					"name": "San Diego Startup Week",
+					"id": 2339762118,
+					"id_str": "2339762118",
+					"indices": [
+						68,
+						82
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/UFDukjg8CU",
+					"expanded_url": "http://ow.ly/g51750K9Gx2",
+					"display_url": "ow.ly/g51750K9Gx2",
+					"indices": [
+						244,
+						267
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1554489211151081500,
+					"id_str": "1554489211151081474",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FZKns5XWYAIwzQ0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZKns5XWYAIwzQ0.jpg",
+					"url": "https://t.co/9KW4jhmImC",
+					"display_url": "pic.twitter.com/9KW4jhmImC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1554489214124937216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1554489211151081500,
+					"id_str": "1554489211151081474",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FZKns5XWYAIwzQ0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZKns5XWYAIwzQ0.jpg",
+					"url": "https://t.co/9KW4jhmImC",
+					"display_url": "pic.twitter.com/9KW4jhmImC",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1554489214124937216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4683,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2424,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4423,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Due to popular demand, we've decided to extend the deadline for our @SDStartupWeek Heavyweight Pitch Competition &amp; Tournament! You have until Sunday, August 14th to submit your application and have a chance to win a CASH PRIZE.\n\n🏆 Sign-up: https://t.co/UFDukjg8CU\n\n#SDSW2022 https://t.co/9KW4jhmImC"
+	},
+	{
 		"created_at": "Mon Aug 01 16:08:20 +0000 2022",
 		"id": 1554136958892494800,
 		"id_str": "1554136958892494848",
