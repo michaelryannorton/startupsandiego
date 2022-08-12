009150d32b31a6224c7f7ef11db156953c212860
Michael Ryan Norton <michael@michaelryannorton.com>
2022-08-11 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 11 14:36:23 +0000 2022",
+		"id": 1557737700421034000,
+		"id_str": "1557737700421033992",
+		"full_text": "We want to give a huge THANKS to our pitch competition partners @nufundventures  &amp; @CONNECTinSD for all their support! They'll be helping us evaluate apps &amp; judge during the event. It's not too late to apply, submit by Sunday, August 14th: https://t.co/RqX9WBxw5M\n\n#SDSW2022 https://t.co/q2uEB2yyrg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						273,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nufundventures",
+					"name": "NuFund",
+					"id": 893178238679236600,
+					"id_str": "893178238679236612",
+					"indices": [
+						64,
+						79
+					]
+				},
+				{
+					"screen_name": "CONNECTinSD",
+					"name": "Connect",
+					"id": 319294242,
+					"id_str": "319294242",
+					"indices": [
+						87,
+						99
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RqX9WBxw5M",
+					"expanded_url": "http://ow.ly/p3EJ50KhR40",
+					"display_url": "ow.ly/p3EJ50KhR40",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1557737698151972900,
+					"id_str": "1557737698151972865",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ4yLueVQAE7Hu9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ4yLueVQAE7Hu9.jpg",
+					"url": "https://t.co/q2uEB2yyrg",
+					"display_url": "pic.twitter.com/q2uEB2yyrg",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1557737700421033992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1557737698151972900,
+					"id_str": "1557737698151972865",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ4yLueVQAE7Hu9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ4yLueVQAE7Hu9.jpg",
+					"url": "https://t.co/q2uEB2yyrg",
+					"display_url": "pic.twitter.com/q2uEB2yyrg",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1557737700421033992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4690,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4428,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We want to give a huge THANKS to our pitch competition partners @nufundventures  &amp; @CONNECTinSD for all their support! They'll be helping us evaluate apps &amp; judge during the event. It's not too late to apply, submit by Sunday, August 14th: https://t.co/RqX9WBxw5M\n\n#SDSW2022 https://t.co/q2uEB2yyrg"
+	},
+	{
 		"created_at": "Tue Aug 09 16:18:08 +0000 2022",
 		"id": 1557038529686999000,
 		"id_str": "1557038529686999043",
