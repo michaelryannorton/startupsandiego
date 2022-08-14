80fd9f443fb6241078d5363deb69f29431ff12b3
Michael Ryan Norton <michael@michaelryannorton.com>
2022-08-13 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 13 15:02:30 +0000 2022",
+		"id": 1558469047016112000,
+		"id_str": "1558469047016112128",
+		"full_text": "There's only 1 DAY LEFT to apply for our Startup Incubator program! Level up your #startup and work with growth experts, get mentorship directly from @ryankuder, Managing Director at  @Techstars Anywhere. \n\nApply by Sunday, August 14th: https://t.co/aHTpBDMRZ0\n\n#SDSW2022 https://t.co/xqEXOHqkmp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						82,
+						90
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						262,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ryankuder",
+					"name": "Ryan Kuder 🦄🌊🌮",
+					"id": 6837002,
+					"id_str": "6837002",
+					"indices": [
+						150,
+						160
+					]
+				},
+				{
+					"screen_name": "Techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						184,
+						194
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/aHTpBDMRZ0",
+					"expanded_url": "http://ow.ly/RSTS50Kjklw",
+					"display_url": "ow.ly/RSTS50Kjklw",
+					"indices": [
+						237,
+						260
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1558469043497083000,
+					"id_str": "1558469043497082881",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDLVoMWIAEmQHe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDLVoMWIAEmQHe.jpg",
+					"url": "https://t.co/xqEXOHqkmp",
+					"display_url": "pic.twitter.com/xqEXOHqkmp",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1558469047016112128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1558469043497083000,
+					"id_str": "1558469043497082881",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDLVoMWIAEmQHe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDLVoMWIAEmQHe.jpg",
+					"url": "https://t.co/xqEXOHqkmp",
+					"display_url": "pic.twitter.com/xqEXOHqkmp",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1558469047016112128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4697,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4430,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "There's only 1 DAY LEFT to apply for our Startup Incubator program! Level up your #startup and work with growth experts, get mentorship directly from @ryankuder, Managing Director at  @Techstars Anywhere. \n\nApply by Sunday, August 14th: https://t.co/aHTpBDMRZ0\n\n#SDSW2022 https://t.co/xqEXOHqkmp"
+	},
+	{
 		"created_at": "Fri Aug 12 15:23:44 +0000 2022",
 		"id": 1558112004526772200,
 		"id_str": "1558112004526772226",
