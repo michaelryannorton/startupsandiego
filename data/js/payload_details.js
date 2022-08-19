e790258a3b417c6cfd3749758627c35b0a8505ab
Michael Ryan Norton <michael@michaelryannorton.com>
2022-08-18 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4252,
-	"created_at": "Thu Aug 18 00:17:02 +0000 2022",
+	"tweets": 4253,
+	"created_at": "Fri Aug 19 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 18 19:14:40 +0000 2022",
+		"id": 1560344446956339200,
+		"id_str": "1560344446956339210",
+		"full_text": "The final day for #SDSW2022 will be hosted at the beautiful San Diego Tech Center by Alexandria Real Estate Equities, Inc.! Join us for keynotes, Startup Awards, expo, reception &amp; an open air networking garden! Get your Innovation passes today: https://t.co/mgzjhzG3Ba\n\n#SDSW2022 https://t.co/W9Bjv2acU0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						18,
+						27
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						274,
+						283
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mgzjhzG3Ba",
+					"expanded_url": "http://ow.ly/wPmV50KniyQ",
+					"display_url": "ow.ly/wPmV50KniyQ",
+					"indices": [
+						249,
+						272
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1560344443613548500,
+					"id_str": "1560344443613548547",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/Fad1ASDXEAMXymk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fad1ASDXEAMXymk.jpg",
+					"url": "https://t.co/W9Bjv2acU0",
+					"display_url": "pic.twitter.com/W9Bjv2acU0",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1560344446956339210/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1560344443613548500,
+					"id_str": "1560344443613548547",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/Fad1ASDXEAMXymk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fad1ASDXEAMXymk.jpg",
+					"url": "https://t.co/W9Bjv2acU0",
+					"display_url": "pic.twitter.com/W9Bjv2acU0",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1560344446956339210/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4705,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4432,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The final day for #SDSW2022 will be hosted at the beautiful San Diego Tech Center by Alexandria Real Estate Equities, Inc.! Join us for keynotes, Startup Awards, expo, reception &amp; an open air networking garden! Get your Innovation passes today: https://t.co/mgzjhzG3Ba\n\n#SDSW2022 https://t.co/W9Bjv2acU0"
+	},
+	{
 		"created_at": "Wed Aug 17 15:25:30 +0000 2022",
 		"id": 1559924387880599600,
 		"id_str": "1559924387880599556",
