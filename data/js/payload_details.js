c162d577c5e5bd50220224bd54adf3e3b093c921
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4257,
-	"created_at": "Wed Aug 31 00:17:02 +0000 2022",
+	"tweets": 4258,
+	"created_at": "Sat Sep 03 00:17:02 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_09.js
@@ -0,0 +1,214 @@
+Grailbird.data.tweets_2022_09 = 
+[
+	{
+		"created_at": "Fri Sep 02 17:03:05 +0000 2022",
+		"id": 1565747149769478100,
+		"id_str": "1565747149769478146",
+		"full_text": "Today is the LAST DAY to apply to our Startup Pitch Competition &amp; Tournament! The winner will get a $10,000 grant provided by ALEPH ONE! 2nd and 3rd place will get @LabsBoost office hours.\n\nApply by TONIGHT 11:59 pm PST: https://t.co/f5JBGxpsZs\n\n#SDSW2022 #pitchcompetition https://t.co/8gMW7D7vJ3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						250,
+						259
+					]
+				},
+				{
+					"text": "pitchcompetition",
+					"indices": [
+						260,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LabsBoost",
+					"name": "LogicBoostLabs",
+					"id": 1182731644119109600,
+					"id_str": "1182731644119109632",
+					"indices": [
+						168,
+						178
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/f5JBGxpsZs",
+					"expanded_url": "https://airtable.com/shrnoG1JfxW1AeUHr",
+					"display_url": "airtable.com/shrnoG1JfxW1Ae…",
+					"indices": [
+						225,
+						248
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1565747147819057200,
+					"id_str": "1565747147819057154",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FbqmvDnWAAIxG6y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbqmvDnWAAIxG6y.jpg",
+					"url": "https://t.co/8gMW7D7vJ3",
+					"display_url": "pic.twitter.com/8gMW7D7vJ3",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1565747149769478146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1565747147819057200,
+					"id_str": "1565747147819057154",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FbqmvDnWAAIxG6y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbqmvDnWAAIxG6y.jpg",
+					"url": "https://t.co/8gMW7D7vJ3",
+					"display_url": "pic.twitter.com/8gMW7D7vJ3",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1565747149769478146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4719,
+			"friends_count": 2304,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4438,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is the LAST DAY to apply to our Startup Pitch Competition &amp; Tournament! The winner will get a $10,000 grant provided by ALEPH ONE! 2nd and 3rd place will get @LabsBoost office hours.\n\nApply by TONIGHT 11:59 pm PST: https://t.co/f5JBGxpsZs\n\n#SDSW2022 #pitchcompetition https://t.co/8gMW7D7vJ3"
+	}
+]
