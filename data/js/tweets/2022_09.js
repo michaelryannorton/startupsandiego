80c43ca8dd8881541e311da2160a93c19d60b80f
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-06 17:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,267 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 06 16:06:37 +0000 2022",
+		"id": 1567182492011987000,
+		"id_str": "1567182492011986944",
+		"full_text": "We've got an exciting line-up for #1stMondays! The night will be all about incubators, accelerators, and #startup growth. Connect with the Startup ecosystem, get tickets: https://t.co/sQ3Joxmopj\n\n@TaraFalcone @ReasonInvest  @ryankuder @Techstars @gotSPARE @wearestellaco https://t.co/futns5Abzu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						34,
+						45
+					]
+				},
+				{
+					"text": "startup",
+					"indices": [
+						105,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TaraFalcone",
+					"name": "Tara Falcone",
+					"id": 4047907342,
+					"id_str": "4047907342",
+					"indices": [
+						196,
+						208
+					]
+				},
+				{
+					"screen_name": "ReasonInvest",
+					"name": "Reason",
+					"id": 1412533501639872500,
+					"id_str": "1412533501639872512",
+					"indices": [
+						209,
+						222
+					]
+				},
+				{
+					"screen_name": "ryankuder",
+					"name": "Ryan Kuder 🦄🌊🌮",
+					"id": 6837002,
+					"id_str": "6837002",
+					"indices": [
+						224,
+						234
+					]
+				},
+				{
+					"screen_name": "Techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						235,
+						245
+					]
+				},
+				{
+					"screen_name": "gotSPARE",
+					"name": "SPARE.",
+					"id": 2518515962,
+					"id_str": "2518515962",
+					"indices": [
+						246,
+						255
+					]
+				},
+				{
+					"screen_name": "wearestellaco",
+					"name": "Stella",
+					"id": 1480627296,
+					"id_str": "1480627296",
+					"indices": [
+						256,
+						270
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/sQ3Joxmopj",
+					"expanded_url": "http://ow.ly/i7aQ50KBgqQ",
+					"display_url": "ow.ly/i7aQ50KBgqQ",
+					"indices": [
+						171,
+						194
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1567182490065830000,
+					"id_str": "1567182490065829889",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb_AK-KWAAEaxSL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb_AK-KWAAEaxSL.jpg",
+					"url": "https://t.co/futns5Abzu",
+					"display_url": "pic.twitter.com/futns5Abzu",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1567182492011986944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1567182490065830000,
+					"id_str": "1567182490065829889",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/Fb_AK-KWAAEaxSL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fb_AK-KWAAEaxSL.jpg",
+					"url": "https://t.co/futns5Abzu",
+					"display_url": "pic.twitter.com/futns5Abzu",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1567182492011986944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4721,
+			"friends_count": 2305,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4439,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We've got an exciting line-up for #1stMondays! The night will be all about incubators, accelerators, and #startup growth. Connect with the Startup ecosystem, get tickets: https://t.co/sQ3Joxmopj\n\n@TaraFalcone @ReasonInvest  @ryankuder @Techstars @gotSPARE @wearestellaco https://t.co/futns5Abzu"
+	},
+	{
 		"created_at": "Fri Sep 02 17:03:05 +0000 2022",
 		"id": 1565747149769478100,
 		"id_str": "1565747149769478146",
