f13c018c05c66caf513f68c343fde9be6025ef7c
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-14 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4264,
-	"created_at": "Wed Sep 14 00:17:02 +0000 2022",
+	"tweets": 4265,
+	"created_at": "Thu Sep 15 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 14 16:00:07 +0000 2022",
+		"id": 1570079958277705700,
+		"id_str": "1570079958277705729",
+		"full_text": "🎉 Congratulations to our Executive Director, @lexaRaeN, for winning @SDbusiness Non-Profit Executive of the Year - Mid-Size Category Award!\n\n➡️ View the full list: https://t.co/yDnIFbPh6L\n\n#StartupSD #startupleadership #nonprofit https://t.co/C3AJK4HIJE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSD",
+					"indices": [
+						189,
+						199
+					]
+				},
+				{
+					"text": "startupleadership",
+					"indices": [
+						200,
+						218
+					]
+				},
+				{
+					"text": "nonprofit",
+					"indices": [
+						219,
+						229
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lexaRaeN",
+					"name": "Alexa-Rae Navarro",
+					"id": 1666344193,
+					"id_str": "1666344193",
+					"indices": [
+						45,
+						54
+					]
+				},
+				{
+					"screen_name": "SDbusiness",
+					"name": "San Diego Business Journal",
+					"id": 24221330,
+					"id_str": "24221330",
+					"indices": [
+						68,
+						79
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/yDnIFbPh6L",
+					"expanded_url": "http://ow.ly/sxqM50KJgzh",
+					"display_url": "ow.ly/sxqM50KJgzh",
+					"indices": [
+						164,
+						187
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1570079956293517300,
+					"id_str": "1570079956293517312",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/FcoLZs_WAAASQfF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcoLZs_WAAASQfF.jpg",
+					"url": "https://t.co/C3AJK4HIJE",
+					"display_url": "pic.twitter.com/C3AJK4HIJE",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1570079958277705729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570079956293517300,
+					"id_str": "1570079956293517312",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/FcoLZs_WAAASQfF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcoLZs_WAAASQfF.jpg",
+					"url": "https://t.co/C3AJK4HIJE",
+					"display_url": "pic.twitter.com/C3AJK4HIJE",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1570079958277705729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4734,
+			"friends_count": 2303,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4447,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎉 Congratulations to our Executive Director, @lexaRaeN, for winning @SDbusiness Non-Profit Executive of the Year - Mid-Size Category Award!\n\n➡️ View the full list: https://t.co/yDnIFbPh6L\n\n#StartupSD #startupleadership #nonprofit https://t.co/C3AJK4HIJE"
+	},
+	{
 		"created_at": "Tue Sep 13 17:05:23 +0000 2022",
 		"id": 1569733996967493600,
 		"id_str": "1569733996967493634",
