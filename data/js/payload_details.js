cd981eed08fcf0a70fc6802519fd63252edd92a1
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-15 17:17:09 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4265,
-	"created_at": "Thu Sep 15 00:17:02 +0000 2022",
+	"tweets": 4266,
+	"created_at": "Fri Sep 16 00:17:07 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 15 17:57:39 +0000 2022",
+		"id": 1570471925033345000,
+		"id_str": "1570471925033345026",
+		"full_text": "🎟️ DAY PASSES are now available for #SDSW2022! If you're only looking to go during a specific track, these tickets are perfect for you. Get a day pass and get full access to workshops, keynotes, and more! Tickets: https://t.co/bv9kL3pdG2\n\n#SDSW2022 #startupevent #StartupSD https://t.co/Lf09tYSGt1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						36,
+						45
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						239,
+						248
+					]
+				},
+				{
+					"text": "startupevent",
+					"indices": [
+						249,
+						262
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						263,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bv9kL3pdG2",
+					"expanded_url": "http://ow.ly/6B9j50KKGCz",
+					"display_url": "ow.ly/6B9j50KKGCz",
+					"indices": [
+						214,
+						237
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1570471922734858200,
+					"id_str": "1570471922734858240",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fctv5JxWAAASOwd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fctv5JxWAAASOwd.jpg",
+					"url": "https://t.co/Lf09tYSGt1",
+					"display_url": "pic.twitter.com/Lf09tYSGt1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1570471925033345026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1570471922734858200,
+					"id_str": "1570471922734858240",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fctv5JxWAAASOwd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fctv5JxWAAASOwd.jpg",
+					"url": "https://t.co/Lf09tYSGt1",
+					"display_url": "pic.twitter.com/Lf09tYSGt1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1570471925033345026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4737,
+			"friends_count": 2303,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4448,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎟️ DAY PASSES are now available for #SDSW2022! If you're only looking to go during a specific track, these tickets are perfect for you. Get a day pass and get full access to workshops, keynotes, and more! Tickets: https://t.co/bv9kL3pdG2\n\n#SDSW2022 #startupevent #StartupSD https://t.co/Lf09tYSGt1"
+	},
+	{
 		"created_at": "Wed Sep 14 16:00:07 +0000 2022",
 		"id": 1570079958277705700,
 		"id_str": "1570079958277705729",
