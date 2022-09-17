cb72d7784e20405363f732dad3291190ba97dbae
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-16 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4266,
-	"created_at": "Fri Sep 16 00:17:07 +0000 2022",
+	"tweets": 4267,
+	"created_at": "Sat Sep 17 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,183 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 16 16:56:34 +0000 2022",
+		"id": 1570818939353174000,
+		"id_str": "1570818939353174018",
+		"full_text": "@wearestellaco @gotSPARE @Techstars @lexaRaeN\n@ryankuder @dontrahughes @TaraFalcone @ReasonInvest",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wearestellaco",
+					"name": "Stella",
+					"id": 1480627296,
+					"id_str": "1480627296",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "gotSPARE",
+					"name": "SPARE.",
+					"id": 2518515962,
+					"id_str": "2518515962",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"screen_name": "Techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						25,
+						35
+					]
+				},
+				{
+					"screen_name": "lexaRaeN",
+					"name": "Alexa-Rae Navarro",
+					"id": 1666344193,
+					"id_str": "1666344193",
+					"indices": [
+						36,
+						45
+					]
+				},
+				{
+					"screen_name": "ryankuder",
+					"name": "Ryan Kuder 🦄🌊🌮",
+					"id": 6837002,
+					"id_str": "6837002",
+					"indices": [
+						46,
+						56
+					]
+				},
+				{
+					"screen_name": "dontrahughes",
+					"name": "D'ontra Hughes",
+					"id": 3586825152,
+					"id_str": "3586825152",
+					"indices": [
+						57,
+						70
+					]
+				},
+				{
+					"screen_name": "TaraFalcone",
+					"name": "Tara Falcone",
+					"id": 4047907342,
+					"id_str": "4047907342",
+					"indices": [
+						71,
+						83
+					]
+				},
+				{
+					"screen_name": "ReasonInvest",
+					"name": "Reason",
+					"id": 1412533501639872500,
+					"id_str": "1412533501639872512",
+					"indices": [
+						84,
+						97
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1570818648276607000,
+		"in_reply_to_status_id_str": "1570818648276606977",
+		"in_reply_to_user_id": 3222944604,
+		"in_reply_to_user_id_str": "3222944604",
+		"in_reply_to_screen_name": "StartupSanDiego",
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4738,
+			"friends_count": 2304,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "qam",
+		"text": "@wearestellaco @gotSPARE @Techstars @lexaRaeN\n@ryankuder @dontrahughes @TaraFalcone @ReasonInvest"
+	},
+	{
 		"created_at": "Thu Sep 15 17:57:39 +0000 2022",
 		"id": 1570471925033345000,
 		"id_str": "1570471925033345026",
