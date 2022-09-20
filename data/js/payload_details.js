d9a59afbf3dcd740cc216d529e50879ba827dbbb
Michael Ryan Norton <michael@michaelryannorton.com>
2022-09-19 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4267,
-	"created_at": "Sat Sep 17 00:17:02 +0000 2022",
+	"tweets": 4268,
+	"created_at": "Tue Sep 20 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Mon Sep 19 15:35:41 +0000 2022",
+		"id": 1571885747535335400,
+		"id_str": "1571885747535335424",
+		"full_text": "This year's Startup Week incubator companies have been chosen! 10 local SD #startups will work closely with @ryankuder, Managing Director at @Techstars Anywhere. Learn what incubators are all about &amp;  gain some knowledge from experts. Join us: https://t.co/86JfwU00Gt\n\n#SDSW2022 https://t.co/KwgauD3tVe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startups",
+					"indices": [
+						75,
+						84
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						273,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ryankuder",
+					"name": "Ryan Kuder 🦄🌊🌮",
+					"id": 6837002,
+					"id_str": "6837002",
+					"indices": [
+						108,
+						118
+					]
+				},
+				{
+					"screen_name": "Techstars",
+					"name": "Techstars",
+					"id": 14277276,
+					"id_str": "14277276",
+					"indices": [
+						141,
+						151
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/86JfwU00Gt",
+					"expanded_url": "http://ow.ly/19j950KN03v",
+					"display_url": "ow.ly/19j950KN03v",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1571885741956800500,
+					"id_str": "1571885741956800514",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FdB1wQzWIAI2Pnb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdB1wQzWIAI2Pnb.jpg",
+					"url": "https://t.co/KwgauD3tVe",
+					"display_url": "pic.twitter.com/KwgauD3tVe",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1571885747535335424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1571885741956800500,
+					"id_str": "1571885741956800514",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FdB1wQzWIAI2Pnb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdB1wQzWIAI2Pnb.jpg",
+					"url": "https://t.co/KwgauD3tVe",
+					"display_url": "pic.twitter.com/KwgauD3tVe",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1571885747535335424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4739,
+			"friends_count": 2302,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4451,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This year's Startup Week incubator companies have been chosen! 10 local SD #startups will work closely with @ryankuder, Managing Director at @Techstars Anywhere. Learn what incubators are all about &amp;  gain some knowledge from experts. Join us: https://t.co/86JfwU00Gt\n\n#SDSW2022 https://t.co/KwgauD3tVe"
+	},
+	{
 		"created_at": "Fri Sep 16 16:56:34 +0000 2022",
 		"id": 1570818939353174000,
 		"id_str": "1570818939353174018",
