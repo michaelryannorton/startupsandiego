53cfd2ee7a6fc81b135b391c6a2bae28306e9c00
Michael Ryan Norton <michael@michaelryannorton.com>
2022-10-22 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4300,
-	"created_at": "Sat Oct 22 00:17:03 +0000 2022",
+	"tweets": 4301,
+	"created_at": "Sun Oct 23 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 22 19:43:07 +0000 2022",
+		"id": 1583906816702177300,
+		"id_str": "1583906816702177280",
+		"full_text": "This is your LAST CHANCE to be an exhibitor at this year's San Diego Startup Week! We only have TEN tables left, and they're going to go fast.\n\n🚀 Get an expo table: https://t.co/B5Q7uMCb3Z\n\n#SDSW2022 #sandiegostartupweek #StartupSD #sandiegoevent #startupevent https://t.co/nw5dlRffeX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			260
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						190,
+						199
+					]
+				},
+				{
+					"text": "sandiegostartupweek",
+					"indices": [
+						200,
+						220
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						221,
+						231
+					]
+				},
+				{
+					"text": "sandiegoevent",
+					"indices": [
+						232,
+						246
+					]
+				},
+				{
+					"text": "startupevent",
+					"indices": [
+						247,
+						260
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/B5Q7uMCb3Z",
+					"expanded_url": "http://ow.ly/1Tru50LinHH",
+					"display_url": "ow.ly/1Tru50LinHH",
+					"indices": [
+						165,
+						188
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1583906814462148600,
+					"id_str": "1583906814462148609",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/Ffsq2xlWQAE_X9j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ffsq2xlWQAE_X9j.jpg",
+					"url": "https://t.co/nw5dlRffeX",
+					"display_url": "pic.twitter.com/nw5dlRffeX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1583906816702177280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1583906814462148600,
+					"id_str": "1583906814462148609",
+					"indices": [
+						261,
+						284
+					],
+					"media_url": "http://pbs.twimg.com/media/Ffsq2xlWQAE_X9j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ffsq2xlWQAE_X9j.jpg",
+					"url": "https://t.co/nw5dlRffeX",
+					"display_url": "pic.twitter.com/nw5dlRffeX",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1583906816702177280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4781,
+			"friends_count": 2301,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2446,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4497,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is your LAST CHANCE to be an exhibitor at this year's San Diego Startup Week! We only have TEN tables left, and they're going to go fast.\n\n🚀 Get an expo table: https://t.co/B5Q7uMCb3Z\n\n#SDSW2022 #sandiegostartupweek #StartupSD #sandiegoevent #startupevent https://t.co/nw5dlRffeX"
+	},
+	{
 		"created_at": "Fri Oct 21 17:34:00 +0000 2022",
 		"id": 1583511936309678000,
 		"id_str": "1583511936309678081",
