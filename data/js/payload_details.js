a0103353469c551a439398f602f2a85feb8d660c
Michael Ryan Norton <michael@michaelryannorton.com>
2022-10-24 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4301,
-	"created_at": "Sun Oct 23 00:17:02 +0000 2022",
+	"tweets": 4302,
+	"created_at": "Tue Oct 25 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 24 19:53:58 +0000 2022",
+		"id": 1584634321704550400,
+		"id_str": "1584634321704550403",
+		"full_text": "For #SDSW2022 Day 3, we're featuring a keynote panel: Marketing Like a Unicorn: Out-of-Home Advertising Plays.\n\n◽ Michele Klein, @CCOutdoorNA \n◽ Aaron Cort,  @clickup \n◽ Daniel Scott, @KandjiMDM \n\n Get tickets: https://t.co/AgCZrrS7Ux\n\n#SDSW2022 https://t.co/RsKBS7YMne",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			245
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SDSW2022",
+					"indices": [
+						4,
+						13
+					]
+				},
+				{
+					"text": "SDSW2022",
+					"indices": [
+						236,
+						245
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CCOutdoorNA",
+					"name": "Clear Channel Outdoor",
+					"id": 31449475,
+					"id_str": "31449475",
+					"indices": [
+						129,
+						141
+					]
+				},
+				{
+					"screen_name": "clickup",
+					"name": "ClickUp",
+					"id": 857643149149085700,
+					"id_str": "857643149149085696",
+					"indices": [
+						158,
+						166
+					]
+				},
+				{
+					"screen_name": "KandjiMDM",
+					"name": "Kandji",
+					"id": 1037044101500162000,
+					"id_str": "1037044101500162050",
+					"indices": [
+						184,
+						194
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AgCZrrS7Ux",
+					"expanded_url": "http://ow.ly/m2Gj50LjytK",
+					"display_url": "ow.ly/m2Gj50LjytK",
+					"indices": [
+						211,
+						234
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1584634318206419000,
+					"id_str": "1584634318206418944",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff3AhEOWQAA6PQ7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff3AhEOWQAA6PQ7.jpg",
+					"url": "https://t.co/RsKBS7YMne",
+					"display_url": "pic.twitter.com/RsKBS7YMne",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1584634321704550403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1584634318206419000,
+					"id_str": "1584634318206418944",
+					"indices": [
+						246,
+						269
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff3AhEOWQAA6PQ7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff3AhEOWQAA6PQ7.jpg",
+					"url": "https://t.co/RsKBS7YMne",
+					"display_url": "pic.twitter.com/RsKBS7YMne",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1584634321704550403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on October 25-28 for Startup Week. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/tDK2Vf20Fi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tDK2Vf20Fi",
+							"expanded_url": "https://startupsd.org/sandiegostartupweek/",
+							"display_url": "startupsd.org/sandiegostartu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4782,
+			"friends_count": 2301,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2447,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4499,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1654011158",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For #SDSW2022 Day 3, we're featuring a keynote panel: Marketing Like a Unicorn: Out-of-Home Advertising Plays.\n\n◽ Michele Klein, @CCOutdoorNA \n◽ Aaron Cort,  @clickup \n◽ Daniel Scott, @KandjiMDM \n\n Get tickets: https://t.co/AgCZrrS7Ux\n\n#SDSW2022 https://t.co/RsKBS7YMne"
+	},
+	{
 		"created_at": "Sat Oct 22 19:43:07 +0000 2022",
 		"id": 1583906816702177300,
 		"id_str": "1583906816702177280",
