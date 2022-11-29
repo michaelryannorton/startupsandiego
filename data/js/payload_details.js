39e255d09b69b9d2cd0f34d7b5ad6d44c14b2a10
Michael Ryan Norton <michael@michaelryannorton.com>
2022-11-28 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4320,
-	"created_at": "Thu Nov 24 00:17:02 +0000 2022",
+	"tweets": 4321,
+	"created_at": "Tue Nov 29 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 28 20:10:17 +0000 2022",
+		"id": 1597322004083916800,
+		"id_str": "1597322004083916800",
+		"full_text": "#GivingTuesday starts tomorrow and we want to share 5 ways to support Startup San Diego!\n\n✅ Purchase Our SSD Merch\n✅ Get 1st Mondays Tickets\n✅ Donate Directly\n✅ Become an Investor\n✅ Share Our Post\n\n🚀 Learn more &amp; support: https://t.co/IvYR5AhVEa\n\n#StartupSD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GivingTuesday",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						251,
+						261
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IvYR5AhVEa",
+					"expanded_url": "http://ow.ly/yJMe50LPkTc",
+					"display_url": "ow.ly/yJMe50LPkTc",
+					"indices": [
+						226,
+						249
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on December 5th for our last 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHErRhC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHErRhC",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4764,
+			"friends_count": 2275,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2469,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4520,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1667937997",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#GivingTuesday starts tomorrow and we want to share 5 ways to support Startup San Diego!\n\n✅ Purchase Our SSD Merch\n✅ Get 1st Mondays Tickets\n✅ Donate Directly\n✅ Become an Investor\n✅ Share Our Post\n\n🚀 Learn more &amp; support: https://t.co/IvYR5AhVEa\n\n#StartupSD"
+	},
+	{
 		"created_at": "Wed Nov 23 17:09:06 +0000 2022",
 		"id": 1595138757191254000,
 		"id_str": "1595464471245299716",
