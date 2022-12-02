28033b628601db4e8c53835dd99137cfa5a64aa5
Michael Ryan Norton <michael@michaelryannorton.com>
2022-12-01 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4322,
-	"created_at": "Wed Nov 30 00:17:06 +0000 2022",
+	"tweets": 4323,
+	"created_at": "Fri Dec 02 00:17:03 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,210 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Thu Dec 01 19:58:39 +0000 2022",
+		"id": 1598406242195701800,
+		"id_str": "1598406242195701798",
+		"full_text": "Thank you community for your ongoing support! Our organization raised over $1,800 in donations and merch sales during #GivingTuesday. We greatly appreciate all your help and generosity. \n\n🚀 Support Startup San Diego: https://t.co/7dhw1FUPe8\n\n#StartupSD #GivingTuesday https://t.co/KmPRAmbOPR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GivingTuesday",
+					"indices": [
+						118,
+						132
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						242,
+						252
+					]
+				},
+				{
+					"text": "GivingTuesday",
+					"indices": [
+						253,
+						267
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7dhw1FUPe8",
+					"expanded_url": "http://startupsd.org/donate",
+					"display_url": "startupsd.org/donate",
+					"indices": [
+						217,
+						240
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1598406239901335600,
+					"id_str": "1598406239901335553",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi6uAagWQAE8Lt9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi6uAagWQAE8Lt9.jpg",
+					"url": "https://t.co/KmPRAmbOPR",
+					"display_url": "pic.twitter.com/KmPRAmbOPR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1598406242195701798/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598406239901335600,
+					"id_str": "1598406239901335553",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi6uAagWQAE8Lt9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi6uAagWQAE8Lt9.jpg",
+					"url": "https://t.co/KmPRAmbOPR",
+					"display_url": "pic.twitter.com/KmPRAmbOPR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1598406242195701798/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on December 5th for our last 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHErRhC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHErRhC",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4767,
+			"friends_count": 2276,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4523,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1667937997",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you community for your ongoing support! Our organization raised over $1,800 in donations and merch sales during #GivingTuesday. We greatly appreciate all your help and generosity. \n\n🚀 Support Startup San Diego: https://t.co/7dhw1FUPe8\n\n#StartupSD #GivingTuesday https://t.co/KmPRAmbOPR"
+	}
+]
