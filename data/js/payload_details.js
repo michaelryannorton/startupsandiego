f32c166415618f0878d0cd52fc651d4623102001
Michael Ryan Norton <michael@michaelryannorton.com>
2022-12-02 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4323,
-	"created_at": "Fri Dec 02 00:17:03 +0000 2022",
+	"tweets": 4324,
+	"created_at": "Sat Dec 03 00:17:02 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Fri Dec 02 19:21:05 +0000 2022",
+		"id": 1598759175647002600,
+		"id_str": "1598759175647002631",
+		"full_text": "Next week is our LAST in-person 1st Mondays of the year! Join us Dec 5th for a holiday-themed networking event &amp; happy hour at @RoughDraftBrew. Bring your ugliest XMAS sweater &amp; celebrate the end-of-the-year with the startup community.\n\n🚀  Get Tickets:  https://t.co/JRefKDVzAR https://t.co/ct2D4kZab1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			285
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RoughDraftBrew",
+					"name": "Rough Draft Brewing",
+					"id": 329693688,
+					"id_str": "329693688",
+					"indices": [
+						131,
+						146
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JRefKDVzAR",
+					"expanded_url": "http://ow.ly/flgB50LTWeK",
+					"display_url": "ow.ly/flgB50LTWeK",
+					"indices": [
+						262,
+						285
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1598759174065868800,
+					"id_str": "1598759174065868800",
+					"indices": [
+						286,
+						309
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi_u_4uX0AA15gO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi_u_4uX0AA15gO.jpg",
+					"url": "https://t.co/ct2D4kZab1",
+					"display_url": "pic.twitter.com/ct2D4kZab1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1598759175647002631/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598759174065868800,
+					"id_str": "1598759174065868800",
+					"indices": [
+						286,
+						309
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi_u_4uX0AA15gO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi_u_4uX0AA15gO.jpg",
+					"url": "https://t.co/ct2D4kZab1",
+					"display_url": "pic.twitter.com/ct2D4kZab1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1598759175647002631/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on December 5th for our last 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHErRhC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHErRhC",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4764,
+			"friends_count": 2274,
+			"listed_count": 116,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4524,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1667937997",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Next week is our LAST in-person 1st Mondays of the year! Join us Dec 5th for a holiday-themed networking event &amp; happy hour at @RoughDraftBrew. Bring your ugliest XMAS sweater &amp; celebrate the end-of-the-year with the startup community.\n\n🚀  Get Tickets:  https://t.co/JRefKDVzAR https://t.co/ct2D4kZab1"
+	},
+	{
 		"created_at": "Thu Dec 01 19:58:39 +0000 2022",
 		"id": 1598406242195701800,
 		"id_str": "1598406242195701798",
