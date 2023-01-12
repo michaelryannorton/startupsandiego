157b93726fb2cfad224bed3beff4eb157ed9acb1
Michael Ryan Norton <michael@michaelryannorton.com>
2023-01-11 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4328,
-	"created_at": "Wed Dec 28 00:17:02 +0000 2022",
+	"tweets": 4329,
+	"created_at": "Thu Jan 12 00:17:02 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,136 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Wed Jan 11 18:01:05 +0000 2023",
+		"id": 1613234558350106600,
+		"id_str": "1613234558350106624",
+		"full_text": "We're happy to announce that our 1st Mondays of 2023 will be at the @arkusnexus MIND HUB in Tijuana, Mexico! Join us on Monday, March 6th, for a night of networking &amp; libations. \n\n🚀 Get tickets &amp; learn more: https://t.co/JMJdZNVus6\n\n#StartupSD #1stMondays",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSD",
+					"indices": [
+						241,
+						251
+					]
+				},
+				{
+					"text": "1stMondays",
+					"indices": [
+						252,
+						263
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arkusnexus",
+					"name": "Arkusnexus",
+					"id": 562489080,
+					"id_str": "562489080",
+					"indices": [
+						68,
+						79
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JMJdZNVus6",
+					"expanded_url": "https://www.eventbrite.com/e/482760017207",
+					"display_url": "eventbrite.com/e/482760017207",
+					"indices": [
+						216,
+						239
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on December 5th for our last 1st Mondays event. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHErRhC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHErRhC",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4760,
+			"friends_count": 2268,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2467,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4527,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1667937997",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're happy to announce that our 1st Mondays of 2023 will be at the @arkusnexus MIND HUB in Tijuana, Mexico! Join us on Monday, March 6th, for a night of networking &amp; libations. \n\n🚀 Get tickets &amp; learn more: https://t.co/JMJdZNVus6\n\n#StartupSD #1stMondays"
+	}
+]
