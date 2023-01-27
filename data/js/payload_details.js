9cc13e07b2cdb8ff6242ac2e1767932e33aff542
Michael Ryan Norton <michael@michaelryannorton.com>
2023-01-26 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4335,
-	"created_at": "Thu Jan 26 00:17:02 +0000 2023",
+	"tweets": 4336,
+	"created_at": "Fri Jan 27 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,139 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Thu Jan 26 17:00:38 +0000 2023",
+		"id": 1618655163119452200,
+		"id_str": "1618655163119452178",
+		"full_text": "With a combined residence of over 5 million, nearly double that of LA, the San Diego/Tijuana mega-region has proved that it has a broader pool of talent &amp; opportunities for business dev. \n\n🚀 Read the latest blog: https://t.co/AjCLRzFH95\n\n#StartupSD #startupsandiego @arkusnexus",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSD",
+					"indices": [
+						242,
+						252
+					]
+				},
+				{
+					"text": "startupsandiego",
+					"indices": [
+						253,
+						269
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arkusnexus",
+					"name": "Arkusnexus",
+					"id": 562489080,
+					"id_str": "562489080",
+					"indices": [
+						270,
+						281
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/AjCLRzFH95",
+					"expanded_url": "http://ow.ly/svXu50MB4ur",
+					"display_url": "ow.ly/svXu50MB4ur",
+					"indices": [
+						217,
+						240
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on March 6th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4771,
+			"friends_count": 2270,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2471,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4534,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1673578319",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "With a combined residence of over 5 million, nearly double that of LA, the San Diego/Tijuana mega-region has proved that it has a broader pool of talent &amp; opportunities for business dev. \n\n🚀 Read the latest blog: https://t.co/AjCLRzFH95\n\n#StartupSD #startupsandiego @arkusnexus"
+	},
+	{
 		"created_at": "Wed Jan 25 18:01:09 +0000 2023",
 		"id": 1618308002053775400,
 		"id_str": "1618308002053775373",
