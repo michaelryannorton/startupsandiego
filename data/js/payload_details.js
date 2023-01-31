e0baaeadc1327d59e6c0c54a2c8aed62e2af4c05
Michael Ryan Norton <michael@michaelryannorton.com>
2023-01-30 16:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4336,
-	"created_at": "Fri Jan 27 00:17:02 +0000 2023",
+	"tweets": 4337,
+	"created_at": "Tue Jan 31 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 30 16:11:03 +0000 2023",
+		"id": 1620092233662795800,
+		"id_str": "1620092233662795777",
+		"full_text": "In just 5 short weeks we'll be hosting our #1stMondays on March 6th at the @arkusnexus MIND Hub in Tijuana, MX! Please welcome our featured panelists:\n\n⭐ Angel Sanchez, Founder at @arkusnexus \n⭐ Marc Barron, Co-founder at @coursekeyedu \n\n🚀 Get tickets: https://t.co/BxkpqhFksc https://t.co/sLCfXC2bO6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						43,
+						54
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "arkusnexus",
+					"name": "Arkusnexus",
+					"id": 562489080,
+					"id_str": "562489080",
+					"indices": [
+						75,
+						86
+					]
+				},
+				{
+					"screen_name": "arkusnexus",
+					"name": "Arkusnexus",
+					"id": 562489080,
+					"id_str": "562489080",
+					"indices": [
+						180,
+						191
+					]
+				},
+				{
+					"screen_name": "coursekeyedu",
+					"name": "CourseKey - Empowering Vocational Schools/Students",
+					"id": 3195832808,
+					"id_str": "3195832808",
+					"indices": [
+						222,
+						235
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BxkpqhFksc",
+					"expanded_url": "http://ow.ly/pFzV50MEBJz",
+					"display_url": "ow.ly/pFzV50MEBJz",
+					"indices": [
+						253,
+						276
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1620092231452344300,
+					"id_str": "1620092231452344321",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Fnu5TQcWQAEPqhK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fnu5TQcWQAEPqhK.jpg",
+					"url": "https://t.co/sLCfXC2bO6",
+					"display_url": "pic.twitter.com/sLCfXC2bO6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1620092233662795777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1620092231452344300,
+					"id_str": "1620092231452344321",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Fnu5TQcWQAEPqhK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fnu5TQcWQAEPqhK.jpg",
+					"url": "https://t.co/sLCfXC2bO6",
+					"display_url": "pic.twitter.com/sLCfXC2bO6",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1620092233662795777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on March 6th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4773,
+			"friends_count": 2270,
+			"listed_count": 117,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2471,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4535,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1673578319",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In just 5 short weeks we'll be hosting our #1stMondays on March 6th at the @arkusnexus MIND Hub in Tijuana, MX! Please welcome our featured panelists:\n\n⭐ Angel Sanchez, Founder at @arkusnexus \n⭐ Marc Barron, Co-founder at @coursekeyedu \n\n🚀 Get tickets: https://t.co/BxkpqhFksc https://t.co/sLCfXC2bO6"
+	},
+	{
 		"created_at": "Thu Jan 26 17:00:38 +0000 2023",
 		"id": 1618655163119452200,
 		"id_str": "1618655163119452178",
