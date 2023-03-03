15aeea62263bd7b39dda8325237cd4971131edae
Michael Ryan Norton <michael@michaelryannorton.com>
2023-03-02 16:17:04 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4348,
-	"created_at": "Tue Feb 28 00:17:02 +0000 2023",
+	"tweets": 4349,
+	"created_at": "Fri Mar 03 00:17:03 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Thu Mar 02 14:33:42 +0000 2023",
+		"id": 1631301759657291800,
+		"id_str": "1631301759657291780",
+		"full_text": "We're only 4 DAYS away til our 1st Mondays - Networking and Expert Panel event! Don't miss out on this opportunity to connect with the SD and TJ startup community &amp; learn from some of the best startup leaders.\n\n🚀 Get tickets: https://t.co/WpDx9ixsJE\n\n#1stMondays #StartupSD https://t.co/jItI8q4qBL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						255,
+						266
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						267,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WpDx9ixsJE",
+					"expanded_url": "https://lnkd.in/gAj7Mciw",
+					"display_url": "lnkd.in/gAj7Mciw",
+					"indices": [
+						230,
+						253
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1631301757119737900,
+					"id_str": "1631301757119737856",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FqOMThqXsAAg2_u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqOMThqXsAAg2_u.jpg",
+					"url": "https://t.co/jItI8q4qBL",
+					"display_url": "pic.twitter.com/jItI8q4qBL",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1631301759657291780/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1631301757119737900,
+					"id_str": "1631301757119737856",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FqOMThqXsAAg2_u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqOMThqXsAAg2_u.jpg",
+					"url": "https://t.co/jItI8q4qBL",
+					"display_url": "pic.twitter.com/jItI8q4qBL",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1631301759657291780/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on March 6th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4790,
+			"friends_count": 2268,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2472,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4544,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1673578319",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We're only 4 DAYS away til our 1st Mondays - Networking and Expert Panel event! Don't miss out on this opportunity to connect with the SD and TJ startup community &amp; learn from some of the best startup leaders.\n\n🚀 Get tickets: https://t.co/WpDx9ixsJE\n\n#1stMondays #StartupSD https://t.co/jItI8q4qBL"
+	}
+]
