42514438c3d02aae5fe912559f8672113d88741a
Michael Ryan Norton <michael@michaelryannorton.com>
2023-03-14 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4351,
-	"created_at": "Fri Mar 10 00:17:02 +0000 2023",
+	"tweets": 4352,
+	"created_at": "Wed Mar 15 00:17:03 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 14 18:09:23 +0000 2023",
+		"id": 1635704691517472800,
+		"id_str": "1635704691517472770",
+		"full_text": "March Mingle will take place at The Prado Grand Ballroom in Balboa Park on March 30th. Get connected with the coolest startups, meetups, &amp; tech nonprofits.\n\n➡️ Get Tickets: https://t.co/r8kNZ5D9Kv\n\n#MarchMingle #networking #sandiegoevents #techevents",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MarchMingle",
+					"indices": [
+						202,
+						214
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						215,
+						226
+					]
+				},
+				{
+					"text": "sandiegoevents",
+					"indices": [
+						227,
+						242
+					]
+				},
+				{
+					"text": "techevents",
+					"indices": [
+						243,
+						254
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/r8kNZ5D9Kv",
+					"expanded_url": "http://ow.ly/y8nh50NiciN",
+					"display_url": "ow.ly/y8nh50NiciN",
+					"indices": [
+						177,
+						200
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on March 6th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4802,
+			"friends_count": 2267,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2476,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4547,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1673578319",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "March Mingle will take place at The Prado Grand Ballroom in Balboa Park on March 30th. Get connected with the coolest startups, meetups, &amp; tech nonprofits.\n\n➡️ Get Tickets: https://t.co/r8kNZ5D9Kv\n\n#MarchMingle #networking #sandiegoevents #techevents"
+	},
+	{
 		"created_at": "Thu Mar 09 01:47:22 +0000 2023",
 		"id": 1633551532062027800,
 		"id_str": "1633645622812180481",
