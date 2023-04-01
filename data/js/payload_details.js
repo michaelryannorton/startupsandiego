a46291f83914ebe4bcdb884450782e5ddbeb1c23
Michael Ryan Norton <michael@michaelryannorton.com>
2023-03-31 17:17:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4354,
-	"created_at": "Fri Mar 31 00:17:03 +0000 2023",
+	"tweets": 4355,
+	"created_at": "Sat Apr 01 00:17:04 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 31 17:53:14 +0000 2023",
+		"id": 1641861222264889300,
+		"id_str": "1641861222264889358",
+		"full_text": "TY to everyone that came by at last night's @marchmingle! It was a pleasure sharing more about SSD's mission &amp; how we serve the startup community. \n\nHuge shoutout to @SDPhelan &amp; the March Mingle team for organizing such a fantastic event.\n\n🚀 See you at #1stMondays June 5th. https://t.co/Y0gZmiQ9n5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						261,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marchmingle",
+					"name": "March Mingle",
+					"id": 113067638,
+					"id_str": "113067638",
+					"indices": [
+						44,
+						56
+					]
+				},
+				{
+					"screen_name": "SDPhelan",
+					"name": "Phelan Riessen",
+					"id": 6538602,
+					"id_str": "6538602",
+					"indices": [
+						170,
+						179
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1641861220700414000,
+					"id_str": "1641861220700413960",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FskQFM5WYAgGhU3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FskQFM5WYAgGhU3.jpg",
+					"url": "https://t.co/Y0gZmiQ9n5",
+					"display_url": "pic.twitter.com/Y0gZmiQ9n5",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1641861222264889358/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1171,
+							"h": 444,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1171,
+							"h": 444,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641861220700414000,
+					"id_str": "1641861220700413960",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FskQFM5WYAgGhU3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FskQFM5WYAgGhU3.jpg",
+					"url": "https://t.co/Y0gZmiQ9n5",
+					"display_url": "pic.twitter.com/Y0gZmiQ9n5",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1641861222264889358/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1171,
+							"h": 444,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1171,
+							"h": 444,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4813,
+			"friends_count": 2263,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2477,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4551,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TY to everyone that came by at last night's @marchmingle! It was a pleasure sharing more about SSD's mission &amp; how we serve the startup community. \n\nHuge shoutout to @SDPhelan &amp; the March Mingle team for organizing such a fantastic event.\n\n🚀 See you at #1stMondays June 5th. https://t.co/Y0gZmiQ9n5"
+	},
+	{
 		"created_at": "Thu Mar 30 18:13:00 +0000 2023",
 		"id": 1641503809733025800,
 		"id_str": "1641503809733025798",
