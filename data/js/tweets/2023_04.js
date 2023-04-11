8f6c85ba3b4b5ba3fde7f94ab1c044a8129786ba
Michael Ryan Norton <michael@michaelryannorton.com>
2023-04-10 17:17:04 -0700

    Added 1 tweets

---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Mon Apr 10 18:48:41 +0000 2023",
+		"id": 1645499056507035600,
+		"id_str": "1645499056507035666",
+		"full_text": "This is the last week to get early bird tickets for this quarter's 1st Mondays - Networking &amp; Happy Hour happening June 5th in San Diego! From now until Fri, April 14th, get 10% off #1stMondays tickets. Use the code \"earlybird10\" at checkout: https://t.co/bkiFZywGMt\n\n#StartupSD https://t.co/EUE0u8KADY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						186,
+						197
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						272,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bkiFZywGMt",
+					"expanded_url": "http://ow.ly/p94u50NFeaF",
+					"display_url": "ow.ly/p94u50NFeaF",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1645499054091108400,
+					"id_str": "1645499054091108352",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FtX8q-VXgAAXvuJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtX8q-VXgAAXvuJ.jpg",
+					"url": "https://t.co/EUE0u8KADY",
+					"display_url": "pic.twitter.com/EUE0u8KADY",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1645499056507035666/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1645499054091108400,
+					"id_str": "1645499054091108352",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/FtX8q-VXgAAXvuJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtX8q-VXgAAXvuJ.jpg",
+					"url": "https://t.co/EUE0u8KADY",
+					"display_url": "pic.twitter.com/EUE0u8KADY",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1645499056507035666/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4815,
+			"friends_count": 2262,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2476,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4552,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is the last week to get early bird tickets for this quarter's 1st Mondays - Networking &amp; Happy Hour happening June 5th in San Diego! From now until Fri, April 14th, get 10% off #1stMondays tickets. Use the code \"earlybird10\" at checkout: https://t.co/bkiFZywGMt\n\n#StartupSD https://t.co/EUE0u8KADY"
+	}
+]
