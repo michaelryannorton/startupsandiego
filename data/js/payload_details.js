353c2fc4829ab70bd4b42f58875b4a563bc2b509
Michael Ryan Norton <michael@michaelryannorton.com>
2023-04-17 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4357,
-	"created_at": "Thu Apr 13 00:17:03 +0000 2023",
+	"tweets": 4358,
+	"created_at": "Tue Apr 18 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,160 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Mon Apr 17 21:31:43 +0000 2023",
+		"id": 1648076798112956400,
+		"id_str": "1648076798112956423",
+		"full_text": "Our community partner, the @BBBPacificSW  is hosting a Speaker Series this Thurs, April 20th, at 8:30 AM, featuring Board Member Jonathan Gallagher of Coastal Payroll.\n\n➡️ Register for free: https://t.co/pLfKmbbvOk\n\n#sandiegoevents #BBB #networking #Ignite #BetterBusinessBureau",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sandiegoevents",
+					"indices": [
+						216,
+						231
+					]
+				},
+				{
+					"text": "BBB",
+					"indices": [
+						232,
+						236
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						237,
+						248
+					]
+				},
+				{
+					"text": "Ignite",
+					"indices": [
+						249,
+						256
+					]
+				},
+				{
+					"text": "BetterBusinessBureau",
+					"indices": [
+						257,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BBBPacificSW",
+					"name": "BBB Serving the Pacific Southwest",
+					"id": 21798919,
+					"id_str": "21798919",
+					"indices": [
+						27,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/pLfKmbbvOk",
+					"expanded_url": "http://ow.ly/Wq1Y50NLax5",
+					"display_url": "ow.ly/Wq1Y50NLax5",
+					"indices": [
+						191,
+						214
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4815,
+			"friends_count": 2262,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2478,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4554,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our community partner, the @BBBPacificSW  is hosting a Speaker Series this Thurs, April 20th, at 8:30 AM, featuring Board Member Jonathan Gallagher of Coastal Payroll.\n\n➡️ Register for free: https://t.co/pLfKmbbvOk\n\n#sandiegoevents #BBB #networking #Ignite #BetterBusinessBureau"
+	},
+	{
 		"created_at": "Wed Apr 12 16:50:52 +0000 2023",
 		"id": 1646194180664836000,
 		"id_str": "1646194180664836097",
