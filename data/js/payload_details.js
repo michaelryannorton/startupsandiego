def7b4d6feef25117bff3b374e61a879930f1c81
Michael Ryan Norton <michael@michaelryannorton.com>
2023-04-27 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4360,
-	"created_at": "Thu Apr 27 00:17:02 +0000 2023",
+	"tweets": 4361,
+	"created_at": "Fri Apr 28 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,165 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Thu Apr 27 16:46:15 +0000 2023",
+		"id": 1651628839649308700,
+		"id_str": "1651628839649308672",
+		"full_text": "Our April newsletter is out! Get the latest news on the Startup San Diego community.\n\n💌 View the full newsletter: https://t.co/WTBiS8Qjtd\n🗞️ Sign up for our monthly newsletter: https://t.co/lSwbQZ0imf\n\n#StartupSD #StartupSanDiego #newsletter #sandiego #innovation #startupevents",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSD",
+					"indices": [
+						202,
+						212
+					]
+				},
+				{
+					"text": "StartupSanDiego",
+					"indices": [
+						213,
+						229
+					]
+				},
+				{
+					"text": "newsletter",
+					"indices": [
+						230,
+						241
+					]
+				},
+				{
+					"text": "sandiego",
+					"indices": [
+						242,
+						251
+					]
+				},
+				{
+					"text": "innovation",
+					"indices": [
+						252,
+						263
+					]
+				},
+				{
+					"text": "startupevents",
+					"indices": [
+						264,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WTBiS8Qjtd",
+					"expanded_url": "http://ow.ly/oVex50NXUAw",
+					"display_url": "ow.ly/oVex50NXUAw",
+					"indices": [
+						114,
+						137
+					]
+				},
+				{
+					"url": "https://t.co/lSwbQZ0imf",
+					"expanded_url": "http://ow.ly/H1mn50NXUAy",
+					"display_url": "ow.ly/H1mn50NXUAy",
+					"indices": [
+						177,
+						200
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4825,
+			"friends_count": 2263,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2479,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4557,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our April newsletter is out! Get the latest news on the Startup San Diego community.\n\n💌 View the full newsletter: https://t.co/WTBiS8Qjtd\n🗞️ Sign up for our monthly newsletter: https://t.co/lSwbQZ0imf\n\n#StartupSD #StartupSanDiego #newsletter #sandiego #innovation #startupevents"
+	},
+	{
 		"created_at": "Wed Apr 26 19:42:58 +0000 2023",
 		"id": 1651310923959746600,
 		"id_str": "1651310923959746563",
