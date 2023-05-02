549beee65d6282d986fc6f5268fc89836343328c
Michael Ryan Norton <michael@michaelryannorton.com>
2023-05-01 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4361,
-	"created_at": "Fri Apr 28 00:17:02 +0000 2023",
+	"tweets": 4362,
+	"created_at": "Tue May 02 00:17:02 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_05.js
@@ -0,0 +1,205 @@
+Grailbird.data.tweets_2023_05 = 
+[
+	{
+		"created_at": "Mon May 01 19:11:56 +0000 2023",
+		"id": 1653115053971759000,
+		"id_str": "1653115053971759126",
+		"full_text": "🎉 Congrats to SD Angel Conference winner LIMBER Prosthetics and Orthotics! LIMBER uses digital design &amp; 3D printing to create accessible prosthetic devices to help those in need.\n\n➡️ Learn more: https://t.co/BkOVDwRU32\n📰 Press release: https://t.co/wg0aw4y1f7\n\n#StartupSanDiego https://t.co/B6IAWUqTFf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "StartupSanDiego",
+					"indices": [
+						265,
+						281
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BkOVDwRU32",
+					"expanded_url": "https://www.limberprosthetics.com/",
+					"display_url": "limberprosthetics.com",
+					"indices": [
+						199,
+						222
+					]
+				},
+				{
+					"url": "https://t.co/wg0aw4y1f7",
+					"expanded_url": "https://www.benzinga.com/pressreleases/23/04/b31948503/limber-prosthetics-orthotics-awarded-200-000-in-funding-at-san-diego-angel-conference-v-with-relav",
+					"display_url": "benzinga.com/pressreleases/…",
+					"indices": [
+						240,
+						263
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1653115052751216600,
+					"id_str": "1653115052751216669",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/FvELYcQWYB09QjQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvELYcQWYB09QjQ.jpg",
+					"url": "https://t.co/B6IAWUqTFf",
+					"display_url": "pic.twitter.com/B6IAWUqTFf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1653115053971759126/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1653115052751216600,
+					"id_str": "1653115052751216669",
+					"indices": [
+						282,
+						305
+					],
+					"media_url": "http://pbs.twimg.com/media/FvELYcQWYB09QjQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvELYcQWYB09QjQ.jpg",
+					"url": "https://t.co/B6IAWUqTFf",
+					"display_url": "pic.twitter.com/B6IAWUqTFf",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1653115053971759126/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 940,
+							"h": 788,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 570,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4828,
+			"friends_count": 2263,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2479,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4558,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🎉 Congrats to SD Angel Conference winner LIMBER Prosthetics and Orthotics! LIMBER uses digital design &amp; 3D printing to create accessible prosthetic devices to help those in need.\n\n➡️ Learn more: https://t.co/BkOVDwRU32\n📰 Press release: https://t.co/wg0aw4y1f7\n\n#StartupSanDiego https://t.co/B6IAWUqTFf"
+	}
+]
