5e0c1057c3a6ad5ef17570b4343d853011399c04
Michael Ryan Norton <michael@michaelryannorton.com>
2023-05-10 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4365,
-	"created_at": "Tue May 09 00:17:02 +0000 2023",
+	"tweets": 4366,
+	"created_at": "Thu May 11 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Wed May 10 18:47:43 +0000 2023",
+		"id": 1656370447787651000,
+		"id_str": "1656370447787651076",
+		"full_text": "Startup San Diego and @HomeOfBiotech have partnered up to bring the next 1st Mondays - Networking and Happy Hour to the beautiful HomeLab Torrey Pines campus.\n\n🚀 Join us Monday, June 5th. Get tickets: https://t.co/jJXATFAowN\n\n#1stMondays #StartupSD #StartupSanDiego #homelab https://t.co/T2VQ39PCRG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						226,
+						237
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						238,
+						248
+					]
+				},
+				{
+					"text": "StartupSanDiego",
+					"indices": [
+						249,
+						265
+					]
+				},
+				{
+					"text": "homelab",
+					"indices": [
+						266,
+						274
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HomeOfBiotech",
+					"name": "HomeLab",
+					"id": 1431655626103349200,
+					"id_str": "1431655626103349249",
+					"indices": [
+						22,
+						36
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/jJXATFAowN",
+					"expanded_url": "http://ow.ly/7pJ950OkLHi",
+					"display_url": "ow.ly/7pJ950OkLHi",
+					"indices": [
+						201,
+						224
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1656370444922941400,
+					"id_str": "1656370444922941468",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FvycJNIWcBwgDRS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvycJNIWcBwgDRS.jpg",
+					"url": "https://t.co/T2VQ39PCRG",
+					"display_url": "pic.twitter.com/T2VQ39PCRG",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1656370447787651076/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1656370444922941400,
+					"id_str": "1656370444922941468",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/FvycJNIWcBwgDRS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvycJNIWcBwgDRS.jpg",
+					"url": "https://t.co/T2VQ39PCRG",
+					"display_url": "pic.twitter.com/T2VQ39PCRG",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1656370447787651076/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4827,
+			"friends_count": 2263,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2480,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4563,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Startup San Diego and @HomeOfBiotech have partnered up to bring the next 1st Mondays - Networking and Happy Hour to the beautiful HomeLab Torrey Pines campus.\n\n🚀 Join us Monday, June 5th. Get tickets: https://t.co/jJXATFAowN\n\n#1stMondays #StartupSD #StartupSanDiego #homelab https://t.co/T2VQ39PCRG"
+	},
+	{
 		"created_at": "Mon May 08 17:47:08 +0000 2023",
 		"id": 1655630424842854400,
 		"id_str": "1655630424842854415",
