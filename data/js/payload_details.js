ac1d12f13b6825bc6d691b9ac7af0ea4987d9497
Michael Ryan Norton <michael@michaelryannorton.com>
2023-05-11 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4366,
-	"created_at": "Thu May 11 00:17:02 +0000 2023",
+	"tweets": 4367,
+	"created_at": "Fri May 12 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Thu May 11 16:29:17 +0000 2023",
+		"id": 1656698000444473300,
+		"id_str": "1656698000444473357",
+		"full_text": "This Friday is your LAST chance to get 1st Mondays tickets at $15! Ticket prices will increase to $25 this Saturday. Get your tickets now before prices go up.\n\n🚀 Get tickets: https://t.co/psKuTtwURM\n\n#1stMondays #StartupSD #StartupSanDiego #startups #networking #innovation https://t.co/hTjzsa4eVO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						200,
+						211
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						212,
+						222
+					]
+				},
+				{
+					"text": "StartupSanDiego",
+					"indices": [
+						223,
+						239
+					]
+				},
+				{
+					"text": "startups",
+					"indices": [
+						240,
+						249
+					]
+				},
+				{
+					"text": "networking",
+					"indices": [
+						250,
+						261
+					]
+				},
+				{
+					"text": "innovation",
+					"indices": [
+						262,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/psKuTtwURM",
+					"expanded_url": "http://ow.ly/ELFB50OlGZs",
+					"display_url": "ow.ly/ELFB50OlGZs",
+					"indices": [
+						175,
+						198
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1656697998087159800,
+					"id_str": "1656697998087159808",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fv3GDTwWAAAqEU9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fv3GDTwWAAAqEU9.jpg",
+					"url": "https://t.co/hTjzsa4eVO",
+					"display_url": "pic.twitter.com/hTjzsa4eVO",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1656698000444473357/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1656697998087159800,
+					"id_str": "1656697998087159808",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/Fv3GDTwWAAAqEU9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fv3GDTwWAAAqEU9.jpg",
+					"url": "https://t.co/hTjzsa4eVO",
+					"display_url": "pic.twitter.com/hTjzsa4eVO",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1656698000444473357/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4830,
+			"friends_count": 2263,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2480,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4564,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This Friday is your LAST chance to get 1st Mondays tickets at $15! Ticket prices will increase to $25 this Saturday. Get your tickets now before prices go up.\n\n🚀 Get tickets: https://t.co/psKuTtwURM\n\n#1stMondays #StartupSD #StartupSanDiego #startups #networking #innovation https://t.co/hTjzsa4eVO"
+	},
+	{
 		"created_at": "Wed May 10 18:47:43 +0000 2023",
 		"id": 1656370447787651000,
 		"id_str": "1656370447787651076",
