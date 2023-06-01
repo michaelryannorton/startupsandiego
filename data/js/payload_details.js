d6b783dfc3483a410b535f8f83e5352309171a2e
Michael Ryan Norton <michael@michaelryannorton.com>
2023-05-31 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4381,
-	"created_at": "Wed May 31 00:17:02 +0000 2023",
+	"tweets": 4382,
+	"created_at": "Thu Jun 01 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Wed May 31 17:22:49 +0000 2023",
+		"id": 1663959229558816800,
+		"id_str": "1663959229558816768",
+		"full_text": "5 DAYS til our 1st Mondays - Networking &amp; Happy Hour! Join +250 #startup community members  for an unforgettable night. \n\n📈 Last-minute ticket prices will increase starting June 1st! \n\n🚀 Get tickets: https://t.co/H4yJ1OHAdN\n\n#1stMondays #StartupSD https://t.co/aLhgMUU9OR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			251
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "startup",
+					"indices": [
+						68,
+						76
+					]
+				},
+				{
+					"text": "1stMondays",
+					"indices": [
+						229,
+						240
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						241,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/H4yJ1OHAdN",
+					"expanded_url": "https://www.eventbrite.com/e/576345383557",
+					"display_url": "eventbrite.com/e/576345383557",
+					"indices": [
+						204,
+						227
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1663959226715078700,
+					"id_str": "1663959226715078659",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/FxeSGcUWAAMIX1m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxeSGcUWAAMIX1m.jpg",
+					"url": "https://t.co/aLhgMUU9OR",
+					"display_url": "pic.twitter.com/aLhgMUU9OR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1663959229558816768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1663959226715078700,
+					"id_str": "1663959226715078659",
+					"indices": [
+						252,
+						275
+					],
+					"media_url": "http://pbs.twimg.com/media/FxeSGcUWAAMIX1m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxeSGcUWAAMIX1m.jpg",
+					"url": "https://t.co/aLhgMUU9OR",
+					"display_url": "pic.twitter.com/aLhgMUU9OR",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1663959229558816768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1600,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4830,
+			"friends_count": 2264,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2487,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4582,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "5 DAYS til our 1st Mondays - Networking &amp; Happy Hour! Join +250 #startup community members  for an unforgettable night. \n\n📈 Last-minute ticket prices will increase starting June 1st! \n\n🚀 Get tickets: https://t.co/H4yJ1OHAdN\n\n#1stMondays #StartupSD https://t.co/aLhgMUU9OR"
+	},
+	{
 		"created_at": "Tue May 30 17:52:45 +0000 2023",
 		"id": 1663604372398559200,
 		"id_str": "1663604372398559233",
