fe5ea81c064ea10f817617808e5e2dbdf04acb1b
Michael Ryan Norton <michael@michaelryannorton.com>
2023-06-02 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4382,
-	"created_at": "Thu Jun 01 00:17:02 +0000 2023",
+	"tweets": 4383,
+	"created_at": "Sat Jun 03 00:17:02 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_06.js
@@ -0,0 +1,210 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Fri Jun 02 16:15:45 +0000 2023",
+		"id": 1664667124755751000,
+		"id_str": "1664667124755750912",
+		"full_text": "At SSD, one of our values is community over all. That's why for #1stMondays, our expert panel will be LIVESTREAMED on all our social platforms to increase accessibility for #founders to learn about the innovations in AI and tech.\n\n🚀 Sign up: https://t.co/JJbIs5Kcai\n\n#1stMondays https://t.co/403OtaA7k1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1stMondays",
+					"indices": [
+						64,
+						75
+					]
+				},
+				{
+					"text": "founders",
+					"indices": [
+						173,
+						182
+					]
+				},
+				{
+					"text": "1stMondays",
+					"indices": [
+						267,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JJbIs5Kcai",
+					"expanded_url": "http://ow.ly/yG8250OElzP",
+					"display_url": "ow.ly/yG8250OElzP",
+					"indices": [
+						242,
+						265
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1664667121194762200,
+					"id_str": "1664667121194762241",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FxoV7UwWAAEK9Ia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxoV7UwWAAEK9Ia.jpg",
+					"url": "https://t.co/403OtaA7k1",
+					"display_url": "pic.twitter.com/403OtaA7k1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1664667124755750912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664667121194762200,
+					"id_str": "1664667121194762241",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FxoV7UwWAAEK9Ia.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FxoV7UwWAAEK9Ia.jpg",
+					"url": "https://t.co/403OtaA7k1",
+					"display_url": "pic.twitter.com/403OtaA7k1",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1664667124755750912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4830,
+			"friends_count": 2264,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4583,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "At SSD, one of our values is community over all. That's why for #1stMondays, our expert panel will be LIVESTREAMED on all our social platforms to increase accessibility for #founders to learn about the innovations in AI and tech.\n\n🚀 Sign up: https://t.co/JJbIs5Kcai\n\n#1stMondays https://t.co/403OtaA7k1"
+	}
+]
