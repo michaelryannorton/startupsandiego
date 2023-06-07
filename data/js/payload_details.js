1011a1f241b195f858407189ff1513aa8e6410eb
Michael Ryan Norton <michael@michaelryannorton.com>
2023-06-06 17:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 4384,
-	"created_at": "Mon Jun 05 00:17:05 +0000 2023",
+	"tweets": 4385,
+	"created_at": "Wed Jun 07 00:17:02 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_06.js
+++ data/js/tweets/2023_06.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2023_06 = 
 [
 	{
+		"created_at": "Tue Jun 06 19:44:15 +0000 2023",
+		"id": 1666169146529267700,
+		"id_str": "1666169146529267712",
+		"full_text": "THANK YOU Startup community for attending our 1st Mondays event! Over 300+ of you networked &amp; learned more about #AI &amp; how it's transforming the startup world.\n\nSee you at the next 1st Mondays, happening August 7th.\n\n🚀 Get Tickets: https://t.co/1m6dN7ssiJ\n\n#1stMondays #StartupSD https://t.co/tFGQSubqmF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			287
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AI",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "1stMondays",
+					"indices": [
+						265,
+						276
+					]
+				},
+				{
+					"text": "StartupSD",
+					"indices": [
+						277,
+						287
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1m6dN7ssiJ",
+					"expanded_url": "http://ow.ly/B3IN50OHeoe",
+					"display_url": "ow.ly/B3IN50OHeoe",
+					"indices": [
+						240,
+						263
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1666169143861575700,
+					"id_str": "1666169143861575686",
+					"indices": [
+						288,
+						311
+					],
+					"media_url": "http://pbs.twimg.com/media/Fx9sAjOWAAYICy3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fx9sAjOWAAYICy3.jpg",
+					"url": "https://t.co/tFGQSubqmF",
+					"display_url": "pic.twitter.com/tFGQSubqmF",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1666169146529267712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1666169143861575700,
+					"id_str": "1666169143861575686",
+					"indices": [
+						288,
+						311
+					],
+					"media_url": "http://pbs.twimg.com/media/Fx9sAjOWAAYICy3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fx9sAjOWAAYICy3.jpg",
+					"url": "https://t.co/tFGQSubqmF",
+					"display_url": "pic.twitter.com/tFGQSubqmF",
+					"expanded_url": "https://twitter.com/StartupSanDiego/status/1666169146529267712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 382,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 1334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3222944604,
+			"id_str": "3222944604",
+			"name": "Startup San Diego",
+			"screen_name": "StartupSanDiego",
+			"location": "San Diego, CA",
+			"description": "Supporting the startup ecosystem since 2013. Join us on June 5th for 1st Mondays - Happy Hour & Networking. Link in bio for tickets. #StartupSD",
+			"url": "https://t.co/H6osHEsp7a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H6osHEsp7a",
+							"expanded_url": "https://linktr.ee/startupsandiego",
+							"display_url": "linktr.ee/startupsandiego",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4834,
+			"friends_count": 2264,
+			"listed_count": 118,
+			"created_at": "Fri May 22 05:50:42 +0000 2015",
+			"favourites_count": 2489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4586,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/952311135113330688/v5DVTjoH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3222944604/1679942817",
+			"profile_link_color": "1E81AF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "THANK YOU Startup community for attending our 1st Mondays event! Over 300+ of you networked &amp; learned more about #AI &amp; how it's transforming the startup world.\n\nSee you at the next 1st Mondays, happening August 7th.\n\n🚀 Get Tickets: https://t.co/1m6dN7ssiJ\n\n#1stMondays #StartupSD https://t.co/tFGQSubqmF"
+	},
+	{
 		"created_at": "Sun Jun 04 17:33:08 +0000 2023",
 		"id": 1665411374695030800,
 		"id_str": "1665411374695030784",
